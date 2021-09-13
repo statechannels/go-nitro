73e024c60d565db111dd90c8fe43c9bd3193e9c2
geoknee <georgeknee@googlemail.com>
2021-09-13 11:49:35 +0100

    add check on indices

--- outcome/transfer.go
+++ outcome/transfer.go
@@ -4,7 +4,7 @@ import (
 	"math/big"
 )
 
-
+// min returns the minimum of the supplied integers
 func min(a big.Int, b big.Int) big.Int {
 	switch a.Cmp(&b) {
 	case -1:
@@ -16,20 +16,23 @@ func min(a big.Int, b big.Int) big.Int {
 
 // ComputeTransferEffectsAndInteractions computes the effects and interactions that will be executed on-chain when "transfer" is called
 func ComputeTransferEffectsAndInteractions(initialHoldings big.Int, allocations Allocations, indices []uint) (newAllocations Allocations, exitAllocations Allocations) {
-	// TODO here we assume indices = [], so pay out all allocations
+	var k uint
 	surplus := big.NewInt(0).Set(&initialHoldings)
 	newAllocations = make([]Allocation, len(allocations))
 	exitAllocations = make([]Allocation, len(allocations))
 
+	// for each allocation
 	for i := 0; i < len(allocations); i++ {
 		// copy allocation
 		newAllocations[i] = Allocation{Destination: allocations[i].Destination, Amount: *big.NewInt(0).Set(&allocations[i].Amount), AllocationType: allocations[i].AllocationType, Metadata: allocations[i].Metadata}
 		// compute payout amount
 		affordsForDestination := min(allocations[i].Amount, *surplus)
-		// decrease allocation amount
-		newAllocations[i].Amount.Sub(&newAllocations[i].Amount, &affordsForDestination)
-		// increase exit allocation amount
-		exitAllocations[i] = Allocation{Destination: allocations[i].Destination, Amount: *big.NewInt(0).Set(&affordsForDestination), AllocationType: allocations[i].AllocationType, Metadata: allocations[i].Metadata}
+		if (len(indices)==0 || k < uint(len(indices)) && indices[k] == uint(i)) {
+			// decrease allocation amount
+			newAllocations[i].Amount.Sub(&newAllocations[i].Amount, &affordsForDestination)
+			// increase exit allocation amount
+			exitAllocations[i] = Allocation{Destination: allocations[i].Destination, Amount: *big.NewInt(0).Set(&affordsForDestination), AllocationType: allocations[i].AllocationType, Metadata: allocations[i].Metadata}
+		}
 		// decrease surplus
 		surplus.Sub(surplus, &affordsForDestination)
 	}
