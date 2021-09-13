57cad30b7db0e2b547f0f7f8c4d4d7a5e4ffd498
geoknee <georgeknee@googlemail.com>
2021-09-13 11:13:50 +0100

    add doc.go for outcome package

---
+++ outcome/doc.go
@@ -0,0 +1,4 @@
+// Package outcome defines the data structures for expressing how channel funds
+// are to be distributed on-chain. It includes functions for manipulating those
+// structures according to the nitro operations "transfer" and "claim".
+package outcome
