1a4fcc477a335da6d9aa747757b491e3b25d7204
geoknee <georgeknee@googlemail.com>
2021-09-13 10:30:07 +0100

    add outcome methods

--- outcome/outcome.go
+++ outcome/outcome.go
@@ -20,3 +20,20 @@ type SingleAssetExit struct {
 }
 
 type Exit []SingleAssetExit
+
+// methods
+func (a Allocation) Equals(b Allocation) bool {
+	return a.Destination == b.Destination && a.AllocationType == b.AllocationType && a.Metadata == b.Metadata && a.Amount.Cmp(&b.Amount) == 0
+}
+
+func (a Allocations) Equals(b Allocations) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if !a[i].Equals(b[i]) {
+			return false
+		}
+	}
+	return true
+}
