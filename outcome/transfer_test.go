a6e6e43f5f313bb34f9946b607891e28bb402819
geoknee <georgeknee@googlemail.com>
2021-09-15 14:08:34 +0100

    neaten up test a bit

--- outcome/transfer_test.go
+++ outcome/transfer_test.go
@@ -12,19 +12,19 @@ func TestTransfer(t *testing.T) {
 	initialHoldings := *big.NewInt(100)
 
 	var initialAllocations = Allocations{{ // [{Alice: 2}]
-		Destination:    common.HexToHash("0x000000000000000000000000000000000000000000000000000000000000000a"),
+		Destination:    common.HexToHash("0x0a"),
 		Amount:         *big.NewInt(2),
 		AllocationType: 0,
 		Metadata:       make(Bytes, 0)}}
 
 	var expectedNewAllocations = Allocations{{ // [{Alice: 0}]
-		Destination:    common.HexToHash("0x000000000000000000000000000000000000000000000000000000000000000a"),
+		Destination:    common.HexToHash("0x0a"),
 		Amount:         *big.NewInt(0),
 		AllocationType: 0,
 		Metadata:       make(Bytes, 0)}}
 
 	var expectedExitAllocations = Allocations{{ // [{Alice: 2}]
-		Destination:    common.HexToHash("0x000000000000000000000000000000000000000000000000000000000000000a"),
+		Destination:    common.HexToHash("0x0a"),
 		Amount:         *big.NewInt(2),
 		AllocationType: 0,
 		Metadata:       make(Bytes, 0)}}
