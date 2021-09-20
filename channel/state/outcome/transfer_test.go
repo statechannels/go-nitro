703af7af61f2b1c082164f9693ee4fcc4d27b6a4
geoknee <georgeknee@googlemail.com>
2021-09-20 15:07:07 +0100

    move transfer and transfer_test to outcome pkg

--- channel/state/outcome/transfer_test.go
+++ channel/state/outcome/transfer_test.go
@@ -5,29 +5,30 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/statechannels/go-nitro/types"
 )
 
-func TestTransfer(t *testing.T) {
+func TestComputeTransferEffectsAndInteractions(t *testing.T) {
 
 	initialHoldings := *big.NewInt(100)
 
 	var initialAllocations = Allocations{{ // [{Alice: 2}]
 		Destination:    common.HexToHash("0x0a"),
-		Amount:         *big.NewInt(2),
+		Amount:         big.NewInt(2),
 		AllocationType: 0,
-		Metadata:       make(Bytes, 0)}}
+		Metadata:       make(types.Bytes, 0)}}
 
 	var expectedNewAllocations = Allocations{{ // [{Alice: 0}]
 		Destination:    common.HexToHash("0x0a"),
-		Amount:         *big.NewInt(0),
+		Amount:         big.NewInt(0),
 		AllocationType: 0,
-		Metadata:       make(Bytes, 0)}}
+		Metadata:       make(types.Bytes, 0)}}
 
 	var expectedExitAllocations = Allocations{{ // [{Alice: 2}]
 		Destination:    common.HexToHash("0x0a"),
-		Amount:         *big.NewInt(2),
+		Amount:         big.NewInt(2),
 		AllocationType: 0,
-		Metadata:       make(Bytes, 0)}}
+		Metadata:       make(types.Bytes, 0)}}
 
 	got1, got2 := ComputeTransferEffectsAndInteractions(initialHoldings, initialAllocations, []uint{})
 	want1 := expectedNewAllocations
