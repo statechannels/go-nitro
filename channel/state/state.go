7409e82ca5383a7525d043199d7bb2e85de2f785
geoknee <georgeknee@googlemail.com>
2021-09-27 09:43:29 +0100

    add comments

--- channel/state/state.go
+++ channel/state/state.go
@@ -8,6 +8,7 @@ import (
 )
 
 type (
+	// State holds all of the data describing the state of a channel
 	State struct {
 		ChainId           *types.Uint256
 		Participants      []types.Address
@@ -20,39 +21,47 @@ type (
 		IsFinal           bool
 	}
 
+	// ChannelPart contains the subset of State data from which the channel id is derived
 	ChannelPart struct {
 		ChainId      *types.Uint256
 		Participants []types.Address
 		ChannelNonce *types.Uint256 // uint48 in solidity
 	}
 
+	// FixedPart contains the subset of State data which does not change.
+	// NOTE: it is a strict superset of ChannelPart.
 	FixedPart struct {
 		ChainId           *types.Uint256
 		Participants      []types.Address
 		ChannelNonce      *types.Uint256 // uint48 in solidity
-		AppDefinition     types.Address
-		ChallengeDuration *uint
+		AppDefinition     types.Address  // This could change (infrequently) without affecting the channel id.
+		ChallengeDuration *uint          // This could change (infrequently) without affecting the channel id.
 	}
 
+	// VariablePart contains the subset of State data which can change with each state update.
 	VariablePart struct {
 		AppData        types.Bytes
 		EncodedOutcome types.Bytes
 	}
 )
 
+// ChannelPart returns the ChannelPart of the State
 func (s State) ChannelPart() ChannelPart {
 	return ChannelPart{s.ChainId, s.Participants, s.ChannelNonce}
 }
 
+// FixedPart returns the FixedPart of the State
 func (s State) FixedPart() FixedPart {
 	return FixedPart{s.ChainId, s.Participants, s.ChannelNonce, s.AppDefinition, s.ChallengeDuration}
 }
 
+// VariablePart returns the VariablePart of the State
 func (s State) VariablePart() VariablePart {
 	encodedOutcome, _ := s.Outcome.Encode() // TODO here we are swallowing the error
 	return VariablePart{s.AppData, encodedOutcome}
 }
 
+// ChannelId computes and returns the id corresponding to a ChannelPart
 func (c ChannelPart) ChannelId() (types.Bytes32, error) {
 	uint256, _ := abi.NewType("uint256", "uint256", nil)
 	addressArray, _ := abi.NewType("address[]", "address[]", nil)
@@ -68,6 +77,7 @@ func (c ChannelPart) ChannelId() (types.Bytes32, error) {
 
 }
 
+// ChannelId computes and returns the id corresponding to a State
 func (s State) ChannelId() (types.Bytes32, error) {
 	return s.ChannelPart().ChannelId()
 }
