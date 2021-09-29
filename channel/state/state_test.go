9b5e31db544483cc045587a94b4f65cab7d92434
geoknee <georgeknee@googlemail.com>
2021-09-29 11:33:37 +0100

    tidy up test file

--- channel/state/state_test.go
+++ channel/state/state_test.go
@@ -28,9 +28,20 @@ var state = State{
 	IsFinal:           false,
 }
 
+// The following constants are generated from our ts nitro-protocol package
+var correctChannelId = common.HexToHash(`b79270eb4cf4d11dcd5cf44c6337b1c9e4730dc3c26f022567bcf2eb63557a72`)
+var correctStateHash = common.HexToHash(`3e460a311caf589f1cf80036adfd092d05a30ff72f234918c5cdbc6c4333343a`)
+var signerPrivateKey = common.Hex2Bytes(`caab404f975b4620747174a75f08d98b4e5a7053b691b41bcfc0d839d48b7634`)
+var signerAddress = common.HexToAddress(`F5A1BB5607C9D079E46d1B3Dc33f257d937b43BD`)
+var correctSignature = Signature{
+	common.Hex2Bytes(`59d8e91bd182fb4d489bb2d76a6735d494d5bea24e4b51dd95c9d219293312d9`),
+	common.Hex2Bytes(`32274a3cec23c31e0c073b3c071cf6e0c21260b0d292a10e6a04257a2d8e87fa`),
+	byte(1), // ethers-js gives v:28, which is a legacy representation. and recoveryParam: 1 which corresponds to v here (i.e. it is the normalized version)
+} // ethers "joinSignature" gives 0x59d8e91bd182fb4d489bb2d76a6735d494d5bea24e4b51dd95c9d219293312d932274a3cec23c31e0c073b3c071cf6e0c21260b0d292a10e6a04257a2d8e87fa1c
+
 func TestChannelId(t *testing.T) {
 
-	want := common.HexToHash(`b79270eb4cf4d11dcd5cf44c6337b1c9e4730dc3c26f022567bcf2eb63557a72`) // generated from our ts nitro-protocol package
+	want := correctChannelId
 	got, error := state.ChannelId()
 
 	if error != nil {
@@ -43,7 +54,7 @@ func TestChannelId(t *testing.T) {
 
 func TestHash(t *testing.T) {
 
-	want := common.HexToHash(`3e460a311caf589f1cf80036adfd092d05a30ff72f234918c5cdbc6c4333343a`) // generated from our ts nitro-protocol package
+	want := correctStateHash
 	got, error := state.Hash()
 
 	if error != nil {
@@ -55,13 +66,9 @@ func TestHash(t *testing.T) {
 }
 
 func TestSign(t *testing.T) {
-	privateKey := common.Hex2Bytes(`caab404f975b4620747174a75f08d98b4e5a7053b691b41bcfc0d839d48b7634`)
+	privateKey := signerPrivateKey
 
-	want_r := common.Hex2Bytes(`59d8e91bd182fb4d489bb2d76a6735d494d5bea24e4b51dd95c9d219293312d9`)
-	want_s := common.Hex2Bytes(`32274a3cec23c31e0c073b3c071cf6e0c21260b0d292a10e6a04257a2d8e87fa`)
-	want_v := byte(1) // ethers-js gives v:28, which is a legacy representation. and recoveryParam: 1 which corresponds to v here (i.e. it is the normalized version)
-	// ethers "joinSignature" gives 0x59d8e91bd182fb4d489bb2d76a6735d494d5bea24e4b51dd95c9d219293312d932274a3cec23c31e0c073b3c071cf6e0c21260b0d292a10e6a04257a2d8e87fa1c
-
+	want_r, want_s, want_v := correctSignature.r, correctSignature.s, correctSignature.v
 	got, error := state.Sign(privateKey)
 	got_r, got_s, got_v := got.r, got.s, got.v
 
@@ -80,16 +87,10 @@ func TestSign(t *testing.T) {
 }
 
 func TestRecoverSigner(t *testing.T) {
-	signature := Signature{
-		common.Hex2Bytes(`59d8e91bd182fb4d489bb2d76a6735d494d5bea24e4b51dd95c9d219293312d9`),
-		common.Hex2Bytes(`32274a3cec23c31e0c073b3c071cf6e0c21260b0d292a10e6a04257a2d8e87fa`),
-		byte(1),
-	}
 
-	got, error := signature.RecoverSigner(state)
+	got, error := correctSignature.RecoverSigner(state)
 
-	// address corresponding to the private key 0xcaab404f975b4620747174a75f08d98b4e5a7053b691b41bcfc0d839d48b7634 used above
-	want := common.HexToAddress(`F5A1BB5607C9D079E46d1B3Dc33f257d937b43BD`)
+	want := signerAddress
 
 	if error != nil {
 		t.Error(error)
