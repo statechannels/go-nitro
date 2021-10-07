f5a7720c98a363e210d82b109ddb5f475a116b13
geoknee <georgeknee@googlemail.com>
2021-10-07 14:44:21 +0100

    hubrunner uses a wallet

--- toyengine/hubrunner.go
+++ toyengine/hubrunner.go
@@ -1,29 +1,5 @@
 package main
 
-import "fmt"
-
-// TODO use the wallet class here, too
 func HubRunner(hubId uint, ledgerInbox *map[uint]chan LedgerChannelState) {
-	var store = LedgerStore{make(map[uint]LedgerChannelState)}
-	var messagesHandled = 0
-
-	// Manual setup for ledger channels
-	for id := uint(1); id < num_leaves+1; id++ {
-		store.ledgerChannels[id] = LedgerChannelState{hubId, ledger_hub_balance, id, ledger_leaf_balance, 1, make(map[string]uint), true, true} // turnNum = 1 so channels are funded
-	}
-
-	// Listen for ledgerUpdates. Countersign blindly! TODO
-	for {
-		select {
-		case update := <-(*ledgerInbox)[hubId]:
-			update.signedByHub = true
-			store.ledgerChannels[update.leafId] = update
-			fmt.Printf("ledger between hub and leaf %v updated to %v\n", update.leafId, update)
-			// send back
-			(*ledgerInbox)[update.leafId] <- update // this will block unless there is something receiving on this channel or if the channel is buffered.
-			messagesHandled++
-			fmt.Printf("%v messages handled\n", messagesHandled)
-
-		}
-	}
+	NewNitroWallet(hubId, ledgerInbox, true) // we don't need to hit the API just yet. So no need to store this in a variable.
 }
