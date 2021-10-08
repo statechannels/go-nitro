da6cd30a1eb8ec1414d023f64e12843b91984b45
geoknee <georgeknee@googlemail.com>
2021-10-08 11:27:07 +0100

    add some comments

--- toyengine/coordinator.go
+++ toyengine/coordinator.go
@@ -6,15 +6,15 @@ import (
 )
 
 const (
-	num_leaves                     = 200
-	ledger_leaf_balance            = 1e9
-	ledger_hub_balance             = 1e9
-	virtual_proposer_balance       = 1e2
-	virtual_joiner_balance         = 1e2
-	leaf_propose_period            = time.Duration(1e9) // ns
-	leaf_payment_period            = time.Duration(1e7) // ns
-	leaf_close_period              = time.Duration(3e9) // ns
-	inter_node_channel_buffer_size = 1e3
+	num_leaves                     = 200                // The number of non-hub nodes in the state channel network
+	ledger_leaf_balance            = 1e9                // The balance of the leaf in each ledger channel
+	ledger_hub_balance             = 1e9                // The balance of the hub in each ledger channel
+	virtual_proposer_balance       = 1e2                // The balance of the proposer in each virtual channel
+	virtual_joiner_balance         = 1e2                // The balance of the joiner in each virtual channel
+	leaf_propose_period            = time.Duration(1e9) // (ns) period between "ticks" that trigger a new virtual channel proposal
+	leaf_payment_period            = time.Duration(1e7) // (ns) period between "ticks" that trigger a virtual channel payment
+	leaf_close_period              = time.Duration(3e9) // (ns) period between "ticks" that trigger the closing of a virtual channel
+	inter_node_channel_buffer_size = 1e3                // The number of messages buggered in each go channel linking nodes for messaging
 )
 
 // Example coordination:
@@ -29,6 +29,7 @@ const (
 // following https://gobyexample.com/stateful-goroutines
 
 func main() {
+
 	fmt.Println(`Setting up communciation channels...`)
 
 	ledgerInbox := make(map[uint]chan LedgerChannelState)
@@ -39,11 +40,13 @@ func main() {
 
 	fmt.Println(`Starting hub runner...`)
 	go HubRunner(0, &ledgerInbox)
+
 	fmt.Println(`Starting leaf runners...`)
 	for l := uint(1); l < num_leaves+1; l++ {
 		go LeafRunner(l, &ledgerInbox)
 	}
 
+	fmt.Println(`Letting everything run for a bit...`)
 	time.Sleep(time.Second * 5)
 
 }
