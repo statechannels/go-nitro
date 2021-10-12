455bbd2bfce003cb014b85f829918ac467449ee2
geoknee <georgeknee@googlemail.com>
2021-10-12 14:33:12 +0100

    extract extended state into common

--- protocols/direct-funding-statechart.go
+++ protocols/direct-funding-statechart.go
@@ -5,44 +5,13 @@ import (
 	"math/big"
 
 	"github.com/statechannels/go-nitro/channel/state"
-	"github.com/statechannels/go-nitro/types"
 )
 
-// DirectFundingExtendedState contains the (potentially infinite) extended state of the Direct Funding machine.
+type DirectFundingExtendedState = ObjectiveState // DirectFundingExtendedState contains the (potentially infinite) extended state of the Direct Funding machine.
 // The extended state of this machine is a cache of a larger store of states and events stored by a Nitro wallet.
 // This struct should be kept as shallow copyable (and this should be tested).
-type DirectFundingExtendedState struct {
-	ParticipantIndex map[types.Address]uint // the index for each participant
-	PreFundSigned    []bool                 // indexed by participant. TODO should this be initialized with my own index showing true?
 
-	MyDepositSafetyThreshold *big.Int // if the on chain holdings are equal to this amount it is safe for me to deposit
-	MyDepositTarget          *big.Int // I want to get the on chain holdings up to this much
-	FullyFundedThreshold     *big.Int // if the on chain holdings are equal
-
-	PostFundSigned []bool // indexed by participant
-}
-
-// PrefundComplete returns true if all participants have signed a prefund state, as reflected by the extended state
-func (s DirectFundingExtendedState) PrefundComplete() bool {
-	for _, index := range s.ParticipantIndex {
-		if !s.PreFundSigned[index] {
-			return false
-		}
-	}
-	return true
-}
-
-// PostfundComplete returns true if all participants have signed a postfund state, as reflected by the extended state
-func (s DirectFundingExtendedState) PostfundComplete() bool {
-	for _, index := range s.ParticipantIndex {
-		if !s.PostFundSigned[index] {
-			return false
-		}
-	}
-	return true
-}
-
-// DirectFundingProtocol state has both enumerable and extended state components.
+// DirectFundingProtocolState has both enumerable and extended state components.
 type DirectFundingProtocolState struct {
 	EnumerableState DirectFundingEnumerableState
 	ExtendedState   DirectFundingExtendedState
@@ -79,10 +48,12 @@ func (s DirectFundingProtocolState) NextState(e DirectFundingProtocolEvent) (Dir
 	switch s.EnumerableState {
 	case WaitingForCompletePrefund:
 		return s.nextStateFromWaitingForCompletePrefund(e)
+	case WaitingForMyTurnToFund:
+		return s.nextStateFromMyWaitingForMyTurnToFund(e)
 	case WaitingForCompleteFunding:
-		return s.nextStateFromFundingIncomplete(e)
+		return s.nextStateFromWaitingForCompleteFunding(e)
 	case WaitingForCompletePostFund:
-		return s.nextStateFromPostfundIncomplete(e)
+		return s.nextStateFromWaitingForCompletePostFund(e)
 	default:
 		return s, NoSideEffects, nil
 	}
@@ -109,46 +80,49 @@ func (s DirectFundingProtocolState) nextStateFromWaitingForCompletePrefund(e Dir
 	}
 
 	if newExtendedState.PrefundComplete() {
-		return DirectFundingProtocolState{WaitingForCompleteFunding, newExtendedState}, NoSideEffects, nil
+		return DirectFundingProtocolState{WaitingForMyTurnToFund, newExtendedState}, NoSideEffects, nil
 	} else {
 		return DirectFundingProtocolState{WaitingForCompletePostFund, newExtendedState}, NoSideEffects, nil
 	}
 }
 
 // nextStateFromFundingIncomplete is a component of the overall DirectFundingProtocol reducer
-func (s DirectFundingProtocolState) nextStateFromFundingIncomplete(e DirectFundingProtocolEvent) (DirectFundingProtocolState, SideEffects, error) {
+func (s DirectFundingProtocolState) nextStateFromMyWaitingForMyTurnToFund(e DirectFundingProtocolEvent) (DirectFundingProtocolState, SideEffects, error) {
 	if e.Type != FundingUpdated { // There's only one way out of this state
 		return s, NoSideEffects, nil
 	}
 
-	if e.OnChainHolding.Cmp(s.ExtendedState.FullyFundedThreshold) > -1 {
-		// We make can progess to the next enumerable state
-		return DirectFundingProtocolState{WaitingForCompletePostFund, s.ExtendedState}, NoSideEffects, nil
+	if gte(e.OnChainHolding, s.ExtendedState.MyDepositTarget) {
+		// Can move to a new enumerable state
+		return DirectFundingProtocolState{WaitingForCompleteFunding, s.ExtendedState}, NoSideEffects, nil
 	}
 
-	// We aren't fully funded
-
-	if e.OnChainHolding.Cmp(s.ExtendedState.MyDepositTarget) > -1 {
-		// Don't need to do anything but wait.
-		return s, NoSideEffects, nil
-	}
-
-	// We haven't yet hit my deposit target
-
-	if e.OnChainHolding.Cmp(s.ExtendedState.MyDepositSafetyThreshold) > -1 {
+	if gte(e.OnChainHolding, s.ExtendedState.MyDepositSafetyThreshold) {
+		// Onlty here is it safe to deposit
 		depositAmount := big.NewInt(0).Sub(s.ExtendedState.MyDepositTarget, e.OnChainHolding)
-		// TODO declare a side effect to deposit depositAmount
 		return s, SideEffects{PrepareDepositTransaction(depositAmount)}, nil
 	}
 
-	// It isn't yet safe for me to fund
+	return s, NoSideEffects, nil
+}
+
+// nextStateFromFundingIncomplete is a component of the overall DirectFundingProtocol reducer
+func (s DirectFundingProtocolState) nextStateFromWaitingForCompleteFunding(e DirectFundingProtocolEvent) (DirectFundingProtocolState, SideEffects, error) {
+	if e.Type != FundingUpdated { // There's only one way out of this state
+		return s, NoSideEffects, nil
+	}
+
+	if gte(e.OnChainHolding, s.ExtendedState.FullyFundedThreshold) {
+		// We make can progess to the next enumerable state
+		return DirectFundingProtocolState{WaitingForCompletePostFund, s.ExtendedState}, SideEffects{"send postfund"}, nil
+	}
 
 	return s, NoSideEffects, nil
 
 }
 
 // nextStateFromPostfundIncomplete is a component of the overall DirectFundingProtocol reducer
-func (s DirectFundingProtocolState) nextStateFromPostfundIncomplete(e DirectFundingProtocolEvent) (DirectFundingProtocolState, SideEffects, error) {
+func (s DirectFundingProtocolState) nextStateFromWaitingForCompletePostFund(e DirectFundingProtocolEvent) (DirectFundingProtocolState, SideEffects, error) {
 	if e.Type != PostFundReceived { // There's only one way out of this state
 		return s, NoSideEffects, nil
 	}
@@ -171,4 +145,8 @@ func (s DirectFundingProtocolState) nextStateFromPostfundIncomplete(e DirectFund
 	} else {
 		return DirectFundingProtocolState{WaitingForCompletePostFund, newExtendedState}, NoSideEffects, nil
 	}
+}
+
+func gte(a *big.Int, b *big.Int) bool {
+	return a.Cmp(b) > -1
 }
