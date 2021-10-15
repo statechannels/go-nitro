f54bcbc24b7e3d67720488e777877a5009db24bf
geoknee <georgeknee@googlemail.com>
2021-10-15 14:54:39 +0100

    add much comments

--- protocols/interfaces.go
+++ protocols/interfaces.go
@@ -4,33 +4,47 @@ import (
 	"math/big"
 
 	"github.com/statechannels/go-nitro/channel/state"
+	"github.com/statechannels/go-nitro/types"
 )
 
-// TODO these are placeholders for now
+// SideEffects is an list of effects to be executed by an imperative shell
 type SideEffects []interface{}
+
+// WaitingFor is an enumerable "pause-point" computed from an Objective. It describes how the objective is blocked on actions by third parties.
 type WaitingFor string
 
-type Status struct {
-	TurnNumRecord uint // TODO add other fields
-}
-type ObjectiveEvent struct {
-	ChannelId          string
-	Sigs               map[*state.State]state.Signature // mapping from state to signature TODO consider using a hash of the state
-	Holdings           big.Int                          // TODO allow for multiple assets
-	AdjudicationStatus Status
+// AdjudicationStatus mirrors the on chain adjudication status of a particular channel.
+// Everything that is stored on chain, other than holdings.
+type AdjudicationStatus struct {
+	TurnNumRecord uint
+	// TODO add other fields
 }
 
-// Objective is the interface for off-chain protocols
+// ObjectiveEvent holds information used to update an Objective. Some fields may be nil.
+type ObjectiveEvent struct {
+	ChannelId          string                            // Must be defined
+	Sigs               map[types.Bytes32]state.Signature // mapping from state hash to signature
+	Holdings           map[types.Address]big.Int         // mapping from asset identifier to amount
+	AdjudicationStatus AdjudicationStatus
+}
+
+// Objective is the interface for off-chain protocols.
+// The lifecycle of an objective is as follows:
+// 	* It is initialized by a single client (passing in various parameters). It is implicitly approved by that client.
+// 	* It is approved or rejected by the other clients
+// 	* It is updated with external information arriving to the client
+// 	* After each update, it is cranked. This generates side effects and other metadata
+// 	* The metadata will eventually indicate that the Objective has stalled OR the Objective has completed successfully
 type Objective interface {
 	Id() ObjectiveId
-	Initialize(initialState state.State) Objective // returns the initial Protocol object, does not declare effects
+	Initialize(initialState state.State) Objective // returns the initial Objective, does not declare effects
 
-	Approve()                              // returns an updated Protocol (a copy, no mutation allowed), does not declare effects
-	Reject()                               // returns an updated Protocol (a copy, no mutation allowed), does not declare effects
-	Update(event ObjectiveEvent) Objective // returns an updated Protocol (a copy, no mutation allowed), does not declare effects
+	Approve() Objective                    // returns an updated Objective (a copy, no mutation allowed), does not declare effects
+	Reject() Objective                     // returns an updated Objective (a copy, no mutation allowed), does not declare effects
+	Update(event ObjectiveEvent) Objective // returns an updated Objective (a copy, no mutation allowed), does not declare effects
 
 	Crank() (SideEffects, WaitingFor, error) // does *not* accept an event, but *does* declare side effects, does *not* return an updated Protocol
 }
 
-// TODO these are placeholders for now (they are the fundamental events the wallet reacts to)
+// ObjectiveId is a unique identifier for an Objective.
 type ObjectiveId string
