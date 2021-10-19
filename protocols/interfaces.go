da608c5c155b7d49cb8ff67eac54011df569e291
geoknee <georgeknee@googlemail.com>
2021-10-19 11:57:29 +0100

    add detail to the lifecycle of an objective

--- protocols/interfaces.go
+++ protocols/interfaces.go
@@ -31,8 +31,8 @@ type ObjectiveEvent struct {
 
 // Objective is the interface for off-chain protocols.
 // The lifecycle of an objective is as follows:
-// 	* It is initialized by a single client (passing in various parameters). It is implicitly approved by that client.
-// 	* It is approved or rejected by the other clients
+// 	* It is initialized by a single client (passing in various parameters). It is implicitly approved by that client. It is communicated to the other clients.
+// 	* It is stored and then approved or rejected by the other clients
 // 	* It is updated with external information arriving to the client
 // 	* After each update, it is cranked. This generates side effects and other metadata
 // 	* The metadata will eventually indicate that the Objective has stalled OR the Objective has completed successfully
