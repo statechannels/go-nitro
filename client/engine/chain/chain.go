608232ecac987c5d0767922ed8ca7f27c0a2ab02
geoknee <georgeknee@googlemail.com>
2021-10-21 14:41:12 +0100

    add GetRecieveChan to engine's services

--- client/engine/chain/chain.go
+++ client/engine/chain/chain.go
@@ -1,7 +1,20 @@
 package chain
 
-import "github.com/statechannels/go-nitro/protocols"
+import (
+	"math/big"
+
+	"github.com/statechannels/go-nitro/protocols"
+	"github.com/statechannels/go-nitro/types"
+)
+
+// ChainEvent is an internal representation of a blockchain event
+type Event struct {
+	ChannelId          types.Bytes32
+	Holdings           map[types.Address]big.Int // indexed by asset
+	AdjudicationStatus protocols.AdjudicationStatus
+}
 
 type Chain interface {
+	GetRecieveChan() chan Event
 	Submit(tx protocols.Transaction)
 }
