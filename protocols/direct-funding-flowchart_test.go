a39b9cbfb0ddec43df373b73042c2f2af90bc64c
Colin Kennedy <colin.kennedy@mesh.xyz>
2021-10-21 11:18:33 -0300

    (tmp) Compiler check fcn

--- protocols/direct-funding-flowchart_test.go
+++ protocols/direct-funding-flowchart_test.go
@@ -2,3 +2,12 @@
 // - prepare a bunch of initial states
 // - crank them
 // - assert on the sideffects and waitingFor string
+package protocols
+
+func assertObjective(o Objective) {
+	var dfObjective DirectFundingObjectiveState
+
+	approved := dfObjective.Approve()
+
+	assertObjective(approved)
+}
