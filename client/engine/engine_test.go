36e8f7fd16a22bdf68a777ba684be5b6267a6fcc
geoknee <georgeknee@googlemail.com>
2021-10-22 12:17:38 +0100

    add engine_test

---
+++ client/engine/engine_test.go
@@ -0,0 +1,31 @@
+package engine
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/statechannels/go-nitro/client/engine/chainservice"
+	"github.com/statechannels/go-nitro/client/engine/messageservice"
+	"github.com/statechannels/go-nitro/client/engine/store"
+	"github.com/statechannels/go-nitro/protocols"
+)
+
+func TestRun(t *testing.T) {
+	fmt.Println(`constructing engine`)
+
+	e := New(messageservice.TestMessageService{}, chainservice.TestChainService{}, store.TestStore{})
+
+	go e.Run()
+	msg := protocols.Message{
+		ObjectiveId: `test`,
+	}
+
+	for j := 1; j <= 10000; j++ {
+		e.fromMsg <- msg
+	}
+
+	// construct an engine with a test store
+	// hit the API repeatedly in such a way as to trigger the testobjective's expensive crank
+	// try this with and without multithreading
+
+}
