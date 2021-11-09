8122ec74f56c2d756751c5f20b1ef04514306bf2
geoknee <georgeknee@googlemail.com>
2021-11-09 13:05:47 +0000

    factor out TestState

--- channel/state/state_test.go
+++ channel/state/state_test.go
@@ -2,31 +2,10 @@ package state
 
 import (
 	"bytes"
-	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/statechannels/go-nitro/channel/state/outcome"
-	"github.com/statechannels/go-nitro/types"
 )
-
-var chainId, _ = big.NewInt(0).SetString("9001", 10)
-
-var state = State{
-	ChainId: chainId,
-	Participants: []types.Address{
-		common.HexToAddress(`0x5e29E5Ab8EF33F050c7cc10B5a0456D975C5F88d`),
-		common.HexToAddress(`0xEe18fF1575055691009aa246aE608132C57a422c`),
-		common.HexToAddress(`0x95125c394F39bBa29178CAf5F0614EE80CBB1702`),
-	},
-	ChannelNonce:      big.NewInt(37140676580),
-	AppDefinition:     common.HexToAddress(`0x5e29E5Ab8EF33F050c7cc10B5a0456D975C5F88d`),
-	ChallengeDuration: big.NewInt(60),
-	AppData:           []byte{},
-	Outcome:           outcome.Exit{},
-	TurnNum:           big.NewInt(5),
-	IsFinal:           false,
-}
 
 // The following constants are generated from our ts nitro-protocol package
 var correctChannelId = common.HexToHash(`b79270eb4cf4d11dcd5cf44c6337b1c9e4730dc3c26f022567bcf2eb63557a72`)
@@ -41,19 +20,19 @@ var correctSignature = Signature{
 
 func TestChannelId(t *testing.T) {
 	want := correctChannelId
-	got, error := state.ChannelId()
+	got, error := TestState.ChannelId()
 	checkErrorAndTestForEqualBytes(t, error, "channelId", got.Bytes(), want.Bytes())
 }
 
 func TestHash(t *testing.T) {
 	want := correctStateHash
-	got, error := state.Hash()
+	got, error := TestState.Hash()
 	checkErrorAndTestForEqualBytes(t, error, "state hash", got.Bytes(), want.Bytes())
 }
 
 func TestSign(t *testing.T) {
 	want_r, want_s, want_v := correctSignature.r, correctSignature.s, correctSignature.v
-	got, error := state.Sign(signerPrivateKey)
+	got, error := TestState.Sign(signerPrivateKey)
 	got_r, got_s, got_v := got.r, got.s, got.v
 
 	if error != nil {
@@ -71,7 +50,7 @@ func TestSign(t *testing.T) {
 }
 
 func TestRecoverSigner(t *testing.T) {
-	got, error := state.RecoverSigner(correctSignature)
+	got, error := TestState.RecoverSigner(correctSignature)
 	want := signerAddress
 	checkErrorAndTestForEqualBytes(t, error, "signer recovered", got.Bytes(), want.Bytes())
 }
--- protocols/direct-funding_test.go
+++ protocols/direct-funding_test.go
@@ -2,34 +2,12 @@ package protocols
 
 import (
 	"fmt"
-	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/statechannels/go-nitro/channel/state"
-	"github.com/statechannels/go-nitro/channel/state/outcome"
-	"github.com/statechannels/go-nitro/types"
 )
 
-var chainId, _ = big.NewInt(0).SetString("9001", 10)
-
-var initialState = state.State{ // todo factor into a test fixture
-	ChainId: chainId,
-	Participants: []types.Address{
-		common.HexToAddress(`0x5e29E5Ab8EF33F050c7cc10B5a0456D975C5F88d`),
-		common.HexToAddress(`0xEe18fF1575055691009aa246aE608132C57a422c`),
-		common.HexToAddress(`0x95125c394F39bBa29178CAf5F0614EE80CBB1702`),
-	},
-	ChannelNonce:      big.NewInt(37140676580),
-	AppDefinition:     common.HexToAddress(`0x5e29E5Ab8EF33F050c7cc10B5a0456D975C5F88d`),
-	ChallengeDuration: big.NewInt(60),
-	AppData:           []byte{},
-	Outcome:           outcome.Exit{},
-	TurnNum:           big.NewInt(5),
-	IsFinal:           false,
-}
-
 func TestNew(t *testing.T) {
-	s, _ := NewDirectFundingObjectiveState(initialState, initialState.Participants[0])
+	s, _ := NewDirectFundingObjectiveState(state.TestState, state.TestState.Participants[0])
 	fmt.Println(s)
 }
