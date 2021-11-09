75d03028f8c9a0da002a4f7070d65776cc7205c9
geoknee <georgeknee@googlemail.com>
2021-11-09 13:29:48 +0000

    add a test for update
    -- trying to update with the wrong channel id
    -- manually mutation-tested

--- protocols/direct-funding_test.go
+++ protocols/direct-funding_test.go
@@ -4,11 +4,23 @@ import (
 	"testing"
 
 	"github.com/statechannels/go-nitro/channel/state"
+	"github.com/statechannels/go-nitro/types"
 )
 
 func TestNew(t *testing.T) {
 	_, err := NewDirectFundingObjectiveState(state.TestState, state.TestState.Participants[0])
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	s, _ := NewDirectFundingObjectiveState(state.TestState, state.TestState.Participants[0])
+	e := ObjectiveEvent{
+		ChannelId: types.Bytes32{},
+	}
+	_, err := s.Update(e)
+	if err == nil {
+		t.Error(`ChannelId mismatch -- expected an error but did not get one`)
 	}
 }
