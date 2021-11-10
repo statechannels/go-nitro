aee066f3f163f2176627745c066ab03036c72fbf
Colin Kennedy <colin.kennedy@mesh.xyz>
2021-11-10 11:12:52 -0400

    Add reference  values to check correct conversions

--- channel/state/outcome/destination_test.go
+++ channel/state/outcome/destination_test.go
@@ -4,11 +4,12 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/statechannels/go-nitro/types"
 )
 
 func TestIsExternal(t *testing.T) {
 
-	e := common.HexToHash("0x00000000000100000000000096f7123E3A80C9813eF50213ADEd0e4511CB820f")
+	e := common.HexToHash("0x00000000000000000000000096f7123E3A80C9813eF50213ADEd0e4511CB820f")
 	i := common.HexToHash("0x6f7123E3A80C9813eF50213A96f7123E3A80C9813eF50213ADEd0e4511CB820f")
 
 	if !IsExternal(e) {
@@ -22,6 +23,11 @@ func TestIsExternal(t *testing.T) {
 }
 
 func TestToAddress(t *testing.T) {
+	referenceAddress := []types.Address{
+		common.HexToAddress(`0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`),
+		common.HexToAddress(`0x0000000000000000000000000000000000000000`),
+		common.HexToAddress(`0x96f7123E3A80C9813eF50213ADEd0e4511CB820f`),
+	}
 	areExternal := []common.Hash{
 		common.HexToHash("0x000000000000000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
 		common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
@@ -34,9 +40,15 @@ func TestToAddress(t *testing.T) {
 		common.HexToHash("0x0000000000b0000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
 	}
 
-	for _, extAddress := range areExternal {
-		if _, err := ToAddress(extAddress); err != nil {
+	for i, extAddress := range areExternal {
+		convertedAddress, err := ToAddress(extAddress)
+		if err != nil {
 			t.Errorf("expected to convert %x to an external address, but failed", extAddress)
+
+		}
+
+		if convertedAddress != referenceAddress[i] {
+			t.Errorf("expected %x to convert to %x, but it did not", extAddress, referenceAddress[i])
 		}
 	}
 
