86541736f60a1dc54889fa5153b4be06df09eb26
geoknee <georgeknee@googlemail.com>
2021-11-12 14:52:51 +0000

    tidy up somewhat

--- protocols/virtual-fund/virtual-funding.go
+++ protocols/virtual-fund/virtual-funding.go
@@ -9,12 +9,11 @@ import (
 	"github.com/statechannels/go-nitro/types"
 )
 
-// TODO use round 1/2/3 terminology?
 const (
-	WaitingForCompletePrefund  protocols.WaitingFor = "WaitingForCompletePrefund"
-	WaitingForCompleteFunding  protocols.WaitingFor = "WaitingForCompleteFunding"
-	WaitingForCompletePostFund protocols.WaitingFor = "WaitingForCompletePostFund"
-	WaitingForNothing          protocols.WaitingFor = "WaitingForNothing" // Finished
+	WaitingForCompletePrefund  protocols.WaitingFor = "WaitingForCompletePrefund"  // Round 1
+	WaitingForCompleteFunding  protocols.WaitingFor = "WaitingForCompleteFunding"  // Round 2
+	WaitingForCompletePostFund protocols.WaitingFor = "WaitingForCompletePostFund" // Round 3
+	WaitingForNothing          protocols.WaitingFor = "WaitingForNothing"          // Finished
 )
 
 var NoSideEffects = protocols.SideEffects{}
@@ -32,16 +31,13 @@ type VirtualFundingObjectiveState struct {
 	requestedLedgerUpdates bool // records that the ledger update side effects were previously generated (they may not have been executed yet)
 
 	ParticipantIndex map[types.Address]uint // the index for each participant
-	ExpectedStates   []state.State          // indexed by turn number
+	MyIndex          uint                   // my participant index in J
 
-	MyIndex uint // my participant index in J
-
-	PreFundSigned []bool // indexed by participant. TODO should this be initialized with my own index showing true?
-
+	PreFundSigned  []bool // indexed by participant. TODO should this be initialized with my own index showing true?
 	PostFundSigned []bool // indexed by participant
 }
 
-// NewVirtualFundingObjectiveState initiates a DirectFundingInitialState with data calculated from
+// NewVirtualFundingObjectiveState initiates a VirtualFundingObjectiveState with data calculated from
 // the supplied initialState and client address
 func NewVirtualFundingObjectiveState(initialState state.State, myAddress types.Address) (VirtualFundingObjectiveState, error) {
 	var init VirtualFundingObjectiveState
@@ -51,10 +47,12 @@ func NewVirtualFundingObjectiveState(initialState state.State, myAddress types.A
 
 // Public methods on the VirtualFundingObjectiveState
 
+// Id returns the objective id
 func (s VirtualFundingObjectiveState) Id() protocols.ObjectiveId {
 	return protocols.ObjectiveId("VirtualFundingAsTerminal-" + s.J.Id.String())
 }
 
+// Approve returns an approved copy of the objective
 func (s VirtualFundingObjectiveState) Approve() protocols.Objective {
 	updated := s.clone()
 	// todo: consider case of s.Status == Rejected
@@ -62,6 +60,7 @@ func (s VirtualFundingObjectiveState) Approve() protocols.Objective {
 	return updated
 }
 
+// Approve returns a rejected copy of the objective
 func (s VirtualFundingObjectiveState) Reject() protocols.Objective {
 	updated := s.clone()
 	updated.Status = protocols.Rejected
@@ -96,7 +95,7 @@ func (s VirtualFundingObjectiveState) Crank(secretKey *[]byte) (protocols.Object
 
 	// Prefunding
 	if !updated.PreFundSigned[updated.MyIndex] {
-
+		// todo sign the prefund
 		return updated, NoSideEffects, WaitingForCompletePrefund, nil
 	}
 
@@ -111,13 +110,13 @@ func (s VirtualFundingObjectiveState) Crank(secretKey *[]byte) (protocols.Object
 		return updated, s.generateLedgerRequestSideEffects(), WaitingForCompleteFunding, nil
 	}
 
-	fundingComplete := updated.fundingComplete()
-	if !fundingComplete {
+	if !updated.fundingComplete() {
 		return updated, NoSideEffects, protocols.WaitingForCompleteFunding, nil
 	}
+
 	// Postfunding
 	if !updated.PostFundSigned[updated.MyIndex] {
-		// todo: []string{SignPostFundEffect(updated.ChannelId)} as SideEffects{}
+		// todo: sign the postfund
 		return updated, NoSideEffects, WaitingForCompletePostFund, nil
 	}
 
@@ -126,7 +125,6 @@ func (s VirtualFundingObjectiveState) Crank(secretKey *[]byte) (protocols.Object
 	}
 
 	// Completion
-	// todo: []string{"protocols.Objective" + s.ChannelId.String() + "complete"} as SideEffects{}
 	return updated, NoSideEffects, WaitingForNothing, nil
 }
 
@@ -158,13 +156,14 @@ func (s VirtualFundingObjectiveState) fundingComplete() bool {
 	n := uint(len(s.L)) // n = numHops + 1 (the number of ledger channels)
 
 	if s.MyRole == n+1 { // If I'm Bob, or peer n+1
-		return s.L[n].GuaranteesFor(s.J.Id).IsNonZero() // TODO a proper check
+		return s.L[n].GuaranteesFor(s.J.Id).IsNonZero() // TODO a proper check on each asset (against s.J.Total)
 	} else {
-		return s.L[s.MyRole].GuaranteesFor(s.J.Id).IsNonZero() // TODO a proper check
+		return s.L[s.MyRole].GuaranteesFor(s.J.Id).IsNonZero() // TODO a proper check on each asset (against s.J.Total)
 	}
 
 }
 
+// generateLedgerRequestSideEffects generates the appropriate side effects, which (when executed and countersigned) will update 1 or 2 ledger channels to guarantee the joint channel
 func (s VirtualFundingObjectiveState) generateLedgerRequestSideEffects() protocols.SideEffects {
 	sideEffects := protocols.SideEffects{}
 	sideEffects.LedgerRequests = make([]protocols.LedgerRequest, 2)
@@ -190,6 +189,7 @@ func (s VirtualFundingObjectiveState) generateLedgerRequestSideEffects() protoco
 	return sideEffects
 }
 
+// inScope returns true if the supplied channelId is the joint channel or one of the ledger channels. Can be used to filter out events that don't concern these channels.
 func (s VirtualFundingObjectiveState) inScope(channelId types.Bytes32) bool {
 	if channelId == s.J.Id {
 		return true
