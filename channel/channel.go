085bd8805b11fb1b694f2b2e49e186c42f9b3ef4
geoknee <georgeknee@googlemail.com>
2021-11-12 14:36:40 +0000

    add Channel abstraction

---
+++ channel/channel.go
@@ -0,0 +1,22 @@
+package channel
+
+import (
+	"github.com/statechannels/go-nitro/channel/state"
+	"github.com/statechannels/go-nitro/types"
+)
+
+type Channel struct {
+	Id             types.Bytes32
+	OnChainFunding types.Funds
+
+	state.FixedPart
+	Support []state.VariablePart
+}
+
+func (c Channel) GuaranteesFor(channelId types.Destination) types.Funds {
+	return types.Funds{} // TODO get this info from the Support
+}
+
+func (c Channel) Total() types.Funds {
+	return types.Funds{} // TODO get this infor from the Support
+}
