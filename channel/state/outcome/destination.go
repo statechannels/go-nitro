95f48bdb9bfd96a78f2ec07ec44f288fe21ee367
Colin Kennedy <colin.kennedy@mesh.xyz>
2021-11-12 10:49:31 -0400

    Apply `Destination` type to dest/address helpers

--- channel/state/outcome/destination.go
+++ channel/state/outcome/destination.go
@@ -7,7 +7,7 @@ import (
 )
 
 // IsExternal returns true if the destination has the 12 leading bytes as zero, false otherwise
-func IsExternal(destination types.Bytes32) bool {
+func IsExternal(destination types.Destination) bool {
 	for _, b := range destination[0:12] {
 		if b != 0 {
 			return false
@@ -18,7 +18,7 @@ func IsExternal(destination types.Bytes32) bool {
 
 // ToAddress returns a types.Address encoded external destination, or an error if
 // destination is not an external address
-func ToAddress(destination types.Bytes32) (types.Address, error) {
+func ToAddress(destination types.Destination) (types.Address, error) {
 	if IsExternal(destination) {
 		address := types.Address{}
 		for i, b := range destination[12:] {
