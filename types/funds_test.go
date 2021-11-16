3af209a12b4b1d74c1d5d9c8a359b24ce1209b50
Colin Kennedy <colin.kennedy@mesh.xyz>
2021-11-16 16:42:17 -0400

    add `Equal` test

--- types/funds_test.go
+++ types/funds_test.go
@@ -20,6 +20,10 @@ var testData map[string]Funds = map[string]Funds{
 	"d": {
 		common.HexToAddress("0x02"): big.NewInt(1),
 	},
+	"e": {
+		common.HexToAddress("0x00"):  big.NewInt(1),
+		common.HexToAddress("0xabc"): big.NewInt(0),
+	},
 	// manually calculated sums
 	"ab": {
 		common.HexToAddress("0x00"): big.NewInt(2),
@@ -44,5 +48,45 @@ func TestSum(t *testing.T) {
 }
 
 func TestEqual(t *testing.T) {
+	// Check self-equality
+	for _, f := range testData {
+		equal := f.Equal(f)
 
+		if !equal {
+			t.Errorf("expected %s to equal %s, but it didn't", f, f)
+		}
+	}
+
+	equalPairs := []Pair{
+		{testData["a"], testData["b"]},
+		{testData["a"], testData["e"]},
+	}
+
+	for _, p := range equalPairs {
+		equal := p.a.Equal(p.b) && p.b.Equal(p.a)
+
+		if !equal {
+			t.Errorf("expected %s to equal %s, but it didn't", p.a, p.b)
+		}
+	}
+
+	unequalPairs := []Pair{
+		{testData["a"], testData["c"]},
+		{testData["a"], testData["d"]},
+		{testData["a"], testData["ab"]},
+	}
+
+	for _, p := range unequalPairs {
+		equal := p.a.Equal(p.b)
+
+		if equal {
+			t.Errorf("expected %s to not equal %s, but it did", p.a, p.b)
+		}
+	}
+
+}
+
+type Pair struct {
+	a Funds
+	b Funds
 }
