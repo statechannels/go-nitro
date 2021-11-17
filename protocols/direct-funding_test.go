ef12cecefa3638e2b75d479aae2774f78b1c940c
geoknee <georgeknee@googlemail.com>
2021-11-17 13:36:55 +0000

    add an assertion for behaviour with dummy & faulty sigs

--- protocols/direct-funding_test.go
+++ protocols/direct-funding_test.go
@@ -3,6 +3,7 @@ package protocols
 import (
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/statechannels/go-nitro/channel/state"
 	"github.com/statechannels/go-nitro/types"
 )
@@ -26,10 +27,20 @@ func TestUpdate(t *testing.T) {
 
 	e.ChannelId = s.ChannelId // Fix to correct channelId
 	e.Sigs = make(map[types.Bytes32]state.Signature)
-	e.Sigs[types.Bytes32(e.ChannelId)] = state.Signature{} // Dummmy signature on dummy statehash
+	var dummySignature = state.Signature{
+		R: common.Hex2Bytes(`49d8e91bd182fb4d489bb2d76a6735d494d5bea24e4b51dd95c9d219293312d9`),
+		S: common.Hex2Bytes(`22274a3cec23c31e0c073b3c071cf6e0c21260b0d292a10e6a04257a2d8e87fa`),
+		V: byte(1),
+	}
+	var dummyStateHash = common.Hash{}
+	e.Sigs[dummyStateHash] = dummySignature // Dummmy signature on dummy statehash
 	_, err = s.Update(e)
 	if err != nil {
-		t.Error(`Useless signature -- expected a noop but caught an error`)
+		t.Error(`Useless signature -- expected a noop but caught an error:`, err)
 	}
-
+	e.Sigs[dummyStateHash] = state.Signature{} // Faulty Signature on dummy statehash
+	_, err = s.Update(e)
+	if err != nil {
+		t.Error(`faulty signature -- expected a noop but caught an error:`, err)
+	}
 }
