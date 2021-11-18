d1cebbb26a03b905684b6d260bc547adf54d2f4f
geoknee <georgeknee@googlemail.com>
2021-11-18 14:02:12 +0000

    Export R,S,V components of SIgnature
    And swap out participant 0 in TestState (we would like to use an address we know the private key for)

--- channel/state/test-fixtures.go
+++ channel/state/test-fixtures.go
@@ -13,7 +13,7 @@ var chainId, _ = big.NewInt(0).SetString("9001", 10)
 var TestState = State{
 	ChainId: chainId,
 	Participants: []types.Address{
-		common.HexToAddress(`0x5e29E5Ab8EF33F050c7cc10B5a0456D975C5F88d`),
+		common.HexToAddress(`0xF5A1BB5607C9D079E46d1B3Dc33f257d937b43BD`), // private key caab404f975b4620747174a75f08d98b4e5a7053b691b41bcfc0d839d48b7634
 		common.HexToAddress(`0xEe18fF1575055691009aa246aE608132C57a422c`),
 		common.HexToAddress(`0x95125c394F39bBa29178CAf5F0614EE80CBB1702`),
 	},
