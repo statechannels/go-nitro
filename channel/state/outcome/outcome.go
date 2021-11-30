32ce382e301e0e83ecaf411aa0a0a703b02f4d1f
Colin Kennedy <colin.kennedy@mesh.xyz>
2021-11-30 12:12:40 -0400

    Add `DepositSafetyThreshold` to `Exit`,`Single`...
    ... `AssetExit`.
    
    note: these methods may be too specific to direct-funding to live on the
    `Exit` / `Outcome` directiy. Alternative is:
    
    `func DepositSafetyThreshold (e Exit, interests []types.Destination)` in
    `direct-funding.go`.

--- channel/state/outcome/outcome.go
+++ channel/state/outcome/outcome.go
@@ -79,6 +79,27 @@ func (allocations Allocations) Affords(given Allocation, funding *big.Int) bool 
 	return false
 }
 
+// DepositSafetyThreshold returns the amount of this asset that a user with
+// the specified interests must see on-chain before the safe recoverability of
+// their own deposts is guaranteed
+func (s SingleAssetExit) DepositSafetyThreshold(interests []types.Destination) *big.Int {
+	sum := big.NewInt(0)
+
+	for _, allocation := range s.Allocations {
+
+		for _, interest := range interests {
+			if allocation.Destination == interest {
+				// we have 'hit' a destination whose balances we are interested in protecting
+				return sum
+			}
+		}
+
+		sum.Add(sum, allocation.Amount)
+	}
+
+	return sum
+}
+
 // SingleAssetExit declares an ordered list of Allocations for a single asset.
 type SingleAssetExit struct {
 	Asset       types.Address // Either the zero address (implying the native token) or the address of an ERC20 contract
@@ -121,6 +142,19 @@ func (e Exit) TotalAllocated() types.Funds {
 	}
 
 	return fullValue
+}
+
+// DepositSafetyThreshold returns the Funds that a user with the specified
+// interests must see on-chain before the safe recoverability of their
+// deposits is guaranteed
+func (e Exit) DepositSafetyThreshold(interests []types.Destination) types.Funds {
+	threshold := types.Funds{}
+
+	for _, assetExit := range e {
+		threshold[assetExit.Asset] = assetExit.DepositSafetyThreshold(interests)
+	}
+
+	return threshold
 }
 
 // allocationsTy describes the shape of Allocations such that github.com/ethereum/go-ethereum/accounts/abi can parse it
