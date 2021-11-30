f78540d69a059b788490aadbec65926e138a5269
geoknee <georgeknee@googlemail.com>
2021-11-30 12:02:53 +0000

    Add Channel.Affords() method
    Threads through to the Outcome.Affords method

--- channel/channel.go
+++ channel/channel.go
@@ -1,7 +1,9 @@
 package channel
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/statechannels/go-nitro/channel/state"
+	"github.com/statechannels/go-nitro/channel/state/outcome"
 	"github.com/statechannels/go-nitro/types"
 )
 
@@ -24,4 +26,14 @@ func (c Channel) Total() types.Funds {
 		funds[sae.Asset] = sae.Allocations.Total()
 	}
 	return funds
+}
+
+// Affords returns true if, for each asset keying the input variables, the channel can afford the allocation given the funding.
+// The decision is made based on the latest supported state of the channel.
+//
+// Both arguments are maps keyed by the same asset
+func (c Channel) Affords(
+	allocationMap map[common.Address]outcome.Allocation,
+	fundingMap types.Funds) bool {
+	return c.LatestSupportedState.Outcome.Affords(allocationMap, fundingMap)
 }
