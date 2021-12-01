a9ff0d94ed0c21be4e8e33af5de88bdb697bb70f
geoknee <georgeknee@googlemail.com>
2021-12-01 14:25:42 +0000

    WIP start testing crank

--- protocols/virtual-fund/virtual-fund_test.go
+++ protocols/virtual-fund/virtual-fund_test.go
@@ -3,6 +3,7 @@ package virtualfund
 import (
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/statechannels/go-nitro/channel/state"
 )
 
@@ -12,4 +13,48 @@ func TestNew(t *testing.T) {
 	if _, err := New(state.TestState, state.TestState.Participants[0], 0); err != nil {
 		t.Error(err)
 	}
+}
+
+var s, _ = New(state.TestState, state.TestState.Participants[0], 0)
+var privateKeyOfParticipant0 = common.Hex2Bytes(`caab404f975b4620747174a75f08d98b4e5a7053b691b41bcfc0d839d48b7634`)
+
+func TestCrank(t *testing.T) {
+	// Assert that cranking an unapproved objective returns an error
+	if _, _, _, err := s.Crank(&privateKeyOfParticipant0); err == nil {
+		t.Error(`Expected error when cranking unapproved objective, but got nil`)
+	}
+
+	// Approve the objective, so that the rest of the test cases can run.
+	o := s.Approve()
+
+	// To test the finite state progression, we are going to progressively mutate o
+	// And then crank it to see which "pause point" (WaitingFor) we end up at.
+
+	// Initial Crank
+	_, _, waitingFor, err := o.Crank(&privateKeyOfParticipant0)
+	if err != nil {
+		t.Error(err)
+	}
+	if waitingFor != WaitingForCompletePrefund {
+		t.Errorf(`WaitingFor: expected %v, got %v`, WaitingForCompletePrefund, waitingFor)
+	}
+
+	// Manually progress the extended state by ...
+
+	// Cranking should move us to the next waiting point
+	_, _, waitingFor, err = o.Crank(&privateKeyOfParticipant0)
+	if err != nil {
+		t.Error(err)
+	}
+	if waitingFor != WaitingForCompleteFunding {
+		t.Errorf(`WaitingFor: expected %v, got %v`, WaitingForCompleteFunding, waitingFor)
+	}
+
+	// This should be the final crank...
+	// TODO manually progress the state by ...
+	// if waitingFor != WaitingForNothing {
+	// 	t.Errorf(`WaitingFor: expected %v, got %v`, WaitingForNothing, waitingFor)
+	// }
+
+	// TODO Test the returned SideEffects
 }
