1ae169633c42b56697cbdddbd21b7c675c003f3f
geoknee <georgeknee@googlemail.com>
2021-12-09 10:37:10 +0000

    add unit tests for setup states

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -11,7 +11,7 @@ func TestChannel(t *testing.T) {
 	s := state.TestState.Clone()
 	_, err1 := New(s, true, 0, state.TestOutcome[0].Allocations[0].Destination, state.TestOutcome[0].Allocations[1].Destination)
 	s.TurnNum = big.NewInt(0)
-	_, err2 := New(s, true, 0, state.TestOutcome[0].Allocations[0].Destination, state.TestOutcome[0].Allocations[1].Destination)
+	c, err2 := New(s, true, 0, state.TestOutcome[0].Allocations[0].Destination, state.TestOutcome[0].Allocations[1].Destination)
 
 	testNew := func(t *testing.T) {
 		if err1 == nil {
@@ -22,5 +22,37 @@ func TestChannel(t *testing.T) {
 		}
 	}
 
+	testPreFund := func(t *testing.T) {
+		got, err1 := c.PreFundState().Hash()
+		want, err2 := s.Hash()
+		if err1 != nil {
+			t.Error(err1)
+		}
+		if err2 != nil {
+			t.Error(err2)
+		}
+		if got != want {
+			t.Errorf(`incorrect incorrect PreFundState returned, got %v wanted %v`, c.PreFundState(), s)
+		}
+	}
+
+	testPostFund := func(t *testing.T) {
+		got, err1 := c.PostFundState().Hash()
+		spf := s.Clone()
+		spf.TurnNum = big.NewInt(1)
+		want, err2 := spf.Hash()
+		if err1 != nil {
+			t.Error(err1)
+		}
+		if err2 != nil {
+			t.Error(err2)
+		}
+		if got != want {
+			t.Errorf(`incorrect incorrect PreFundState returned, got %v wanted %v`, c.PostFundState(), s)
+		}
+	}
+
 	t.Run(`TestNew`, testNew)
+	t.Run(`TestPreFund`, testPreFund)
+	t.Run(`TestPostFund`, testPostFund)
 }
