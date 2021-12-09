3d69ba54bf84c743afc6235a0ac7171a42825c08
geoknee <georgeknee@googlemail.com>
2021-12-09 11:00:54 +0000

    add unit test for c.Total()

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -4,7 +4,10 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/google/go-cmp/cmp"
 	"github.com/statechannels/go-nitro/channel/state"
+	"github.com/statechannels/go-nitro/types"
 )
 
 func TestChannel(t *testing.T) {
@@ -52,7 +55,72 @@ func TestChannel(t *testing.T) {
 		}
 	}
 
+	testPreFundSignedByMe := func(t *testing.T) {
+		got := c.PreFundSignedByMe()
+		want := false
+		if got != want {
+			t.Error(`expected c.PreFundSignedByMe() to be false, but it is true`)
+		}
+
+	}
+
+	testPostFundSignedByMe := func(t *testing.T) {
+		got := c.PostFundSignedByMe()
+		want := false
+		if got != want {
+			t.Error(`expected c.PostFundSignedByMe() to be false, but it is true`)
+		}
+
+	}
+
+	testPreFundComplete := func(t *testing.T) {
+		got := c.PreFundComplete()
+		want := false
+		if got != want {
+			t.Error(`expected c.PreFundComplete() to be false, but it is true`)
+		}
+
+	}
+
+	testPostFundComplete := func(t *testing.T) {
+		got := c.PostFundComplete()
+		want := false
+		if got != want {
+			t.Error(`expected c.PostFundComplete() to be false, but it is true`)
+		}
+	}
+
+	testLatestSupportedState := func(t *testing.T) {
+		got, err1 := c.LatestSupportedState().Hash()
+		want, err2 := s.Hash()
+		if err1 != nil {
+			t.Error(err1)
+		}
+		if err2 != nil {
+			t.Error(err2)
+		}
+		if got != want {
+			t.Errorf(`incorrect LatestSupportedState returned, got %v wanted %v`, c.LatestSupportedState(), s)
+		}
+	}
+
+	testTotal := func(t *testing.T) {
+		got := c.Total()
+		want := types.Funds{
+			common.Address{}: big.NewInt(10),
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("TestCrank: side effects mismatch (-want +got):\n%s", diff)
+		}
+	}
+
 	t.Run(`TestNew`, testNew)
 	t.Run(`TestPreFund`, testPreFund)
 	t.Run(`TestPostFund`, testPostFund)
+	t.Run(`TestPreFundSignedByMe`, testPreFundSignedByMe)
+	t.Run(`TestPostFundSignedByMe`, testPostFundSignedByMe)
+	t.Run(`TestPreFundComplete`, testPreFundComplete)
+	t.Run(`TestPostFundComplete`, testPostFundComplete)
+	t.Run(`TestLatestSupportedState`, testLatestSupportedState)
+	t.Run(`TestTotal`, testTotal)
 }
