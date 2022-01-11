369b8ba692dfe29b845f6ca8c88e444129f9bb9f
geoknee <georgeknee@googlemail.com>
2022-01-11 16:38:42 +0000

    Add mockchainservice_test.go

---
+++ client/engine/chainservice/mockchainservice_test.go
@@ -0,0 +1,60 @@
+package chainservice
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/statechannels/go-nitro/protocols"
+	"github.com/statechannels/go-nitro/types"
+)
+
+func TestNew(t *testing.T) {
+	NewMockChainService()
+}
+func TestInstantDeposit(t *testing.T) {
+	// MockChainService should react to a deposit transaction for a given channel by:
+	// - immediately sending an event with updated holdings for that channel.
+
+	// Construct Mock Chain Service and get references to chans
+	mcs := NewMockChainService()
+	send := mcs.GetSendChan()
+	recieve := mcs.GetReceiveChan()
+
+	// Prepare test data to trigger MockChainService
+	testDeposit := types.Funds{
+		common.HexToAddress("0x00"): big.NewInt(1),
+	}
+	testTx := protocols.Transaction{
+		ChannelId: types.Destination(common.HexToHash(`4ebd366d014a173765ba1e50f284c179ade31f20441bec41664712aac6cc461d`)),
+		Deposit:   testDeposit,
+	}
+
+	// Send one transaction and recieve one event
+	send <- testTx
+	event := <-recieve
+
+	if event.ChannelId != testTx.ChannelId {
+		t.Error(`channelId mismatch`)
+	}
+	if !event.Holdings.Equal(testTx.Deposit) {
+		t.Error(`holdings mismatch`)
+	}
+
+	// Send the transaction again and recieve another event
+	send <- testTx
+	event = <-recieve
+
+	// The expectation is that the MockChainService remembered the previous deposit and added this one to it:
+	expectedHoldings := types.Funds{
+		common.HexToAddress("0x00"): big.NewInt(2),
+	}
+
+	if event.ChannelId != testTx.ChannelId {
+		t.Error(`channelId mismatch`)
+	}
+	if !event.Holdings.Equal(expectedHoldings) {
+		t.Error(`holdings mismatch`)
+	}
+
+}
