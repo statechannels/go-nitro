c8871c8c4be1fab4901224440ca25e8af434813a
Your Name <andrew.gord.stewart@gmail.com>
2022-01-13 17:12:11 -0800

    add some voucher-related code
    This adds some scaffolding code that we can use to discuss the
    requirements of a payment provider and receipt provider. The initial
    prototype aims to outline the core payment flow.
    
    With this prototype, I make the following assumptions:
    1. Payments are always unidirectional, and Alice always pays Bob.
    2. The virtual-defunder protocol will use a voucher to determine how
       to conclude a virtually funded channel
    
    Both assumptions are easy to relax, depending on feature requests.

---
+++ payments/vouchers.go
@@ -0,0 +1,66 @@
+package payments
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/statechannels/go-nitro/channel/state"
+	nc "github.com/statechannels/go-nitro/crypto"
+	"github.com/statechannels/go-nitro/types"
+)
+
+type (
+	// A Voucher signed by Alice can be used by Bob to redeem payments in case of
+	// a misbehaving Alice.
+	//
+	// During normal operation, Alice & Bob would terminate the channel with an
+	// outcome reflecting the largest amount signed by Alice. For instance,
+	// - if the channel started with balances {alice: 100, bob: 0}
+	// - and the biggest voucher signed by alice had amount = 20
+	// - then Alice and Bob would cooperatively conclude the channel with outcome
+	//   {alice: 80, bob: 20}
+	Voucher struct {
+		channelId types.Destination
+		amount    uint
+		signature state.Signature
+	}
+)
+
+func (v Voucher) hash() (types.Bytes32, error) {
+	encoded, err := abi.Arguments{
+		{Type: nc.Destination},
+		{Type: nc.Uint256},
+	}.Pack(v.channelId, v.amount)
+
+	if err != nil {
+		return types.Bytes32{}, fmt.Errorf("failed to encode voucher: %w", err)
+	}
+	return crypto.Keccak256Hash(encoded), nil
+}
+
+func (v *Voucher) sign(pk []byte) error {
+	hash, err := v.hash()
+	if err != nil {
+		return err
+	}
+
+	sig, err := nc.SignEthereumMessage(hash.Bytes(), pk)
+
+	if err != nil {
+		return err
+	}
+
+	v.signature = sig
+
+	return nil
+}
+
+func (v Voucher) recoverSigner() (types.Address, error) {
+	h, error := v.hash()
+	if error != nil {
+		return types.Address{}, error
+	}
+	return nc.RecoverEthereumMessageSigner(h[:], v.signature)
+}
+
