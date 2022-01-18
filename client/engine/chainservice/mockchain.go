8b6ea4efdc38ae85003fb5d8d11d5e8bb6e73531
geoknee <georgeknee@googlemail.com>
2022-01-18 18:07:48 +0000

    Introduce MockChain type

---
+++ client/engine/chainservice/mockchain.go
@@ -0,0 +1,71 @@
+package chainservice
+
+import (
+	"github.com/statechannels/go-nitro/protocols"
+	"github.com/statechannels/go-nitro/types"
+)
+
+// MockChain provides an interface which simulates a blockchain network. It is designed for use as a central service which multiple
+// ChainServices connect to with Go chans.
+// It keeps a record of of holdings and adjudication status for each channel, accepts transactions and emits events.
+type MockChain struct {
+	out map[types.Address]chan Event // out is a mapping with a chan for each connected ChainService, used to send Events to that service
+	in  chan protocols.Transaction   // in is the chan used to recieve Transactions from multiple ChainServices
+
+	holdings map[types.Destination]types.Funds // holdings tracks funds for each channel
+}
+
+// Out() returns the out chan for a particular ChainService, and narrows the type so that external consumers mays only recieve on it.
+func (mc MockChain) Out(a types.Address) <-chan Event {
+	return mc.out[a]
+}
+
+// In returns the in chan but narrows the type so that external consumers may only send on it.
+func (mc MockChain) In() chan<- protocols.Transaction {
+	return mc.in
+}
+
+// NewMockChain returns a new MockChain with an out chan initialized for each of the addresses passed in.
+func NewMockChain(addresses []types.Address) MockChain {
+
+	mc := MockChain{}
+	mc.out = make(map[types.Address]chan Event)
+	mc.in = make(chan protocols.Transaction)
+	mc.holdings = make(map[types.Destination]types.Funds)
+
+	const BUFFER_SIZE = 10
+	for _, a := range addresses {
+		mc.out[a] = make(chan Event, BUFFER_SIZE)
+	}
+
+	go mc.Run()
+	return mc
+}
+
+// Run starts a listener for transactions on the MockChain's in chan.
+func (mc MockChain) Run() {
+	for tx := range mc.in {
+		mc.handleTx(tx)
+	}
+}
+
+// handleTx responds to the given tx.
+func (mc MockChain) handleTx(tx protocols.Transaction) {
+	if tx.Deposit.IsNonZero() {
+		mc.holdings[tx.ChannelId] = mc.holdings[tx.ChannelId].Add(tx.Deposit)
+	}
+	event := Event{
+		ChannelId:          tx.ChannelId,
+		Holdings:           mc.holdings[tx.ChannelId],
+		AdjudicationStatus: protocols.AdjudicationStatus{TurnNumRecord: 0},
+	}
+	for _, out := range mc.out {
+		go sendEvent(out, event) // we use a goroutine for each send to prevent being blocked by one bad listener
+	}
+
+}
+
+// sendEvent sends event to the supplied chan
+func sendEvent(out chan Event, event Event) {
+	out <- event
+}
