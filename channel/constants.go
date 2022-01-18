5a2c4b089978d3857eb4f41d273e123fb6bd45bf
andrewgordstewart <andrew.gord.stewart@gmail.com>
2022-01-18 13:15:44 -0800

    update comment

--- channel/constants.go
+++ channel/constants.go
@@ -3,5 +3,5 @@ package channel
 const (
 	PreFundTurnNum uint64 = iota
 	PostFundTurnNum
-	MaxTurnNum = ^uint64(0) // MAXTURNNUM is a reserved value which is taken to mean "there is not yet a supported state"
+	MaxTurnNum = ^uint64(0) // MaxTurnNum is a reserved value which is taken to mean "there is not yet a supported state"
 )
