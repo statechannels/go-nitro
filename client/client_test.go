7f080aa133309b553490a790577f61592d4a34bc
geoknee <georgeknee@googlemail.com>
2022-01-20 12:41:12 +0000

    generate keypair in test
    in future, we should probably have this step stuffed into client.New() and not inject anything. We would retain a trapdoor to inject stuff optionally in tests, though

--- client/client_test.go
+++ client/client_test.go
@@ -1,9 +1,10 @@
 package client
 
 import (
+	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/statechannels/go-nitro/channel/state/outcome"
 	"github.com/statechannels/go-nitro/client/engine/chainservice"
 	"github.com/statechannels/go-nitro/client/engine/messageservice"
 	"github.com/statechannels/go-nitro/client/engine/store"
@@ -12,20 +13,16 @@ import (
 
 func TestNew(t *testing.T) {
 
-	skA := common.Hex2Bytes("c417e8a75ebe2bfe16fe108e1e04802c324974eef6ea2cc3d55194fa38677b5e")
-	a := types.Address(common.HexToAddress(`0xaaa3D879df547333a9ac87341C92f11e5FB79CD4`))
+	aKey, a := GeneratePrivateKeyAndAddress()
+	_, b := GeneratePrivateKeyAndAddress()
+	chain := chainservice.MockChain{}
 
-	b := types.Address(common.HexToAddress(`b`))
-
-	chain := chainservice.NewMockChain([]types.Address{a, b})
-
-	chainservice := chainservice.NewSimpleChainService(chain, a)
-
+	chainserv := chainservice.NewSimpleChainService(chain, a)
 	messageservice := messageservice.NewTestMessageService(a)
+	store := store.NewMockStore()
+	client := New(messageservice, chainserv, store, aKey, a)
 
-	store := store.NewMockStore(skA)
+	chain = chainservice.NewMockChain([]types.Address{a, b})
 
-	client := New(messageservice, chainservice, store)
-
-	client.CreateChannel()
+	client.CreateDirectChannel(b, types.Address{}, types.Bytes{}, outcome.Exit{}, big.NewInt(0))
 }
