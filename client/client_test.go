c2cb55e0519bc4892bddcd99529b5fba0eb0e09b
geoknee <georgeknee@googlemail.com>
2022-01-21 13:42:53 +0000

    pk => sk

--- client/client_test.go
+++ client/client_test.go
@@ -12,12 +12,12 @@ import (
 
 func TestNew(t *testing.T) {
 
-	pkA := common.Hex2Bytes("c417e8a75ebe2bfe16fe108e1e04802c324974eef6ea2cc3d55194fa38677b5e")
+	skA := common.Hex2Bytes("c417e8a75ebe2bfe16fe108e1e04802c324974eef6ea2cc3d55194fa38677b5e")
 	a := types.Address(common.HexToAddress(`0xaaa3D879df547333a9ac87341C92f11e5FB79CD4`))
 	b := types.Address(common.HexToAddress(`b`))
 	chain := chainservice.NewMockChain([]types.Address{a, b})
 	chainservice := chainservice.NewSimpleChainService(chain, a)
 	messageservice := messageservice.NewTestMessageService(a)
-	store := store.NewMockStore(pkA)
+	store := store.NewMockStore(skA)
 	New(messageservice, chainservice, store) // TODO reinstate client:=
 }
