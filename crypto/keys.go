d7b516cc2b164ec417cefe6145c187c4e8561510
geoknee <georgeknee@googlemail.com>
2022-01-24 14:07:21 +0000

    add GeneratePrivateKeyAndAddress() to crypto package

---
+++ crypto/keys.go
@@ -0,0 +1,25 @@
+package crypto
+
+import (
+	"crypto/ecdsa"
+	"log"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/statechannels/go-nitro/types"
+)
+
+func GeneratePrivateKeyAndAddress() (types.Bytes, types.Address) {
+	channelSecretKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	channelSecretKeyBytes := crypto.FromECDSA(channelSecretKey)
+
+	publicKey := channelSecretKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+	return channelSecretKeyBytes, address
+}
