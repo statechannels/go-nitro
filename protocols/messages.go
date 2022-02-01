96621474388f7d43cc6036e7b9be5542918ed5eb
geoknee <georgeknee@googlemail.com>
2022-02-01 10:49:42 +0000

    add Equal method to Message type

--- protocols/messages.go
+++ protocols/messages.go
@@ -1,6 +1,7 @@
 package protocols
 
 import (
+	"bytes"
 	"encoding/json"
 
 	"github.com/statechannels/go-nitro/channel/state"
@@ -15,15 +16,35 @@ type Message struct {
 	Proposal     Objective
 }
 
-// Serialize serializes the message into a string
-func (m Message) Serialize() string {
-	bytes, _ := json.Marshal(m) // TODO handle error
-	return string(bytes)
+// Serialize serializes the message into a string.
+func (m Message) Serialize() (string, error) {
+	bytes, err := json.Marshal(m)
+	return string(bytes), err
 }
 
-// DeserialiseMessage deserializes the passed string into a protocols.Message
+// DeserialiseMessage deserializes the passed string into a protocols.Message.
 func DeserialiseMessage(s string) (Message, error) {
 	msg := Message{}
 	err := json.Unmarshal([]byte(s), &msg)
 	return msg, err
+}
+
+// Equal returns true if the passed Message is deeply equal in value to the receiver, and false otherwise.
+func (m Message) Equal(n Message) bool {
+	if bytes.Equal(m.To.Bytes(), n.To.Bytes()) {
+		return false
+	}
+	if m.ObjectiveId != n.ObjectiveId {
+		return false
+	}
+	if len(m.SignedStates) != len(n.SignedStates) {
+		return false
+	}
+	for i, ss := range m.SignedStates {
+		if !ss.Equal(m.SignedStates[i]) {
+			return false
+		}
+	}
+	// TODO handle Proposal field :-/
+	return true
 }
