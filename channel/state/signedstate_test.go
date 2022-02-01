816605af361e648fa7ba204633863077b165b832
geoknee <georgeknee@googlemail.com>
2022-02-01 16:51:31 +0000

    add TestMerge

---
+++ channel/state/signedstate_test.go
@@ -0,0 +1,38 @@
+package state
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestMerge(t *testing.T) {
+
+	ss1 := NewSignedState(TestState)
+	sigA, _ := TestState.Sign(common.Hex2Bytes(`caab404f975b4620747174a75f08d98b4e5a7053b691b41bcfc0d839d48b7634`))
+	ss1.AddSignature(sigA)
+
+	ss2 := NewSignedState(TestState)
+	sigB, _ := TestState.Sign(common.Hex2Bytes(`62ecd49c4ccb41a70ad46532aed63cf815de15864bc415c87d507afd6a5e8da2`))
+	ss2.AddSignature(sigB)
+
+	err := ss1.Merge(ss2)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	got := ss1
+	want := SignedState{
+		TestState,
+		map[uint]Signature{
+			0: sigA,
+			1: sigB,
+		},
+	}
+
+	if !got.Equal(want) {
+		t.Errorf(`incorrect merge, got %v, wanted %v`, got, want)
+	}
+
+}
