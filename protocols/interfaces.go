7f3e10781f290e2cae04aa4f6c9c486e17a8a4a4
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-02-03 12:23:27 -0400

    add `MarshalJSON()` to `Objective`

--- protocols/interfaces.go
+++ protocols/interfaces.go
@@ -68,6 +68,7 @@ type Objective interface {
 	Channels() []types.Destination
 
 	Crank(secretKey *[]byte) (Objective, SideEffects, WaitingFor, error) // does *not* accept an event, but *does* accept a pointer to a signing key; declare side effects; return an updated Objective
+	MarshalJSON() ([]byte, error)
 }
 
 // ObjectiveId is a unique identifier for an Objective.
