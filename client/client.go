fc7d31f14a0ebe9a6530e11646ad76cfe0e6d3e3
Alex Gap <alex.gap@gmail.com>
2022-02-03 10:55:16 -0800

    rename to channel funded

--- client/client.go
+++ client/client.go
@@ -17,17 +17,18 @@ import (
 	"github.com/statechannels/go-nitro/types"
 )
 
-type ChannelReadyEvent struct {
+// ChannelFundedEvents is returned in a channel when a state channel has been successfly funded.
+type ChannelFundedEvent struct {
 	ChannelId   types.Destination
 	ObjectiveId protocols.ObjectiveId
 }
 
 // Client provides the interface for the consuming application
 type Client struct {
-	engine       engine.Engine // The core business logic of the client
-	Address      *types.Address
-	ChannelReady chan ChannelReadyEvent // All Objective updates from the engine
-	listeners    map[protocols.ObjectiveId]chan<- ChannelReadyEvent
+	engine        engine.Engine // The core business logic of the client
+	Address       *types.Address
+	ChannelFunded chan ChannelFundedEvent
+	listeners     map[protocols.ObjectiveId]chan<- ChannelFundedEvent
 }
 
 // New is the constructor for a Client. It accepts a messaging service, a chain service, and a store as injected dependencies.
@@ -35,18 +36,19 @@ func New(messageService messageservice.MessageService, chainservice chainservice
 	c := Client{}
 	c.Address = store.GetAddress()
 	c.engine = engine.New(messageService, chainservice, store, logDestination)
-	c.ChannelReady = make(chan ChannelReadyEvent, 100)
-	c.listeners = make(map[protocols.ObjectiveId]chan<- ChannelReadyEvent)
+	c.ChannelFunded = make(chan ChannelFundedEvent, 100)
+	c.listeners = make(map[protocols.ObjectiveId]chan<- ChannelFundedEvent)
 	// Start the engine in a go routine
 	go c.engine.Run()
 
 	// Start the event handler in a go routine
+	// It will listen for events from the engine and dispatch events to client channels
 	go c.handleEngineEvents()
 
 	return c
 }
 
-func (c *Client) registerListener(objectiveId protocols.ObjectiveId, listener chan<- ChannelReadyEvent) {
+func (c *Client) registerListener(objectiveId protocols.ObjectiveId, listener chan<- ChannelFundedEvent) {
 	c.listeners[objectiveId] = listener
 }
 func (c *Client) removeListener(objectiveId protocols.ObjectiveId) {
@@ -64,12 +66,13 @@ func (c *Client) handleEngineEvents() {
 	for update := range c.engine.ToApi() {
 
 		for _, completed := range update.CompletedObjectives {
+			// TODO: We're assuming the first channel id is the one we're interested in.
 			channelId := completed.Channels()[0]
-			event := ChannelReadyEvent{ObjectiveId: completed.Id(), ChannelId: channelId}
+			event := ChannelFundedEvent{ObjectiveId: completed.Id(), ChannelId: channelId}
 
 			// We dispatch an event to the channel that handles **all** objective updates.
 			// This provides a central place to monitor for objective updates.
-			c.ChannelReady <- event
+			c.ChannelFunded <- event
 
 			// Dispatch an event to any listeners that have been registered by calls to CreateDirectChannel
 			if l, ok := c.listeners[completed.Id()]; ok {
@@ -86,7 +89,7 @@ func (c *Client) handleEngineEvents() {
 // Begin API
 
 // CreateDirectChannel creates a directly funded channel with the given counterparty
-func (c *Client) CreateDirectChannel(counterparty types.Address, appDefinition types.Address, appData types.Bytes, outcome outcome.Exit, challengeDuration *types.Uint256) (protocols.ObjectiveId, chan ChannelReadyEvent) {
+func (c *Client) CreateDirectChannel(counterparty types.Address, appDefinition types.Address, appData types.Bytes, outcome outcome.Exit, challengeDuration *types.Uint256) (protocols.ObjectiveId, chan ChannelFundedEvent) {
 	// Convert the API call into an internal event.
 	objective, _ := directfund.New(true,
 		state.State{
@@ -110,8 +113,8 @@ func (c *Client) CreateDirectChannel(counterparty types.Address, appDefinition t
 	// Send the event to the engine
 	c.engine.FromAPI <- apiEvent
 
-	clientResponse := make(chan ChannelReadyEvent)
-	// We register a listener  channelfor the objective id
+	clientResponse := make(chan ChannelFundedEvent)
+	// We register a listener so a ChannelFundedEvent will get sent to clientResponse when the objective completes
 	c.registerListener(objective.Id(), clientResponse)
 
 	return objective.Id(), clientResponse
