140b6c229d12dbc0143a26b92a5914f7a858da3d
geoknee <georgeknee@googlemail.com>
2022-02-04 11:53:55 +0000

    add doc.go to crypto package

---
+++ crypto/doc.go
@@ -0,0 +1,2 @@
+// Package crypto contains types and functions for creating Ethereum private keys and accounts, and creating/recovering signatures made with such keys.
+package crypto // import "github.com/statechannels/go-nitro/crypto"
