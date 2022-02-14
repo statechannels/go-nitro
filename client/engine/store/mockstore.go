0c9868039943ab4a65640d9167a2ec9ffc68b1b0
Alex Gap <alex.gap@gmail.com>
2022-02-14 14:33:19 -0800

    Add get and set channel func on Store

--- client/engine/store/mockstore.go
+++ client/engine/store/mockstore.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/statechannels/go-nitro/channel"
 	"github.com/statechannels/go-nitro/protocols"
 	"github.com/statechannels/go-nitro/types"
 )
@@ -55,12 +56,30 @@ func (ms MockStore) SetObjective(obj protocols.Objective) error {
 	ms.objectives[obj.Id()] = obj
 	return nil
 }
+func (ms MockStore) SetChannel(channel *channel.Channel) error {
+	// TODO: Right now we're just updating the channel in memory, so setChannel can be a no-op.
+	// This should be updated in https://github.com/statechannels/go-nitro/issues/191
+	return nil
+
+}
+
+func (ms MockStore) GetChannel(channelId types.Destination) (*channel.Channel, bool) {
+	// todo: locking
+	for _, obj := range ms.objectives {
+		for _, ch := range obj.Channels() {
+			if ch.Id == channelId {
+				return ch, true
+			}
+		}
+	}
+	return nil, false
+}
 
 func (ms MockStore) GetObjectiveByChannelId(channelId types.Destination) (protocols.Objective, bool) {
 	// todo: locking
 	for _, obj := range ms.objectives {
 		for _, ch := range obj.Channels() {
-			if ch == channelId {
+			if ch.Id == channelId {
 				return obj, true
 			}
 		}
