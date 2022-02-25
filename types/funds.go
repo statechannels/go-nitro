f159b359bb8cb5000414023bf3becb5ef711819f
geoknee <georgeknee@googlemail.com>
2022-02-25 11:43:20 +0000

    add TestFundsClone to types package

--- types/funds.go
+++ types/funds.go
@@ -80,7 +80,7 @@ func (f Funds) canAfford(g Funds) bool {
 	return true
 }
 
-// Clone returns a deep copy of the receiver
+// Clone returns a deep copy of the receiver.
 func (f Funds) Clone() Funds {
 	return Sum(f, Funds{})
 }
