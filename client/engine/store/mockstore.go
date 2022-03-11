1b1a606d38f05a64f2cc0161dca73acf91bd08a2
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-03-11 12:11:54 -0400

    refactor: pull Channel saturation code to new fcn

--- client/engine/store/mockstore.go
+++ client/engine/store/mockstore.go
@@ -51,42 +51,11 @@ func (ms MockStore) GetObjectiveById(id protocols.ObjectiveId) (protocols.Object
 	if err != nil {
 		return nil, fmt.Errorf("error decoding objective %s: %w", id, err)
 	}
-	// populate channel data
-	if dfo, isDirectFund := obj.(*directfund.Objective); isDirectFund {
-		ch, err := ms.getChannelById(dfo.C.Id)
 
-		if err != nil {
-			return nil, fmt.Errorf("error retrieving channel data for objective %s: %w", id, err)
-		}
-
-		dfo.C = &ch
-
-		obj = dfo
-	} else if vfo, isVirtualFund := obj.(*virtualfund.Objective); isVirtualFund {
-		v, err := ms.getChannelById(vfo.V.Id)
-		if err != nil {
-			return nil, fmt.Errorf("error retrieving virtual channel data for objective %s: %w", id, err)
-		}
-		vfo.V = &channel.SingleHopVirtualChannel{Channel: v}
-
-		if vfo.ToMyLeft != nil && vfo.ToMyLeft.Channel != nil {
-			left, err := ms.getChannelById(vfo.ToMyLeft.Channel.Id)
-			if err != nil {
-				return nil, fmt.Errorf("error retrieving left ledger channel data for objective %s: %w", id, err)
-			}
-			vfo.ToMyLeft.Channel = &channel.TwoPartyLedger{Channel: left}
-		}
-
-		if vfo.ToMyRight != nil && vfo.ToMyRight.Channel != nil {
-			right, err := ms.getChannelById(vfo.ToMyRight.Channel.Id)
-			if err != nil {
-				return nil, fmt.Errorf("error retrieving right ledger channel data for objective %s: %w", id, err)
-			}
-			vfo.ToMyRight.Channel = &channel.TwoPartyLedger{Channel: right}
-
-		}
-
-		obj = vfo
+	obj, err = ms.populateChannelData(obj)
+	if err != nil {
+		// return existing objective data along with error
+		return obj, fmt.Errorf("error populating channel data for objective %s: %w", id, err)
 	}
 
 	return obj, nil
@@ -159,6 +128,55 @@ func (ms MockStore) GetObjectiveByChannelId(channelId types.Destination) (protoc
 	}
 
 	return nil, false
+}
+
+// populateChannelData fetches stored Channel data relevent to the given
+// objective, attaches it to the objective, and returns the objective
+func (ms MockStore) populateChannelData(obj protocols.Objective) (protocols.Objective, error) {
+	id := obj.Id()
+
+	if dfo, isDirectFund := obj.(*directfund.Objective); isDirectFund {
+
+		ch, err := ms.getChannelById(dfo.C.Id)
+
+		if err != nil {
+			return nil, fmt.Errorf("error retrieving channel data for objective %s: %w", id, err)
+		}
+
+		dfo.C = &ch
+
+		return dfo, nil
+
+	} else if vfo, isVirtualFund := obj.(*virtualfund.Objective); isVirtualFund {
+
+		v, err := ms.getChannelById(vfo.V.Id)
+		if err != nil {
+			return nil, fmt.Errorf("error retrieving virtual channel data for objective %s: %w", id, err)
+		}
+		vfo.V = &channel.SingleHopVirtualChannel{Channel: v}
+
+		if vfo.ToMyLeft != nil && vfo.ToMyLeft.Channel != nil {
+			left, err := ms.getChannelById(vfo.ToMyLeft.Channel.Id)
+			if err != nil {
+				return nil, fmt.Errorf("error retrieving left ledger channel data for objective %s: %w", id, err)
+			}
+			vfo.ToMyLeft.Channel = &channel.TwoPartyLedger{Channel: left}
+		}
+
+		if vfo.ToMyRight != nil && vfo.ToMyRight.Channel != nil {
+			right, err := ms.getChannelById(vfo.ToMyRight.Channel.Id)
+			if err != nil {
+				return nil, fmt.Errorf("error retrieving right ledger channel data for objective %s: %w", id, err)
+			}
+			vfo.ToMyRight.Channel = &channel.TwoPartyLedger{Channel: right}
+
+		}
+
+		return vfo, nil
+
+	} else {
+		return nil, fmt.Errorf("objective %s did not correctly represent a known Objective type", id)
+	}
 }
 
 // decodeObjective is a helper which encapsulates the deserialization
