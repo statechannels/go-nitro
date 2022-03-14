2e9be31c817b61d2b0019cc07a5acfe1dab36277
Alex Gap <alex.gap@gmail.com>
2022-03-14 11:22:04 -0700

    refactor test this allows us to increase the channels easily

--- client_test/virtualfund_multiparty_integration_test.go
+++ client_test/virtualfund_multiparty_integration_test.go
@@ -5,8 +5,10 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/statechannels/go-nitro/client"
 	"github.com/statechannels/go-nitro/client/engine/chainservice"
 	"github.com/statechannels/go-nitro/client/engine/messageservice"
+	"github.com/statechannels/go-nitro/protocols"
 	"github.com/statechannels/go-nitro/protocols/virtualfund"
 	"github.com/statechannels/go-nitro/types"
 )
@@ -28,33 +30,39 @@ func TestMultiPartyVirtualFundIntegration(t *testing.T) {
 	directlyFundALedgerChannel(t, clientAlice, clientIrene)
 	directlyFundALedgerChannel(t, clientIrene, clientBob)
 	directlyFundALedgerChannel(t, clientIrene, clientBrian)
-	withBobRequest := virtualfund.ObjectiveRequest{
-		MyAddress:         alice,
-		CounterParty:      bob,
-		Intermediary:      irene,
-		Outcome:           createVirtualOutcome(alice, bob),
+
+	bobIds := createVirtualChannels(&clientAlice, bob, irene, 5)
+	brianIds := createVirtualChannels(&clientAlice, brian, irene, 5)
+	allIds := append(bobIds, brianIds...)
+	waitTimeForCompletedObjectiveIds(t, &clientBob, defaultTimeout, bobIds...)
+	waitTimeForCompletedObjectiveIds(t, &clientBrian, defaultTimeout, brianIds...)
+
+	waitTimeForCompletedObjectiveIds(t, &clientAlice, defaultTimeout, allIds...)
+	waitTimeForCompletedObjectiveIds(t, &clientIrene, defaultTimeout, allIds...)
+}
+
+// createVirtualChannels is a helper function to create virtual channels in bulk
+func createVirtualChannels(client *client.Client, counterParty types.Address, intermediary types.Address, amount uint) []protocols.ObjectiveId {
+	request := virtualfund.ObjectiveRequest{
+		MyAddress:         *client.Address,
+		CounterParty:      counterParty,
+		Intermediary:      intermediary,
+		Outcome:           createVirtualOutcome(*client.Address, counterParty),
 		AppDefinition:     types.Address{},
 		AppData:           types.Bytes{},
 		ChallengeDuration: big.NewInt(0),
 		Nonce:             rand.Int63(),
 	}
-	withBrianRequest := virtualfund.ObjectiveRequest{
-		MyAddress:         alice,
-		CounterParty:      brian,
-		Intermediary:      irene,
-		Outcome:           createVirtualOutcome(alice, bob),
-		AppDefinition:     types.Address{},
-		AppData:           types.Bytes{},
-		ChallengeDuration: big.NewInt(0),
-		Nonce:             rand.Int63(),
+
+	ids := []protocols.ObjectiveId{}
+	for i := 0; i < int(amount); i++ {
+		// Generate a unique nonce for each request
+		request.Nonce = rand.Int63()
+
+		id := client.CreateVirtualChannel(request)
+		ids = append(ids, id)
+
 	}
-	id := clientAlice.CreateVirtualChannel(withBobRequest)
-	id2 := clientAlice.CreateVirtualChannel(withBrianRequest)
-
-	waitTimeForCompletedObjectiveIds(t, &clientBob, defaultTimeout, id)
-	waitTimeForCompletedObjectiveIds(t, &clientBrian, defaultTimeout, id2)
-
-	waitTimeForCompletedObjectiveIds(t, &clientAlice, defaultTimeout, id, id2)
-	waitTimeForCompletedObjectiveIds(t, &clientIrene, defaultTimeout, id, id2)
+	return ids
 
 }
