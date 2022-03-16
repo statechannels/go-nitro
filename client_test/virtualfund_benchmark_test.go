35f6e219d7adf72be61f7e3d8fb3872156dd4da1
Your Name <andrew.gord.stewart@gmail.com>
2022-03-16 14:12:51 -0700

    introduce a rudimentary "benchmarking" test
    This test asserts that creating one virtual channel takes at most 10ms.
    
    If run locally in verbose mode, it will print how long the VirtualFund
    objective took to complete:
    
    ```
    go test ./... -run TestBenchmark -v
    
    ...
    === RUN   TestBenchmark
    VirtualFund-0xe25bb77c1b36b618f553c1fab4b473c53c2a67a1c63591bdc2a72b244e039fd6 took 6.005875ms
    ...
    ```

---
+++ client_test/virtualfund_benchmark_test.go
@@ -0,0 +1,79 @@
+package client_test
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/statechannels/go-nitro/client"
+	"github.com/statechannels/go-nitro/client/engine/chainservice"
+	"github.com/statechannels/go-nitro/client/engine/messageservice"
+	"github.com/statechannels/go-nitro/types"
+)
+
+// TestBenchmark sets up three clients, then runs a virtual funding benchmark, printing the duration
+// to the screen.
+func TestBenchmark(t *testing.T) {
+
+	logFile := "virtualfund_benchmark_test.log"
+	truncateLog(logFile)
+
+	chain := chainservice.NewMockChain()
+	broker := messageservice.NewBroker()
+
+	clientAlice := setupClient(aliceKey, chain, broker, logFile)
+	clientBob := setupClient(bobKey, chain, broker, logFile)
+	clientIrene := setupClient(ireneKey, chain, broker, logFile)
+
+	directlyFundALedgerChannel(t, clientAlice, clientIrene)
+	directlyFundALedgerChannel(t, clientIrene, clientBob)
+
+	done := make(chan bool)
+
+	go benchmarkVirtualChannelCreation(t, clientAlice, clientBob, irene, done)
+	// Once the wallet can safely handle multiple virtual channel creations, we
+	// can do things like the following:
+	// go benchmarkVirtualChannelCreation(t, clientAlice, clientBob, irene, done)
+	// go benchmarkVirtualChannelCreation(t, clientAlice, clientBob, irene, done)
+	// expect(t, done, 3)
+
+	expect(t, done, 1, time.Millisecond*10)
+}
+
+func benchmarkVirtualChannelCreation(t *testing.T, alice, bob client.Client, irene types.Address, done chan bool) {
+	outcome := createVirtualOutcome(*alice.Address, *bob.Address)
+	id := alice.CreateVirtualChannel(*bob.Address, irene, types.Address{}, types.Bytes{}, outcome, big.NewInt(0))
+
+	defer elapsed(string(id))()
+	waitTimeForCompletedObjectiveIds(t, &alice, defaultTimeout, id)
+	done <- true
+}
+
+// Returns after `done` has received `num` messages.
+//
+// To ensure it eventually returns, it will error after a timeout, which resets
+// whenever `done` receives a message. So, it will return after at most
+// `num * defaultTimeout` time has elapsed.
+func expect(t *testing.T, done chan bool, num int, timeout time.Duration) {
+	count := 0
+	for {
+		select {
+		case <-done:
+			count += 1
+			if count == num {
+				return
+			}
+		case <-time.After(timeout):
+			t.Errorf("Ran out of time. %v out of %v completed", count, num)
+			t.FailNow()
+		}
+	}
+}
+
+func elapsed(what string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", what, time.Since(start))
+	}
+}
