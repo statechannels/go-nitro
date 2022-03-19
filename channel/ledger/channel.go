0adb67552ff35937ea8c834f1e60eb03724b5e93
Your Name <andrew.gord.stewart@gmail.com>
2022-03-18 17:53:39 -0700

    add Proposal struct

--- channel/ledger/channel.go
+++ channel/ledger/channel.go
@@ -15,3 +15,20 @@ type LedgerChannel struct {
 
 	consensus state.SignedState
 }
+
+
+type Proposal struct {
+	turnNum uint64
+	add     *struct {
+		amount types.Funds
+		vId    types.Destination
+		left   types.Destination
+		right  types.Destination
+	}
+	remove *struct {
+		vId        types.Destination
+		amountPaid types.Funds
+	}
+	signature state.Signature
+}
+
