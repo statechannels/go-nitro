f74f547a69a418b370f4d4837d9ead358122908d
Mike Kerzhner <mikerzhner@gmail.com>
2022-03-28 11:58:21 -0600

    add doc headers

--- channel/consensus_channel/follower_channel.go
+++ channel/consensus_channel/follower_channel.go
@@ -15,6 +15,7 @@ type FollowerChannel struct {
 	consensusChannel
 }
 
+// NewFollowerChannel constructs a new FollowerChannel
 func NewFollowerChannel(fp state.FixedPart, outcome LedgerOutcome, signatures [2]state.Signature) (FollowerChannel, error) {
 	channel, err := newConsensusChannel(fp, leader, outcome, signatures)
 
@@ -31,6 +32,8 @@ func (c *FollowerChannel) Includes(g Guarantee) bool {
 	return c.current.Outcome.includes(g)
 }
 
+// SignNextProposal inspects whether the expected proposal matches the first proposal in
+// the queue. If so, the proposal is removed from the queue and integrated into the channel state
 func (c *FollowerChannel) SignNextProposal(expectedUpdate interface{}, pk []byte) error {
 	if len(c.proposalQueue) == 0 {
 		return ErrNoProposals
