a1aa301a47f6ae6b0efd9ce757f4be655d1752e3
Alex Gap <alex.gap@gmail.com>
2022-03-28 14:13:15 -0700

    update tests to be more thorough

--- channel/consensus_channel/follower_channel_test.go
+++ channel/consensus_channel/follower_channel_test.go
@@ -3,10 +3,10 @@ package consensus_channel
 import (
 	"errors"
 	"math/big"
+	"reflect"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/google/go-cmp/cmp"
 	"github.com/statechannels/go-nitro/channel/state"
 	"github.com/statechannels/go-nitro/internal/testdata"
 	"github.com/statechannels/go-nitro/types"
@@ -77,6 +77,24 @@ func add(turnNum, amount uint64, vId, left, right types.Destination) Add {
 	}
 }
 
+// createSignedProposal generates a signed proposal given the vars, proposal fixed parts and private key
+// The vars passed in are NOT mutated!
+func createSignedProposal(vars Vars, proposal Add, fp state.FixedPart, pk []byte) SignedProposal {
+	proposalVars := Vars{TurnNum: vars.TurnNum, Outcome: vars.Outcome.clone()}
+	_ = proposalVars.Add(proposal)
+
+	state := proposalVars.asState(fp)
+	sig, _ := state.Sign(pk)
+
+	signedProposal := SignedProposal{
+		Proposal:  proposal,
+		Signature: sig,
+	}
+
+	return signedProposal
+
+}
+
 func TestReceive(t *testing.T) {
 	initialVars := Vars{Outcome: ledgerOutcome(), TurnNum: 0}
 	aliceSig, _ := initialVars.asState(fp()).Sign(testdata.Actors.Alice.PrivateKey)
@@ -88,12 +106,7 @@ func TestReceive(t *testing.T) {
 		t.Fatal("unable to construct channel")
 	}
 
-	proposalVars := Vars{Outcome: ledgerOutcome(), TurnNum: 0}
 	proposal := add(1, vAmount, targetChannel, alice, bob)
-	err = proposalVars.Add(proposal)
-	if err != nil {
-		t.Fatal("unable to add proposal")
-	}
 
 	// Create a proposal with an incorrect signature
 	badSigProposal := SignedProposal{bobsSig, proposal}
@@ -102,16 +115,9 @@ func TestReceive(t *testing.T) {
 		t.Fatalf("expected %v, but got %v", ErrInvalidProposalSignature, err)
 	}
 
-	signature, err := channel.sign(proposalVars, testdata.Actors.Alice.PrivateKey)
-	if err != nil {
-		t.Fatalf("unable to sign proposal: %v", err)
-	}
-	signedProposal := SignedProposal{
-		Proposal:  proposal,
-		Signature: signature,
-	}
+	valid := createSignedProposal(initialVars, proposal, fp(), testdata.Actors.Alice.PrivateKey)
 
-	err = channel.Receive(signedProposal)
+	err = channel.Receive(valid)
 	if err != nil {
 		t.Fatalf("unable to receive proposal: %v", err)
 	}
@@ -120,12 +126,37 @@ func TestReceive(t *testing.T) {
 		t.Fatalf("Expected only one proposal in queue")
 	}
 	queued := channel.proposalQueue[0]
-	if !cmp.Equal(queued.Proposal, proposal) {
+	if !reflect.DeepEqual(queued.Proposal, proposal) {
 		t.Fatalf("Expected proposal to be queued")
 	}
 
-	// If we call receive again, it should fail due to the turn num already being used
-	err = channel.Receive(signedProposal)
+	// Generate a second proposal
+	latestProposed, _ := channel.latestProposedVars()
+	secondProposal := add(2, vAmount, types.Destination{3}, alice, bob)
+	anotherValid := createSignedProposal(latestProposed, secondProposal, fp(), testdata.Actors.Alice.PrivateKey)
+	err = channel.Receive(anotherValid)
+	if err != nil {
+		t.Fatalf("unable to receive proposal: %v", err)
+	}
+
+	if len(channel.proposalQueue) != 2 {
+		t.Fatalf("Expected both proposals in the queue")
+	}
+	queued = channel.proposalQueue[1]
+	if !reflect.DeepEqual(queued.Proposal, secondProposal) {
+		t.Fatalf("Expect the latest proposal to be the last in the queue")
+	}
+
+	// Check that receive rejects a stale proposal
+	stale := createSignedProposal(Vars{TurnNum: 0, Outcome: ledgerOutcome()}, proposal, fp(), testdata.Actors.Alice.PrivateKey)
+	err = channel.Receive(stale)
+	if !errors.Is(ErrInvalidTurnNum, err) {
+		t.Fatalf("expected %v, but got %v", ErrInvalidTurnNum, err)
+	}
+
+	// Check that  receive rejects a proposal too far in the future
+	tooFar := createSignedProposal(Vars{TurnNum: 10, Outcome: ledgerOutcome()}, proposal, fp(), testdata.Actors.Alice.PrivateKey)
+	err = channel.Receive(tooFar)
 	if !errors.Is(ErrInvalidTurnNum, err) {
 		t.Fatalf("expected %v, but got %v", ErrInvalidTurnNum, err)
 	}
