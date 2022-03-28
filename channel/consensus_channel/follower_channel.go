f71ba07629ddff304dcbe8dc6ac53f6fc929ee12
Mike Kerzhner <mikerzhner@gmail.com>
2022-03-28 14:56:51 -0600

    update current vars for SignNextProposal

--- channel/consensus_channel/follower_channel.go
+++ channel/consensus_channel/follower_channel.go
@@ -17,7 +17,7 @@ type FollowerChannel struct {
 
 // NewFollowerChannel constructs a new FollowerChannel
 func NewFollowerChannel(fp state.FixedPart, outcome LedgerOutcome, signatures [2]state.Signature) (FollowerChannel, error) {
-	channel, err := newConsensusChannel(fp, leader, outcome, signatures)
+	channel, err := newConsensusChannel(fp, follower, outcome, signatures)
 
 	return FollowerChannel{consensusChannel: channel}, err
 }
@@ -51,14 +51,24 @@ func (c *FollowerChannel) SignNextProposal(expectedProposal interface{}, pk []by
 		return ErrNonMatchingProposals
 	}
 
+	// vars are cloned and modified instead of modified in place to simplify recovering from error
 	vars := Vars{
 		TurnNum: c.current.TurnNum,
 		Outcome: c.current.Outcome.clone(),
 	}
-
 	err := vars.Add(p)
 	if err != nil {
 		return err
+	}
+
+	signature, err := c.sign(vars, pk)
+	if err != nil {
+		return fmt.Errorf("unable to sign state update: %f", err)
+	}
+
+	c.current = SignedVars{
+		Vars:       vars,
+		Signatures: [2]state.Signature{c.proposalQueue[0].Signature, signature},
 	}
 	c.proposalQueue = c.proposalQueue[1:]
 
