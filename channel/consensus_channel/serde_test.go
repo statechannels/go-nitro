08b992ef181ae45af996458480be23b9e50e7cfe
Alex Gap <alex.gap@gmail.com>
2022-03-30 14:02:30 -0700

    get serde test passing

--- channel/consensus_channel/serde_test.go
+++ channel/consensus_channel/serde_test.go
@@ -12,18 +12,16 @@ import (
 )
 
 func TestSerde(t *testing.T) {
-	// TODO unskip this test when we have solved the issue for persisting big.Ints
-	// https://github.com/statechannels/go-nitro/issues/439
-	t.Skip()
+
 	someGuarantee := Guarantee{
-		amount: *big.NewInt(1),
+		amount: big.NewInt(1),
 		left:   testdata.Actors.Alice.Destination(),
 		right:  testdata.Actors.Alice.Destination(),
 		target: types.Destination{99},
 	}
 	someOutcome := makeOutcome(
-		Balance{testdata.Actors.Alice.Destination(), *big.NewInt(2)},
-		Balance{testdata.Actors.Bob.Destination(), *big.NewInt(7)},
+		Balance{testdata.Actors.Alice.Destination(), big.NewInt(2)},
+		Balance{testdata.Actors.Bob.Destination(), big.NewInt(7)},
 		someGuarantee)
 
 	t.Run("Guarantee", func(t *testing.T) {
@@ -31,10 +29,9 @@ func TestSerde(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		// TODO: this expectation is not right and currently has some placeholders  / gaps
-		want := `{"Amount":{SOMETHING!},"Target":"0x6300000000000000000000000000000000000000000000000000000000000000","Left":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Right":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce"}`
+		want := `{"Amount":1,"Target":"0x6300000000000000000000000000000000000000000000000000000000000000","Left":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Right":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce"}`
 		if string(got) != want {
-			t.Fatalf("incorrect json marshalling, expected %v got %v", want, string(got))
+			t.Fatalf("incorrect json marshalling, expected %v got \n%v", want, string(got))
 		}
 	})
 
@@ -43,10 +40,10 @@ func TestSerde(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		// TODO: this expectation is not right and currently has some placeholders  / gaps
-		want := `{"AssetAddress":"0x0000000000000000000000000000000000000000","Left":{"Destination":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Amount":{SOMETHING!}},"Right":{"Destination":"0x000000000000000000000000bbb676f9cff8d242e9eac39d063848807d3d1d94","Amount":{}},"Guarantees":{"0x6300000000000000000000000000000000000000000000000000000000000000":{"Amount":{},"Target":"0x6300000000000000000000000000000000000000000000000000000000000000","Left":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Right":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce"}}}`
+
+		want := `{"AssetAddress":"0x0000000000000000000000000000000000000000","Left":{"Destination":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Amount":2},"Right":{"Destination":"0x000000000000000000000000bbb676f9cff8d242e9eac39d063848807d3d1d94","Amount":7},"Guarantees":{"0x6300000000000000000000000000000000000000000000000000000000000000":{"Amount":1,"Target":"0x6300000000000000000000000000000000000000000000000000000000000000","Left":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Right":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce"}}}`
 		if string(got) != want {
-			t.Fatalf("incorrect json marshalling, expected %v got %v", want, string(got))
+			t.Fatalf("incorrect json marshalling, expected %v\n got %v", want, string(got))
 		}
 	})
 
@@ -86,12 +83,11 @@ func TestSerde(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		// TODO: this expectation is not right and currently has some placeholders  / gaps
-		want := `{"Id":"0x0100000000000000000000000000000000000000000000000000000000000000","MyIndex":0,"FP":{"ChainId":0,"Participants":["0xaaa6628ec44a8a742987ef3a114ddfe2d4f7adce","0xbbb676f9cff8d242e9eac39d063848807d3d1d94"],"ChannelNonce":9001,"AppDefinition":"0x0000000000000000000000000000000000000000","ChallengeDuration":100},"Current":{"TurnNum":0,"Outcome":{"AssetAddress":"0x0000000000000000000000000000000000000000","Left":{"Destination":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Amount":{}},"Right":{"Destination":"0x000000000000000000000000bbb676f9cff8d242e9eac39d063848807d3d1d94","Amount":{}},"Guarantees":{"0x6300000000000000000000000000000000000000000000000000000000000000":{"Amount":{},"Target":"0x6300000000000000000000000000000000000000000000000000000000000000","Left":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Right":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce"}}},"Signatures":[{"R":"cEs6/MbnAhAsoa8/c887N/MAfzaMQOi4HKgjpldAoFM=","S":"FAQK1MWY27BVpQQwFCoTUY4TMLedJO7Yb8vf8aepVYk=","V":0},{"R":"FAQK1MWY27BVpQQwFCoTUY4TMLedJO7Yb8vf8aepVYk=","S":"cEs6/MbnAhAsoa8/c887N/MAfzaMQOi4HKgjpldAoFM=","V":0}]},"ProposalQueue":[{"R":"FAQK1MWY27BVpQQwFCoTUY4TMLedJO7Yb8vf8aepVYk=","S":"cEs6/MbnAhAsoa8/c887N/MAfzaMQOi4HKgjpldAoFM=","V":0,"Proposal":{"Amount":{SOMETHING},"Target":"0x0300000000000000000000000000000000000000000000000000000000000000","Left":"0x0400000000000000000000000000000000000000000000000000000000000000","Right":"0x0500000000000000000000000000000000000000000000000000000000000000"}}]}`
+		want := `{"Id":"0x0100000000000000000000000000000000000000000000000000000000000000","MyIndex":0,"FP":{"ChainId":0,"Participants":["0xaaa6628ec44a8a742987ef3a114ddfe2d4f7adce","0xbbb676f9cff8d242e9eac39d063848807d3d1d94"],"ChannelNonce":9001,"AppDefinition":"0x0000000000000000000000000000000000000000","ChallengeDuration":100},"Current":{"TurnNum":0,"Outcome":{"AssetAddress":"0x0000000000000000000000000000000000000000","Left":{"Destination":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Amount":2},"Right":{"Destination":"0x000000000000000000000000bbb676f9cff8d242e9eac39d063848807d3d1d94","Amount":7},"Guarantees":{"0x6300000000000000000000000000000000000000000000000000000000000000":{"Amount":1,"Target":"0x6300000000000000000000000000000000000000000000000000000000000000","Left":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce","Right":"0x000000000000000000000000aaa6628ec44a8a742987ef3a114ddfe2d4f7adce"}}},"Signatures":[{"R":"cEs6/MbnAhAsoa8/c887N/MAfzaMQOi4HKgjpldAoFM=","S":"FAQK1MWY27BVpQQwFCoTUY4TMLedJO7Yb8vf8aepVYk=","V":0},{"R":"FAQK1MWY27BVpQQwFCoTUY4TMLedJO7Yb8vf8aepVYk=","S":"cEs6/MbnAhAsoa8/c887N/MAfzaMQOi4HKgjpldAoFM=","V":0}]},"ProposalQueue":[{"R":"FAQK1MWY27BVpQQwFCoTUY4TMLedJO7Yb8vf8aepVYk=","S":"cEs6/MbnAhAsoa8/c887N/MAfzaMQOi4HKgjpldAoFM=","V":0,"Proposal":{"Amount":2,"Target":"0x0300000000000000000000000000000000000000000000000000000000000000","Left":"0x0400000000000000000000000000000000000000000000000000000000000000","Right":"0x0500000000000000000000000000000000000000000000000000000000000000"}}]}`
 
 		if string(got) != want {
 
-			t.Fatalf("incorrect json marshalling, expected %v got %v", want, string(got))
+			t.Fatalf("incorrect json marshalling, expected %v got \n%v", want, string(got))
 		}
 	})
 
