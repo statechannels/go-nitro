043214f7c1ad4791f663f9bd827a7e52695ac27a
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-04-01 12:23:03 -0300

    clarify usage

--- internal/testdata/doc.go
+++ internal/testdata/doc.go
@@ -1,3 +1,9 @@
-// package testdata (TestData) provides literals and utility functions for generating objects-of-concern
+// package testdata provides literals and utility functions for generating objects-of-concern
 // (objectives, channels, user-agents, etc) for other test packages.
+//
+// testdata should NOT be imported by implementation packages, including the x_test.go files
+// of implementation packages.
+//
+// See github.com/statechannels/go-nitro/issues/242 and github.com/statechannels/go-nitro/issues/438
+// for further detail
 package testdata
