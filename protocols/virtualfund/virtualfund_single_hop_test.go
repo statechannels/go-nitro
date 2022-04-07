0fc762e3bce9d488eb4b91fdb705ed831b451259
Your Name <andrew.gord.stewart@gmail.com>
2022-04-07 16:20:22 -0700

    start a new test

---
+++ protocols/virtualfund/virtualfund_single_hop_test.go
@@ -0,0 +1,44 @@
+package virtualfund
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/statechannels/go-nitro/channel/state"
+	"github.com/statechannels/go-nitro/channel/state/outcome"
+	"github.com/statechannels/go-nitro/types"
+)
+
+func TestSingleHopVirtualFundNew(t *testing.T) {
+	/////////////////////
+	// VIRTUAL CHANNEL //
+	/////////////////////
+
+	// Virtual Channel
+	vPreFund := state.State{
+		ChainId:           big.NewInt(9001),
+		Participants:      []types.Address{alice.address, p1.address, bob.address}, // A single hop virtual channel
+		ChannelNonce:      big.NewInt(0),
+		AppDefinition:     types.Address{},
+		ChallengeDuration: big.NewInt(45),
+		AppData:           []byte{},
+		Outcome: outcome.Exit{outcome.SingleAssetExit{
+			Allocations: outcome.Allocations{
+				outcome.Allocation{
+					Destination: alice.destination,
+					Amount:      big.NewInt(6),
+				},
+				outcome.Allocation{
+					Destination: bob.destination,
+					Amount:      big.NewInt(4),
+				},
+			},
+		}},
+		TurnNum: 0,
+		IsFinal: false,
+	}
+	vPostFund := vPreFund.Clone()
+	vPostFund.TurnNum = 1
+
+
+}
