0d4fa206d4cbc24445124e18ef63693bca0699d7
Alex Gap <alex.gap@gmail.com>
2022-04-12 10:48:58 -0700

    add a little more godoc

--- internal/testhelpers/testhelpers.go
+++ internal/testhelpers/testhelpers.go
@@ -17,7 +17,8 @@ const makeRed = "\033[31m"
 // as it is intended to be used at the end of a string, it also adds two linebreaks
 const makeBlack = "\033[39m\n\n"
 
-// Assert fails the test if the condition is false.
+// Assert fails the test immediately if the condition is false.
+// If the assertion fails the formatted message will be output to the console.
 func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
@@ -26,7 +27,8 @@ func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	}
 }
 
-// Ok fails the test if an err is not nil.
+// Ok fails the test immediately if an err is not nil.
+// If the error is not nil the message containing the error will be outputted to the console
 func Ok(tb testing.TB, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
@@ -35,11 +37,12 @@ func Ok(tb testing.TB, err error) {
 	}
 }
 
-// Equals fails the test if exp is not equal to act.
-func Equals(tb testing.TB, exp, act interface{}) {
-	if !reflect.DeepEqual(exp, act) {
+// Equals fails the test is want is not deeply equal to got.
+// Equals uses reflect.DeepEqual to compare the two values.
+func Equals(tb testing.TB, want, got interface{}) {
+	if !reflect.DeepEqual(want, got) {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf(makeRed+"%s:%d:\n\n\texp: %#v\n\n\tgot: %#v"+makeBlack, filepath.Base(file), line, exp, act)
+		fmt.Printf(makeRed+"%s:%d:\n\n\texp: %#v\n\n\tgot: %#v"+makeBlack, filepath.Base(file), line, want, got)
 		tb.FailNow()
 	}
 }
