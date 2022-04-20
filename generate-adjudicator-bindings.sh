6529033e537beb2637ad3b05901e681f90a7146b
George C. Knee <georgeknee@googlemail.com>
2022-04-20 13:19:05 +0100

    Fix action (#560)
    * make trivial change to contracts
    
    * make script executable
    
    * regenerate lockfile
    
    * regenerate binding
    
    * use a specific version of geth
    
    * remove node_modules and npm install
    
    * move oz from dev to prod dep
    
    *  remove file path information from contract binary
    
    * rename action

--- generate-adjudicator-bindings.sh
+++ generate-adjudicator-bindings.sh
@@ -1,6 +1,6 @@
 set -e
 cd nitro-protocol
-solc @statechannels/exit-format/=$(pwd)/node_modules/@statechannels/exit-format/ @openzeppelin/contracts/=$(pwd)/node_modules/@openzeppelin/contracts/ $(pwd)/contracts/NitroAdjudicator.sol --optimize --bin --abi -o tmp-build
+solc --base-path $(pwd) @statechannels/exit-format/=node_modules/@statechannels/exit-format/ @openzeppelin/contracts/=node_modules/@openzeppelin/contracts/ contracts/NitroAdjudicator.sol --optimize --bin --abi -o tmp-build
 abigen --abi=$(pwd)/tmp-build/NitroAdjudicator.abi --bin=$(pwd)/tmp-build/NitroAdjudicator.bin --pkg=NitroAdjudicator --out=$(pwd)/../client/engine/chainservice/adjudicator/NitroAdjudicator.go
 rm -rf $(pwd)/tmp-build
 echo "Deleted tmp-build directory."
