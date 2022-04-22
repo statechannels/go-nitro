1052ccef947a8a613d658164a603b5f5d1962d48
geoknee <georgeknee@googlemail.com>
2022-04-22 14:49:58 +0100

    break store into embedded interfaces

--- client/engine/store/store.go
+++ client/engine/store/store.go
@@ -18,19 +18,40 @@ type Store interface {
 	GetChannelSecretKey() *[]byte // Get a pointer to a secret key for signing channel updates
 	GetAddress() *types.Address   // Get the (Ethereum) address associated with the ChannelSecretKey
 
-	GetObjectiveById(protocols.ObjectiveId) (protocols.Objective, error)          // Read an existing objective
-	GetObjectiveByChannelId(types.Destination) (obj protocols.Objective, ok bool) // Get the objective that currently owns the channel with the supplied ChannelId
-	SetObjective(protocols.Objective) error                                       // Write an objective
-	GetTwoPartyLedger(firstParty types.Address, secondParty types.Address) (channel *channel.TwoPartyLedger, ok bool)
-	GetChannelById(id types.Destination) (c *channel.Channel, ok bool)
-	SetChannel(*channel.Channel) error
+	ObjectiveGetter
+	ObjectiveSetter
 
+	ChannelGetter
+	ChannelSetter
 	ReleaseChannelFromOwnership(types.Destination) // Release channel from being owned by any objective
 
-	ConsensusChannelStore
+	ConsensusChannelGetter
+	ConsensusChannelSetter
+	GetTwoPartyLedger(firstParty types.Address, secondParty types.Address) (channel *channel.TwoPartyLedger, ok bool) // deprecated
+
 }
 
-type ConsensusChannelStore interface {
+type ObjectiveGetter interface {
+	GetObjectiveById(protocols.ObjectiveId) (protocols.Objective, error)          // Read an existing objective
+	GetObjectiveByChannelId(types.Destination) (obj protocols.Objective, ok bool) // Get the objective that currently owns the channel with the supplied ChannelId
+
+}
+
+type ObjectiveSetter interface {
+	SetObjective(protocols.Objective) error // Write an objective
+}
+
+type ChannelGetter interface {
+	GetChannelById(id types.Destination) (c *channel.Channel, ok bool)
+}
+
+type ChannelSetter interface {
+	SetChannel(*channel.Channel) error
+}
+
+type ConsensusChannelGetter interface {
 	GetConsensusChannel(counterparty types.Address) (channel *consensus_channel.ConsensusChannel, ok bool)
+}
+type ConsensusChannelSetter interface {
 	SetConsensusChannel(*consensus_channel.ConsensusChannel) error
 }
