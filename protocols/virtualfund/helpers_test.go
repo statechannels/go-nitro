f4415b269ee31b809608099ee4e4dbe2c6b17fbb
Mike Kerzhner <mikerzhner@gmail.com>
2022-04-22 07:58:53 -0600

    Refactor duplicated logic in helpers_test

--- protocols/virtualfund/helpers_test.go
+++ protocols/virtualfund/helpers_test.go
@@ -9,29 +9,33 @@ import (
 	"github.com/statechannels/go-nitro/types"
 )
 
-func fixedPart(left, right testactors.Actor) state.FixedPart {
-	return state.FixedPart{
+// prepareConsensusChannel prepares a consensus channel with a consensus outcome
+//  - allocating 6 to left
+//  - allocating 4 to right
+//  - including the given guarantees
+func prepareConsensusChannel(role uint, left, right testactors.Actor, guarantees ...consensus_channel.Guarantee) *consensus_channel.ConsensusChannel {
+	return prepareConsensusChannelHelper(role, left, right, 6, 4, 1, guarantees...)
+}
+
+func consensusStateSignatures(left, right testactors.Actor, guarantees ...consensus_channel.Guarantee) [2]state.Signature {
+	return prepareConsensusChannelHelper(0, left, right, 0, 0, 2, guarantees...).Signatures()
+}
+
+func prepareConsensusChannelHelper(role uint, left, right testactors.Actor, leftBalance, rightBalance, turnNum int, guarantees ...consensus_channel.Guarantee) *consensus_channel.ConsensusChannel {
+	fp := state.FixedPart{
 		ChainId:           big.NewInt(9001),
 		Participants:      []types.Address{left.Address, right.Address},
 		ChannelNonce:      big.NewInt(0),
 		AppDefinition:     types.Address{},
 		ChallengeDuration: big.NewInt(45),
 	}
-}
 
-// prepareConsensusChannel prepares a consensus channel with a consensus outcome
-//  - allocating 6 to left
-//  - allocating 4 to right
-//  - including the given guarantees
-func prepareConsensusChannel(role uint, left, right testactors.Actor, guarantees ...consensus_channel.Guarantee) *consensus_channel.ConsensusChannel {
-	fp := fixedPart(left, right)
-
-	leftBal := consensus_channel.NewBalance(left.Destination(), big.NewInt(6))
-	rightBal := consensus_channel.NewBalance(right.Destination(), big.NewInt(4))
+	leftBal := consensus_channel.NewBalance(left.Destination(), big.NewInt(int64(leftBalance)))
+	rightBal := consensus_channel.NewBalance(right.Destination(), big.NewInt(int64(rightBalance)))
 
 	lo := *consensus_channel.NewLedgerOutcome(types.Address{}, leftBal, rightBal, guarantees)
 
-	signedVars := consensus_channel.SignedVars{Vars: consensus_channel.Vars{Outcome: lo, TurnNum: 1}}
+	signedVars := consensus_channel.SignedVars{Vars: consensus_channel.Vars{Outcome: lo, TurnNum: uint64(turnNum)}}
 	leftSig, err := signedVars.Vars.AsState(fp).Sign(left.PrivateKey)
 	if err != nil {
 		panic(err)
@@ -45,37 +49,13 @@ func prepareConsensusChannel(role uint, left, right testactors.Actor, guarantees
 	var cc consensus_channel.ConsensusChannel
 
 	if role == 0 {
-		cc, err = consensus_channel.NewLeaderChannel(fp, 1, lo, sigs)
+		cc, err = consensus_channel.NewLeaderChannel(fp, uint64(turnNum), lo, sigs)
 	} else {
-		cc, err = consensus_channel.NewFollowerChannel(fp, 1, lo, sigs)
+		cc, err = consensus_channel.NewFollowerChannel(fp, uint64(turnNum), lo, sigs)
 	}
 	if err != nil {
 		panic(err)
 	}
 
 	return &cc
-}
-
-func consensusStateSignatures(left, right testactors.Actor, guarantees ...consensus_channel.Guarantee) [2]state.Signature {
-	fp := fixedPart(left, right)
-
-	leftBal := consensus_channel.NewBalance(left.Destination(), big.NewInt(0))
-	rightBal := consensus_channel.NewBalance(right.Destination(), big.NewInt(0))
-
-	lo := *consensus_channel.NewLedgerOutcome(types.Address{}, leftBal, rightBal, guarantees)
-
-	signedVars := consensus_channel.SignedVars{Vars: consensus_channel.Vars{Outcome: lo, TurnNum: 2}}
-
-	st := signedVars.Vars.AsState(fp)
-
-	leftSig, err := st.Sign(left.PrivateKey)
-	if err != nil {
-		panic(err)
-	}
-	rightSig, err := st.Sign(right.PrivateKey)
-	if err != nil {
-		panic(err)
-	}
-
-	return [2]state.Signature{leftSig, rightSig}
 }
