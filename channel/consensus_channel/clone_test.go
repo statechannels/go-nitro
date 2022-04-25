11efe4dbde237849ae425628129e56ef1b2d7296
geoknee <georgeknee@googlemail.com>
2022-04-25 14:52:39 +0100

    fix clone test

--- channel/consensus_channel/clone_test.go
+++ channel/consensus_channel/clone_test.go
@@ -4,31 +4,42 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/google/go-cmp/cmp"
-	"github.com/statechannels/go-nitro/channel"
 	"github.com/statechannels/go-nitro/channel/state"
+	"github.com/statechannels/go-nitro/types"
 )
 
 func TestClone(t *testing.T) {
 
-	cc, _ := newConsensusChannel(state.TestState.FixedPart(), 0, 1, ledgerOutcome(), [2]state.Signature{
-		{
-			R: common.Hex2Bytes(`704b3afcc6e702102ca1af3f73cf3b37f3007f368c40e8b81ca823a65740a053`),
-			S: common.Hex2Bytes(`14040ad4c598dbb055a50430142a13518e1330b79d24eed86fcbdff1a7a95589`),
-			V: byte(27),
-		},
-		{
-			R: common.Hex2Bytes(`704b3afcc6e702102ca1af3f73cf3b37f3007f368c40e8b81ca823a65740a053`),
-			S: common.Hex2Bytes(`14040ad4c598dbb055a50430142a13518e1330b79d24eed86fcbdff1a7a95589`),
-			V: byte(27),
-		},
-	})
+	outcome := makeOutcome(
+		allocation(alice, aBal),
+		allocation(bob, bBal),
+		guarantee(vAmount, types.Destination{7}, alice, bob),
+	)
+
+	initialVars := Vars{Outcome: outcome, TurnNum: 0}
+	aliceSig, _ := initialVars.AsState(fp()).Sign(alice.PrivateKey)
+	bobsSig, _ := initialVars.AsState(fp()).Sign(bob.PrivateKey)
+	sigs := [2]state.Signature{aliceSig, bobsSig}
+
+	cc, err := newConsensusChannel(fp(), Leader, 0, outcome, sigs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	clone := cc.Clone()
 
 	compareConsensusChannels := func(a, b ConsensusChannel) string {
-		return cmp.Diff(&a, &b, cmp.AllowUnexported(ConsensusChannel{}, channel.Channel{}, big.Int{}, state.SignedState{}))
+		return cmp.Diff(&a, &b,
+			cmp.AllowUnexported(
+				ConsensusChannel{},
+				Vars{},
+				LedgerOutcome{},
+				Guarantee{},
+				Balance{},
+				big.Int{},
+				state.SignedState{}))
 	}
 
 	if diff := compareConsensusChannels(cc, *clone); diff != "" {
