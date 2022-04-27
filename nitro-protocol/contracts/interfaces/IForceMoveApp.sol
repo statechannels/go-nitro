27bd89e7990c0af2cc324d5eda5c165e6251c322
actpohabtNS <actpohabt.ns@gmail.com>
2022-04-27 16:57:28 +0300

    update IForceMoveApp, TrivialApp

--- nitro-protocol/contracts/interfaces/IForceMoveApp.sol
+++ nitro-protocol/contracts/interfaces/IForceMoveApp.sol
@@ -5,21 +5,19 @@ pragma experimental ABIEncoderV2;
 import './INitroTypes.sol';
 
 /**
- * @dev The IForceMoveApp interface calls for its children to implement an application-specific validTransition function, defining the state machine of a ForceMove state channel DApp.
+ * @dev The IForceMoveApp interface calls for its children to implement an application-specific latestSupportedState function, defining the state machine of a ForceMove state channel DApp.
  */
 interface IForceMoveApp is INitroTypes {
-    
+
     /**
      * @notice Encodes application-specific rules for a particular ForceMove-compliant state channel.
      * @dev Encodes application-specific rules for a particular ForceMove-compliant state channel.
-     * @param a State being transitioned from.
-     * @param b State being transitioned to.
-     * @param nParticipants Number of participants in this state channel.
-     * @return true if the transition conforms to this application's rules, false otherwise
-     */
-    function validTransition(
-        VariablePart calldata a,
-        VariablePart calldata b,
-        uint256 nParticipants
-    ) external pure returns (bool);
+     * @param fixedPart Fixed part of the state channel.
+     * @param signedVariableParts Array of variable parts with signatures to find the latest of.
+     * @return VariablePart Latest supported by application variable part from supplied array.
+     */    
+    function latestSupportedState(
+        FixedPart calldata fixedPart,
+        SignedVariablePart[] calldata signedVariableParts
+    ) external pure returns (VariablePart memory);
 }
