f355b70f92587a47b26a67122667a58591afbbb7
geoknee <georgeknee@googlemail.com>
2022-04-28 13:57:56 +0100

    factor out openAVirtualChannel test fn

--- client_test/virtualfund_integration_test.go
+++ client_test/virtualfund_integration_test.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/statechannels/go-nitro/client"
 	"github.com/statechannels/go-nitro/client/engine/chainservice"
 	"github.com/statechannels/go-nitro/client/engine/messageservice"
 	td "github.com/statechannels/go-nitro/internal/testdata"
@@ -13,19 +14,7 @@ import (
 	"github.com/statechannels/go-nitro/types"
 )
 
-func TestVirtualFundIntegration(t *testing.T) {
-
-	// Setup logging
-	logDestination := &bytes.Buffer{}
-	t.Cleanup(flushToFileCleanupFn(logDestination, "virtualfund_client_test.log"))
-
-	chain := chainservice.NewMockChain()
-	broker := messageservice.NewBroker()
-
-	clientA, _ := setupClient(alice.PrivateKey, chain, broker, logDestination, 0)
-	clientB, _ := setupClient(bob.PrivateKey, chain, broker, logDestination, 0)
-	clientI, _ := setupClient(irene.PrivateKey, chain, broker, logDestination, 0)
-
+func openAVirtualChannel(t *testing.T, clientA client.Client, clientB client.Client, clientI client.Client) types.Destination {
 	directlyFundALedgerChannel(t, clientA, clientI)
 	directlyFundALedgerChannel(t, clientI, clientB)
 
@@ -40,10 +29,27 @@ func TestVirtualFundIntegration(t *testing.T) {
 		ChallengeDuration: big.NewInt(0),
 		Nonce:             rand.Int63(),
 	}
-	id := clientA.CreateVirtualChannel(request)
+	response := clientA.CreateVirtualChannel(request)
 
-	waitTimeForCompletedObjectiveIds(t, &clientA, defaultTimeout, id)
-	waitTimeForCompletedObjectiveIds(t, &clientB, defaultTimeout, id)
-	waitTimeForCompletedObjectiveIds(t, &clientI, defaultTimeout, id)
+	waitTimeForCompletedObjectiveIds(t, &clientA, defaultTimeout, response.Id)
+	waitTimeForCompletedObjectiveIds(t, &clientB, defaultTimeout, response.Id)
+	waitTimeForCompletedObjectiveIds(t, &clientI, defaultTimeout, response.Id)
 
+	return response.ChannelId
+
+}
+func TestVirtualFundIntegration(t *testing.T) {
+
+	// Setup logging
+	logDestination := &bytes.Buffer{}
+	t.Cleanup(flushToFileCleanupFn(logDestination, "virtualfund_client_test.log"))
+
+	chain := chainservice.NewMockChain()
+	broker := messageservice.NewBroker()
+
+	clientA, _ := setupClient(alice.PrivateKey, chain, broker, logDestination, 0)
+	clientB, _ := setupClient(bob.PrivateKey, chain, broker, logDestination, 0)
+	clientI, _ := setupClient(irene.PrivateKey, chain, broker, logDestination, 0)
+
+	openAVirtualChannel(t, clientA, clientB, clientI)
 }
