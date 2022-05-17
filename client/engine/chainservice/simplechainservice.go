cdef0ace146e80815c277b852721de7e6854c7b2
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-05-17 15:26:47 -0300

    refactor: hide implementation details of...
    `SimpleChainService`

--- client/engine/chainservice/simplechainservice.go
+++ client/engine/chainservice/simplechainservice.go
@@ -5,8 +5,8 @@ import (
 	"github.com/statechannels/go-nitro/types"
 )
 
-// SimpleChainService forwards inputted transactions to a MockChain, and passes Events straight back.
-type SimpleChainService struct {
+// simpleChainService forwards inputted transactions to a MockChain, and passes Events straight back.
+type simpleChainService struct {
 	out chan Event                      // out is the chan used to send Events to the engine
 	in  chan protocols.ChainTransaction // in is the chan used to receive Transactions from the engine
 
@@ -16,7 +16,7 @@ type SimpleChainService struct {
 
 // NewSimpleChainService returns a SimpleChainService which is listening for transactions and events.
 func NewSimpleChainService(mc *MockChain, address types.Address) ChainService {
-	mcs := SimpleChainService{}
+	mcs := simpleChainService{}
 	mcs.out = make(chan Event)
 	mcs.in = make(chan protocols.ChainTransaction)
 	mcs.chain = mc
@@ -30,24 +30,24 @@ func NewSimpleChainService(mc *MockChain, address types.Address) ChainService {
 }
 
 // Out returns the out chan but narrows the type so that external consumers may only receive on it.
-func (mcs SimpleChainService) Out() <-chan Event {
+func (mcs simpleChainService) Out() <-chan Event {
 	return mcs.out
 }
 
 // In returns the in chan but narrows the type so that external consumers may only send on it.
-func (mcs SimpleChainService) In() chan<- protocols.ChainTransaction {
+func (mcs simpleChainService) In() chan<- protocols.ChainTransaction {
 	return mcs.in
 }
 
 // forwardTransactions pipes transactions to the MockChain
-func (mcs SimpleChainService) forwardTransactions() {
+func (mcs simpleChainService) forwardTransactions() {
 	for tx := range mcs.in {
 		mcs.chain.In() <- tx
 	}
 }
 
 // forwardEvents pipes events from the MockChain
-func (mcs SimpleChainService) forwardEvents() {
+func (mcs simpleChainService) forwardEvents() {
 	for event := range mcs.chain.Out(mcs.address) {
 		mcs.out <- event
 	}
