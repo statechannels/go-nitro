fbb27f343a4be2c1d91db742dd687314a5984951
actpohabtNS <actpohabt.ns@gmail.com>
2022-05-17 13:40:53 +0300

    update CountingApp implementation using TurnTaking

--- nitro-protocol/contracts/CountingApp.sol
+++ nitro-protocol/contracts/CountingApp.sol
@@ -3,12 +3,13 @@ pragma solidity 0.7.6;
 pragma experimental ABIEncoderV2;
 
 import './interfaces/IForceMoveApp.sol';
+import './examples/signature-logic/TurnTaking.sol';
 import {ExitFormat as Outcome} from '@statechannels/exit-format/contracts/ExitFormat.sol';
 
 /**
  * @dev The CountingApp contracts complies with the ForceMoveApp interface and allows only for a simple counter to be incremented. Used for testing purposes.
  */
-contract CountingApp is IForceMoveApp {
+contract CountingApp is IForceMoveApp, TurnTaking {
     struct CountingAppData {
         uint256 counter;
     }
@@ -35,7 +36,45 @@ contract CountingApp is IForceMoveApp {
         FixedPart calldata fixedPart,
         SignedVariablePart[] calldata signedVariableParts
     ) external pure override returns (VariablePart memory) {
-        // TODO see https://github.com/statechannels/go-nitro/issues/558
+        _requireValidTurnTaking(fixedPart, signedVariableParts);
+
+        for (uint i = 1; i < signedVariableParts.length; i++) {
+            _requireIncrementedCounter(signedVariableParts[i], signedVariableParts[i-1]);
+            _requireOutcomeEquals(signedVariableParts[i], signedVariableParts[i-1]);
+        }
+
         return signedVariableParts[signedVariableParts.length - 1].variablePart;
+    }
+
+    /**
+     * @notice Checks that counter encoded in first variable part equals an incremented counter in second variable part.
+     * @dev Checks that counter encoded in first variable part equals an incremented counter in second variable part.
+     * @param b SignedVariablePart with incremented counter.
+     * @param a SignedVariablePart with counter before incrementing.
+     */
+    function _requireIncrementedCounter(
+        SignedVariablePart memory b,
+        SignedVariablePart memory a
+    ) internal pure {
+        require(
+            appData(b.variablePart.appData).counter == appData(a.variablePart.appData).counter + 1,
+            'Counter must be incremented'
+        );
+    }
+
+    /**
+     * @notice Checks that supplied signed variable parts contain the same outcome.
+     * @dev Checks that supplied signed variable parts contain the same outcome.
+     * @param a First SignedVariablePart.
+     * @param b Second SignedVariablePart.
+     */
+    function _requireOutcomeEquals(
+        SignedVariablePart memory a,
+        SignedVariablePart memory b
+    ) internal pure {
+        require(
+            Outcome.exitsEqual(a.variablePart.outcome, b.variablePart.outcome),
+            'Outcome must not change'
+        );
     }
 }
