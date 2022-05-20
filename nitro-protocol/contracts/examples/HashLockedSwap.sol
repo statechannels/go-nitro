9dc74a7d9e65445450a992cf3cddced7929a5162
nksazonov <nsazonov@openware.com>
2022-05-20 17:29:56 +0300

    upgrade HashLockedSwap to support SATP

--- nitro-protocol/contracts/examples/HashLockedSwap.sol
+++ nitro-protocol/contracts/examples/HashLockedSwap.sol
@@ -3,12 +3,13 @@ pragma solidity 0.7.6;
 pragma experimental ABIEncoderV2;
 
 import '../interfaces/IForceMoveApp.sol';
+import './examples/signature-logic/TurnTaking.sol';
 import {ExitFormat as Outcome} from '@statechannels/exit-format/contracts/ExitFormat.sol';
 
 /**
  * @dev The HashLockedSwap contract complies with the ForceMoveApp interface and implements a HashLockedSwaped payment
  */
-contract HashLockedSwap is IForceMoveApp {
+contract HashLockedSwap is IForceMoveApp, TurnTaking {
     struct AppData {
         bytes32 h;
         bytes preImage;
@@ -18,45 +19,46 @@ contract HashLockedSwap is IForceMoveApp {
         FixedPart calldata fixedPart,
         SignedVariablePart[] calldata signedVariableParts
     ) external pure override returns (VariablePart memory) {
-        // TODO see https://github.com/statechannels/go-nitro/issues/558
+        // is this the first and only swap?
+        require(signedVariableParts.length == 2, 'excess states');
+        require(signedVariableParts[1].variablePart.turnNum == 4, 'latest turn number != 4');
 
-        // // is this the first and only swap?
-        // require(b.turnNum == 4, 'b.turnNum != 4');
+        _requireValidTurnTaking(fixedPart, signedVariableParts);
 
-        // // Decode variables.
-        // // Assumptions:
-        // //  - single asset in this channel
-        // //  - two parties in this channel
-        // //  - not a "guarantee" channel (c.f. Nitro paper)
-        // Outcome.Allocation[] memory allocationsA = decode2PartyAllocation(a.outcome);
-        // Outcome.Allocation[] memory allocationsB = decode2PartyAllocation(b.outcome);
-        // bytes memory preImage = abi.decode(b.appData, (AppData)).preImage;
-        // bytes32 h = abi.decode(a.appData, (AppData)).h;
+        // Decode variables.
+        // Assumptions:
+        //  - single asset in this channel
+        //  - two parties in this channel
+        //  - not a "guarantee" channel (c.f. Nitro paper)
+        Outcome.Allocation[] memory allocationsA = decode2PartyAllocation(signedVariableParts[0].variablePart.outcome);
+        Outcome.Allocation[] memory allocationsB = decode2PartyAllocation(signedVariableParts[1].variablePart.outcome);
+        bytes memory preImage = abi.decode(signedVariableParts[1].variablePart.appData, (AppData)).preImage;
+        bytes32 h = abi.decode(signedVariableParts[0].variablePart.appData, (AppData)).h;
 
-        // // is the preimage correct?
-        // require(sha256(preImage) == h, 'Incorrect preimage');
-        // // NOTE ON GAS COSTS
-        // // The gas cost of hashing depends on the choice of hash function
-        // // and the length of the the preImage.
-        // // sha256 is twice as expensive as keccak256
-        // // https://ethereum.stackexchange.com/a/3200
-        // // But is compatible with bitcoin.
+        // is the preimage correct?
+        require(sha256(preImage) == h, 'Incorrect preimage');
+        // NOTE ON GAS COSTS
+        // The gas cost of hashing depends on the choice of hash function
+        // and the length of the the preImage.
+        // sha256 is twice as expensive as keccak256
+        // https://ethereum.stackexchange.com/a/3200
+        // But is compatible with bitcoin.
 
-        // // slots for each participant unchanged
-        // require(
-        //     allocationsA[0].destination == allocationsB[0].destination &&
-        //         allocationsA[1].destination == allocationsB[1].destination,
-        //     'destinations may not change'
-        // );
+        // slots for each participant unchanged
+        require(
+            allocationsA[0].destination == allocationsB[0].destination &&
+                allocationsA[1].destination == allocationsB[1].destination,
+            'destinations may not change'
+        );
 
-        // // was the payment made?
-        // require(
-        //     allocationsA[0].amount == allocationsB[1].amount &&
-        //         allocationsA[1].amount == allocationsB[0].amount,
-        //     'amounts must be permuted'
-        // );
+        // was the payment made?
+        require(
+            allocationsA[0].amount == allocationsB[1].amount &&
+                allocationsA[1].amount == allocationsB[0].amount,
+            'amounts must be permuted'
+        );
 
-        // return true;
+        return signedVariableParts[1].variablePart;
     }
 
     function decode2PartyAllocation(Outcome.SingleAssetExit[] memory outcome)
