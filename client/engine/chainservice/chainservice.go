b09f44ce94e56c736361123b0606a38120cf1e2b
Mike Kerzhner <mikerzhner@gmail.com>
2022-05-22 09:07:44 -0600

    Sanity check for a chain service using Simulated Backend

--- client/engine/chainservice/chainservice.go
+++ client/engine/chainservice/chainservice.go
@@ -2,8 +2,20 @@
 package chainservice // import "github.com/statechannels/go-nitro/client/chainservice"
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	ethTypes "github.com/ethereum/go-ethereum/core/types"
+
 	"github.com/statechannels/go-nitro/protocols"
 	"github.com/statechannels/go-nitro/types"
+
+	NitroAdjudicator "github.com/statechannels/go-nitro/client/engine/chainservice/adjudicator"
 )
 
 // Event dictates which methods all chain events must implement
@@ -51,4 +63,82 @@ type ChainEventHandler interface {
 type ChainService interface {
 	Out() <-chan Event
 	In() chan<- protocols.ChainTransaction
+}
+
+type EventProducer interface {
+	SubscribeFilterLogs(ctx context.Context, query ethereum.FilterQuery, ch chan<- ethTypes.Log) (ethereum.Subscription, error)
+}
+
+type ChainConnection struct {
+	in  chan protocols.ChainTransaction
+	out chan Event
+	na  *NitroAdjudicator.NitroAdjudicator
+	to  *bind.TransactOpts
+}
+
+func NewChainConnection(na *NitroAdjudicator.NitroAdjudicator, naAddress common.Address, to *bind.TransactOpts, ep EventProducer) ChainConnection {
+	cc := ChainConnection{}
+	cc.in = make(chan protocols.ChainTransaction)
+	cc.out = make(chan Event)
+	cc.na = na
+	cc.to = to
+
+	go cc.listenForTx()
+	go cc.listenForEvents(na, naAddress, to, ep)
+
+	return cc
+}
+
+func (cc ChainConnection) listenForTx() {
+	for tx := range cc.in {
+		cc.handleTx(tx)
+	}
+}
+
+func (cc ChainConnection) listenForEvents(na *NitroAdjudicator.NitroAdjudicator, naAddress common.Address, to *bind.TransactOpts, ep EventProducer) {
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{naAddress},
+	}
+	logs := make(chan ethTypes.Log)
+	sub, err := ep.SubscribeFilterLogs(context.Background(), query, logs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		select {
+		case err := <-sub.Err():
+			log.Fatal(err)
+		case vLog := <-logs:
+			fmt.Println(vLog) // pointer to event log
+
+			// send dummy event
+			event := DepositedEvent{}
+			attemptSend(cc.out, event)
+		}
+	}
+}
+
+// handleTx responds to the given tx.
+func (cc ChainConnection) handleTx(tx protocols.ChainTransaction) {
+	switch tx.Type {
+	case protocols.DepositTransactionType:
+		for address, amount := range tx.Deposit {
+			cc.to.Value = amount
+			tx, err := cc.na.Deposit(cc.to, address, tx.ChannelId, big.NewInt(0), amount)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Transaction pending: %v", tx)
+		}
+	default:
+		panic("unexpected chain transaction")
+	}
+}
+
+func (cc ChainConnection) In() chan<- protocols.ChainTransaction {
+	return cc.in
+}
+
+func (cc ChainConnection) Out(a types.Address) <-chan Event {
+	return cc.out
 }
---
+++ client/engine/chainservice/chainservice_test.go
@@ -0,0 +1,60 @@
+package chainservice
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+	NitroAdjudicator "github.com/statechannels/go-nitro/client/engine/chainservice/adjudicator"
+	"github.com/statechannels/go-nitro/protocols"
+	"github.com/statechannels/go-nitro/types"
+)
+
+func TestChainService(t *testing.T) {
+	// Setup transacting EOA
+	key, _ := crypto.GenerateKey()
+	auth := bind.NewKeyedTransactor(key)
+	auth.GasPrice = big.NewInt(10000000000)
+	address := auth.From
+	balance := new(big.Int)
+	balance.SetString("10000000000000000000", 10) // 10 eth in wei
+
+	// Setup "blockchain"
+	gAlloc := map[common.Address]core.GenesisAccount{
+		address: {Balance: balance},
+	}
+	blockGasLimit := uint64(4712388)
+	sim := backends.NewSimulatedBackend(gAlloc, blockGasLimit)
+
+	// Deploy Adjudicator
+	naAddress, _, na, err := NitroAdjudicator.DeployNitroAdjudicator(auth, sim)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sim.Commit()
+
+	cc := NewChainConnection(na, naAddress, auth, sim)
+
+	// Prepare test data to trigger MockChainService
+	testDeposit := types.Funds{
+		common.HexToAddress("0x00"): big.NewInt(1),
+	}
+	testTx := protocols.ChainTransaction{
+		ChannelId: types.Destination(common.HexToHash(`4ebd366d014a173765ba1e50f284c179ade31f20441bec41664712aac6cc461d`)),
+		Deposit:   testDeposit,
+		Type:      protocols.DepositTransactionType,
+	}
+
+	cc.in <- testTx
+
+	sim.Commit()
+
+	event := <-cc.out
+	fmt.Println(event)
+}
