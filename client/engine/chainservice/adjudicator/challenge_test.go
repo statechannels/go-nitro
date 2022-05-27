71900a10e0e3ed536d068596919b355c630f71ac
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-05-27 11:20:01 -0300

    set chainID for simulated backend...
    dropping usage of deprecated `bind.NewKeyedTransactor`

--- client/engine/chainservice/adjudicator/challenge_test.go
+++ client/engine/chainservice/adjudicator/challenge_test.go
@@ -74,7 +74,7 @@ func TestChallenge(t *testing.T) {
 
 	// Setup transacting EOA
 	key, _ := crypto.GenerateKey()
-	auth := bind.NewKeyedTransactor(key)
+	auth, _ := bind.NewKeyedTransactorWithChainID(key, big.NewInt(1337)) // 1337 according to godoc on backends.NewSimulatedBackend
 	auth.GasPrice = big.NewInt(10000000000)
 	address := auth.From
 	balance := new(big.Int)
