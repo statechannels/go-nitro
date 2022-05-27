a50e1eaeeb7a44d5d8b89043aaf3b473f071b71b
geoknee <georgeknee@googlemail.com>
2022-05-27 14:55:22 +0100

    update tcp message service

--- client/engine/messageservice/simple-tcp/simpletcp-messageservice.go
+++ client/engine/messageservice/simple-tcp/simpletcp-messageservice.go
@@ -16,7 +16,6 @@ const (
 
 // SimpleTCPMessageService is a rudimentary message service that uses TCP to send and receive messages
 type SimpleTCPMessageService struct {
-	in  chan protocols.Message // for receiving messages from engine
 	out chan protocols.Message // for sending message to engine
 
 	peers map[types.Address]string
@@ -35,7 +34,6 @@ func NewSimpleTCPMessageService(myUrl string, peers map[types.Address]string) *S
 		panic(err)
 	}
 	h := &SimpleTCPMessageService{
-		in:    make(chan protocols.Message, 5),
 		out:   make(chan protocols.Message, 5),
 		peers: peers,
 
@@ -44,53 +42,40 @@ func NewSimpleTCPMessageService(myUrl string, peers map[types.Address]string) *S
 	}
 
 	go h.listenForIncoming()
-	go h.listenForOutgoing()
 
 	return h
 
 }
 
-// listenForOutgoing listens for any messages that should be sent out to peers and sends them
-func (s *SimpleTCPMessageService) listenForOutgoing() {
-	for {
-		select {
-		case <-s.quit:
-			return
+// Send dispatches messages
+func (s *SimpleTCPMessageService) Send(msg protocols.Message) {
+	peer, ok := s.peers[msg.To]
 
-		case m := <-s.in:
-			{
-				peer, ok := s.peers[m.To]
-
-				if !ok {
-					panic(fmt.Errorf("no peer port for %s", m.To))
-				}
-
-				raw, err := m.Serialize()
-
-				// Append the delimiter to the message to indicate the end of the message
-				raw = fmt.Sprintf("%s%c", raw, DELIMETER)
-
-				if err != nil {
-					s.panicIfRunning(err)
-					return
-				}
-
-				conn, err := net.Dial(CONN_TYPE, peer)
-				if err != nil {
-					s.panicIfRunning(err)
-					return
-				}
-				_, err = conn.Write([]byte(raw))
-				if err != nil {
-					s.panicIfRunning(err)
-					return
-				}
-				conn.Close()
-
-			}
-
-		}
+	if !ok {
+		panic(fmt.Errorf("no peer port for %s", msg.To))
 	}
+
+	raw, err := msg.Serialize()
+
+	// Append the delimiter to the message to indicate the end of the message
+	raw = fmt.Sprintf("%s%c", raw, DELIMETER)
+
+	if err != nil {
+		s.panicIfRunning(err)
+		return
+	}
+
+	conn, err := net.Dial(CONN_TYPE, peer)
+	if err != nil {
+		s.panicIfRunning(err)
+		return
+	}
+	_, err = conn.Write([]byte(raw))
+	if err != nil {
+		s.panicIfRunning(err)
+		return
+	}
+	conn.Close()
 
 }
 
@@ -134,10 +119,6 @@ func (s *SimpleTCPMessageService) panicIfRunning(err error) {
 
 func (s *SimpleTCPMessageService) Out() <-chan protocols.Message {
 	return s.out
-}
-
-func (s *SimpleTCPMessageService) In() chan<- protocols.Message {
-	return s.in
 }
 
 // Close closes the SimpleTCPMessageService
