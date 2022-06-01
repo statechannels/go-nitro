fb3bfd427ec6a5ba7d713b252e7c9f4d71da92ed
Alex Gap <alex.gap@gmail.com>
2022-06-01 14:40:22 -0700

    godoc and clean up

--- client/engine/metrics.go
+++ client/engine/metrics.go
@@ -5,9 +5,11 @@ import (
 	"time"
 
 	"github.com/statechannels/go-nitro/client/engine/store/safesync"
+	"github.com/statechannels/go-nitro/types"
 )
 
-// The API for recording metrics.
+// MetricsApi defines the API for recording metrics the engine expects.
+// This is used by the engine to record various metrics, like the duration of an objective.
 type MetricsApi interface {
 	// RecordMetric records metric information in some data store.
 	// The metricName is the name of the metric. IE: "message-count"
@@ -16,13 +18,13 @@ type MetricsApi interface {
 	RecordMetric(metricName string, value float64, additionalData map[string]string)
 }
 
-// MetricRecorder is a wrapper around a MetricsApi that helps in recording elapsed time for operations.
+// MetricRecorder is a wrapper around a MetricsApi that aids in recording duration metrics.
 type MetricRecorder struct {
 	metricsApi MetricsApi
 	operations safesync.Map[operation]
 }
 
-// operation is used to keep track of when an operation started.
+// operation is used to keep track of when an operation started internally in the MetricRecorder
 type operation struct {
 	Id             string
 	Name           string
@@ -39,25 +41,31 @@ func NewMetricRecorder(metricsApi MetricsApi) *MetricRecorder {
 }
 
 // RecordOperationDuration records the duration of some operation.
-func (r *MetricRecorder) RecordOperationDuration(operationName string, operationFunc func(), additionalData map[string]string) {
+// It does this by calling the provided operationFunc and recording the duration.
+// The metric information recorded will also contain the wallet address and start/stop timestamps
+func (r *MetricRecorder) RecordOperationDuration(metricName string, operationFunc func(), walletAddress types.Address) {
 
 	startTime := time.Now()
 	operationFunc()
 	stopTime := time.Now()
-
+	additionalData := map[string]string{"wallet": walletAddress.String()}
 	additionalData["startTime"] = fmt.Sprint(startTime.Nanosecond())
 	additionalData["stopTime"] = fmt.Sprint(stopTime.Nanosecond())
 
-	r.metricsApi.RecordMetric(operationName, float64(stopTime.Sub(startTime).Nanoseconds()), additionalData)
+	r.metricsApi.RecordMetric(metricName, float64(stopTime.Sub(startTime).Nanoseconds()), additionalData)
 
 }
 
-// MarkOperationStart is used to mark the start of some operation.
-func (r *MetricRecorder) MarkOperationStart(operationName string, operationId string, additionalData map[string]string) {
-	r.operations.Store(operationId, operation{Name: operationName, Id: operationId, StartTime: time.Now(), AdditionalData: additionalData})
+// MarkOperationStart is used to record that some operation has started.
+// MarkOperationStop can then be called when the operation is complete to record the duration of the operation.
+// The operationId is some unique identifier for the operation that can be later be passed into MarkOperationStop
+func (r *MetricRecorder) MarkOperationStart(metricName string, operationId string, walletAddress types.Address) {
+	additionalData := map[string]string{"wallet": walletAddress.String()}
+	r.operations.Store(operationId, operation{Name: metricName, Id: operationId, StartTime: time.Now(), AdditionalData: additionalData})
 }
 
-// MarkOperationStop is used to indicate the operation has now completed. This records the elapsed time for the operation using the metrics API
+// MarkOperationStop is used to indicate the operation has now completed.
+// This records the elapsed time for the operation using the metrics API
 func (r *MetricRecorder) MarkOperationStop(operationId string) {
 	operation, ok := r.operations.Load(operationId)
 	if !ok {
@@ -71,9 +79,11 @@ func (r *MetricRecorder) MarkOperationStop(operationId string) {
 	r.operations.Delete(operationId)
 }
 
-//  RecordMetric records any arbitrary metric information.
-func (r *MetricRecorder) RecordMetric(metricName string, value float64, additionalData map[string]string) {
-	r.RecordMetric(metricName, value, additionalData)
+//  RecordMetric records a float64 value for the metric specified by metricName.
+// The metric information recorded will contain a timestamp of the current time and the wallet address.
+func (r *MetricRecorder) RecordMetric(metricName string, value float64, walletAddress types.Address) {
+	additionalData := map[string]string{"wallet": walletAddress.String()}
+	r.metricsApi.RecordMetric(metricName, value, additionalData)
 }
 
 // NoOpMetricsApi implements the MetricsAPI interface and does nothing.
