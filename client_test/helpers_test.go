15e37c2021cf32baf92f73095a3e11f8a1a608b2
Mike Kerzhner <mikerzhner@gmail.com>
2022-06-06 15:27:03 -0600

    Change setupClient to accept a value chain parameter

--- client_test/helpers_test.go
+++ client_test/helpers_test.go
@@ -66,7 +66,7 @@ func waitTimeForCompletedObjectiveIds(t *testing.T, client *client.Client, timeo
 }
 
 // setupClient is a helper function that contructs a client and returns the new client and its store.
-func setupClient(pk []byte, chain *chainservice.MockChain, msgBroker messageservice.Broker, logDestination io.Writer, meanMessageDelay time.Duration) (client.Client, store.Store) {
+func setupClient(pk []byte, chain chainservice.ChainService, msgBroker messageservice.Broker, logDestination io.Writer, meanMessageDelay time.Duration) (client.Client, store.Store) {
 	myAddress := crypto.GetAddressFromSecretKeyBytes(pk)
 	messageservice := messageservice.NewTestMessageService(myAddress, msgBroker, meanMessageDelay)
 	storeA := store.NewMemStore(pk)
