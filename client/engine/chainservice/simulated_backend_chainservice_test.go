966bb7c833256418db9837914127538a14271403
Mike Kerzhner <mikerzhner@gmail.com>
2022-06-10 12:22:20 -0600

    Consistently name simulated backend test code

--- client/engine/chainservice/simulated_backend_chainservice_test.go
+++ client/engine/chainservice/simulated_backend_chainservice_test.go
@@ -10,13 +10,13 @@ import (
 	"github.com/statechannels/go-nitro/types"
 )
 
-func TestEthChainService(t *testing.T) {
+func TestSimulatedBackendChainService(t *testing.T) {
 	sim, na, naAddress, ethAccounts, err := SetupSimulatedBackend(1)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	acs := NewSimulatedBackendChaneService(sim, sim, na, naAddress, ethAccounts[0])
+	cs := NewSimulatedBackendChaneService(sim, sim, na, naAddress, ethAccounts[0])
 
 	// Prepare test data to trigger EthChainService
 	testDeposit := types.Funds{
@@ -29,9 +29,9 @@ func TestEthChainService(t *testing.T) {
 		Type:      protocols.DepositTransactionType,
 	}
 
-	out := acs.SubscribeToEvents(ethAccounts[0].From)
+	out := cs.SubscribeToEvents(ethAccounts[0].From)
 	// Submit transactiom
-	acs.SendTransaction(testTx)
+	cs.SendTransaction(testTx)
 
 	// Check that the recieved event matches the expected event
 	receivedEvent := <-out
