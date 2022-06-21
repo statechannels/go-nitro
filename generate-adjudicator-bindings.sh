beab386c4c08183d1fefa99e1ea645cbec6eaa08
geoknee <georgeknee@googlemail.com>
2022-06-21 13:30:18 +0100

    add ConsensusApp bindings

--- generate-adjudicator-bindings.sh
+++ generate-adjudicator-bindings.sh
@@ -1,6 +1,13 @@
 set -e
 cd nitro-protocol
+# NitroAdjudicator
 solc --base-path $(pwd) @statechannels/exit-format/=node_modules/@statechannels/exit-format/ @openzeppelin/contracts/=node_modules/@openzeppelin/contracts/ contracts/NitroAdjudicator.sol --optimize --bin --abi -o tmp-build
 abigen --abi=$(pwd)/tmp-build/NitroAdjudicator.abi --bin=$(pwd)/tmp-build/NitroAdjudicator.bin --pkg=NitroAdjudicator --out=$(pwd)/../client/engine/chainservice/adjudicator/NitroAdjudicator.go
+
+# TrivialApp
+solc --base-path $(pwd) @statechannels/exit-format/=node_modules/@statechannels/exit-format/ @openzeppelin/contracts/=node_modules/@openzeppelin/contracts/ contracts/ConsensusApp.sol --optimize --bin --abi --overwrite -o tmp-build 
+abigen --abi=$(pwd)/tmp-build/ConsensusApp.abi --bin=$(pwd)/tmp-build/ConsensusApp.bin --pkg=NitroAdjudicator --out=$(pwd)/../client/engine/chainservice/adjudicator/ConsensusApp.go 
+
+
 rm -rf $(pwd)/tmp-build
 echo "Deleted tmp-build directory."
