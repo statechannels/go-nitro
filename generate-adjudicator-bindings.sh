e08834add3c74dc0800b3d25d4e0fb8a3a03b5e1
George C. Knee <georgeknee@googlemail.com>
2022-06-24 12:21:48 +0100

    Update generate-adjudicator-bindings.sh

--- generate-adjudicator-bindings.sh
+++ generate-adjudicator-bindings.sh
@@ -4,7 +4,7 @@ cd nitro-protocol
 solc --base-path $(pwd) @statechannels/exit-format/=node_modules/@statechannels/exit-format/ @openzeppelin/contracts/=node_modules/@openzeppelin/contracts/ contracts/NitroAdjudicator.sol --optimize --bin --abi -o tmp-build
 abigen --abi=$(pwd)/tmp-build/NitroAdjudicator.abi --bin=$(pwd)/tmp-build/NitroAdjudicator.bin --pkg=NitroAdjudicator --out=$(pwd)/../client/engine/chainservice/adjudicator/NitroAdjudicator.go
 
-# TrivialApp
+# ConsensusApp
 solc --base-path $(pwd) @statechannels/exit-format/=node_modules/@statechannels/exit-format/ @openzeppelin/contracts/=node_modules/@openzeppelin/contracts/ contracts/ConsensusApp.sol --optimize --bin --abi --overwrite -o tmp-build 
 abigen --abi=$(pwd)/tmp-build/ConsensusApp.abi --bin=$(pwd)/tmp-build/ConsensusApp.bin --pkg=ConsensusApp --out=$(pwd)/../client/engine/chainservice/consensusapp/ConsensusApp.go 
 
