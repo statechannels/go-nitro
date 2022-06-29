9e0129274e163e30e7955c539188cf736e6e53e3
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-06-29 11:46:02 -0300

    add specific err type for in-use ledger channel

--- protocols/directdefund/directdefund.go
+++ protocols/directdefund/directdefund.go
@@ -26,6 +26,7 @@ const ObjectivePrefix = "DirectDefunding-"
 var (
 	ErrChannelUpdateInProgress = errors.New("can only defund a channel when the latest state is supported or when the channel has a final state")
 	ErrNoFinalState            = errors.New("cannot spawn direct defund objective without a final state")
+	ErrNotEmpty                = errors.New("ledger channel has running guarantees")
 )
 
 // Objective is a cache of data computed by reading from the store. It stores (potentially) infinite data
@@ -73,7 +74,7 @@ func NewObjective(
 	}
 
 	if len(cc.FundingTargets()) != 0 {
-		return Objective{}, fmt.Errorf("ledger channel has running guarantees")
+		return Objective{}, ErrNotEmpty
 	}
 
 	c, err := CreateChannelFromConsensusChannel(*cc)
