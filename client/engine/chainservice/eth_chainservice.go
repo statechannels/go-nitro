baf575f8c57245c8d86435af40157d7270243692
Mike Kerzhner <mikerzhner@gmail.com>
2022-07-11 17:09:49 -0600

    Log errors in listenForLogEvents instead of panicking

--- client/engine/chainservice/eth_chainservice.go
+++ client/engine/chainservice/eth_chainservice.go
@@ -137,13 +137,14 @@ func (ecs *EthChainService) listenForLogEvents(sub ethereum.Subscription, logs c
 	for {
 		select {
 		case err := <-sub.Err():
-			log.Fatal(err)
+			// TODO should we try resubscribing to chain events
+			ecs.logger.Printf("event subscription error: %v", err)
 		case chainEvent := <-logs:
 			switch chainEvent.Topics[0] {
 			case depositedTopic:
 				nad, err := ecs.na.ParseDeposited(chainEvent)
 				if err != nil {
-					log.Fatal(err)
+					ecs.logger.Printf("error in ParseDeposited: %v", err)
 				}
 
 				event := NewDepositedEvent(nad.Destination, chainEvent.BlockNumber, nad.Asset, nad.AmountDeposited, nad.DestinationHoldings)
@@ -151,30 +152,33 @@ func (ecs *EthChainService) listenForLogEvents(sub ethereum.Subscription, logs c
 			case allocationUpdatedTopic:
 				au, err := ecs.na.ParseAllocationUpdated(chainEvent)
 				if err != nil {
-					panic(err)
+					ecs.logger.Printf("error in ParseAllocationUpdated: %v", err)
 				}
 
 				tx, pending, err := ecs.chain.TransactionByHash(context.Background(), chainEvent.TxHash)
-				if pending || err != nil {
-					panic("Expected transacion to be part of the chain")
+				if pending {
+					ecs.logger.Printf("Expected transacion to be part of the chain, but the transaction is pending")
+				}
+				if err != nil {
+					ecs.logger.Printf("error in TransactoinByHash: %v", err)
 				}
 
 				assetAddress, amount, err := getChainHolding(ecs.na, tx, au)
 				if err != nil {
-					panic(err)
+					ecs.logger.Printf("error in getChainHoldings: %v", err)
 				}
 				event := NewAllocationUpdatedEvent(au.ChannelId, chainEvent.BlockNumber, assetAddress, amount)
 				ecs.broadcast(event)
 			case concludedTopic:
 				ce, err := ecs.na.ParseConcluded(chainEvent)
 				if err != nil {
-					panic(err)
+					ecs.logger.Printf("error in ParseConcluded: %v", err)
 				}
 
 				event := ConcludedEvent{commonEvent: commonEvent{channelID: ce.ChannelId, BlockNum: chainEvent.BlockNumber}}
 				ecs.broadcast(event)
 			default:
-				panic("Unknown chain event")
+				ecs.logger.Printf("Unknown chain event")
 			}
 		}
 	}
