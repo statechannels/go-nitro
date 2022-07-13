5b891eb3e1ceea2aa13cef5c8ef00b079a182a30
geoknee <georgeknee@googlemail.com>
2022-07-13 15:10:10 +0100

    updated CountingApp to use StrictTurnTaking and recoveredVariableParts

--- nitro-protocol/contracts/CountingApp.sol
+++ nitro-protocol/contracts/CountingApp.sol
@@ -3,11 +3,11 @@ pragma solidity 0.7.6;
 pragma experimental ABIEncoderV2;
 
 import {ExitFormat as Outcome} from '@statechannels/exit-format/contracts/ExitFormat.sol';
-import {ShortcuttingTurnTaking} from './libraries/signature-logic/ShortcuttingTurnTaking.sol';
+import {StrictTurnTaking} from './libraries/signature-logic/StrictTurnTaking.sol';
 import './interfaces/IForceMoveApp.sol';
 
 /**
- * @dev The CountingApp contract complies with the ForceMoveApp interface and shortcutting turn taking logic and allows only for a simple counter to be incremented. Used for testing purposes.
+ * @dev The CountingApp contract complies with the ForceMoveApp interface and strict turn taking logic and allows only for a simple counter to be incremented. Used for testing purposes.
  */
 contract CountingApp is IForceMoveApp {
     struct CountingAppData {
@@ -28,32 +28,32 @@ contract CountingApp is IForceMoveApp {
      * @notice Encodes application-specific rules for a particular ForceMove-compliant state channel.
      * @dev Encodes application-specific rules for a particular ForceMove-compliant state channel.
      * @param fixedPart Fixed part of the state channel.
-     * @param signedVariableParts Array of variable parts to find the latest of.
+     * @param recoveredVariableParts Array of variable parts to find the latest of.
      * @return VariablePart Latest supported by application variable part from supplied array.
      */    
     function latestSupportedState(
         FixedPart calldata fixedPart,
-        SignedVariablePart[] calldata signedVariableParts
+        RecoveredVariablePart[] calldata recoveredVariableParts
     ) external pure override returns (VariablePart memory) {
-        ShortcuttingTurnTaking.requireValidTurnTaking(fixedPart, signedVariableParts);
+        StrictTurnTaking.requireValidTurnTaking(fixedPart, recoveredVariableParts);
 
-        for (uint i = 1; i < signedVariableParts.length; i++) {
-            _requireIncrementedCounter(signedVariableParts[i], signedVariableParts[i-1]);
-            _requireEqualOutcomes(signedVariableParts[i], signedVariableParts[i-1]);
+        for (uint i = 1; i < recoveredVariableParts.length; i++) {
+            _requireIncrementedCounter(recoveredVariableParts[i], recoveredVariableParts[i-1]);
+            _requireEqualOutcomes(recoveredVariableParts[i], recoveredVariableParts[i-1]);
         }
 
-        return signedVariableParts[signedVariableParts.length - 1].variablePart;
+        return recoveredVariableParts[recoveredVariableParts.length - 1].variablePart;
     }
 
     /**
      * @notice Checks that counter encoded in first variable part equals an incremented counter in second variable part.
      * @dev Checks that counter encoded in first variable part equals an incremented counter in second variable part.
-     * @param b SignedVariablePart with incremented counter.
-     * @param a SignedVariablePart with counter before incrementing.
+     * @param b RecoveredVariablePart with incremented counter.
+     * @param a RecoveredVariablePart with counter before incrementing.
      */
     function _requireIncrementedCounter(
-        SignedVariablePart memory b,
-        SignedVariablePart memory a
+        RecoveredVariablePart memory b,
+        RecoveredVariablePart memory a
     ) internal pure {
         require(
             appData(b.variablePart.appData).counter == appData(a.variablePart.appData).counter + 1,
@@ -64,12 +64,12 @@ contract CountingApp is IForceMoveApp {
     /**
      * @notice Checks that supplied signed variable parts contain the same outcome.
      * @dev Checks that supplied signed variable parts contain the same outcome.
-     * @param a First SignedVariablePart.
-     * @param b Second SignedVariablePart.
+     * @param a First RecoveredVariablePart.
+     * @param b Second RecoveredVariablePart.
      */
     function _requireEqualOutcomes(
-        SignedVariablePart memory a,
-        SignedVariablePart memory b
+        RecoveredVariablePart memory a,
+        RecoveredVariablePart memory b
     ) internal pure {
         require(
             Outcome.exitsEqual(a.variablePart.outcome, b.variablePart.outcome),
