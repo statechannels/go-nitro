2b3fd7242f6b173274ca9f74d95a2fa30060805a
Mike Kerzhner <mikerzhner@gmail.com>
2022-07-19 15:48:03 -0600

    Remove EthAssetHolder and ERC20AssetHolder deploys

--- nitro-protocol/hardhat-deploy/01-token.ts
+++ nitro-protocol/hardhat-deploy/01-token.ts
@@ -8,14 +8,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     getNamedAccounts,
   } = hre;
 
-  const chainId = await getChainId();
-
-  // This is the default hardhat network chainid
-  if (chainId !== '31337') {
-    console.info(`Skipping deployment of Token for chain with ID: ${chainId}`);
-    return;
-  }
-
   const {deployer} = await getNamedAccounts();
 
   await deploy('Token', {
