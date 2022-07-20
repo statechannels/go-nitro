1228826d2bc8ed7ba444e8adf6bddced652dd489
nksazonov <nsazonov@openware.com>
2022-07-20 14:44:43 +0300

    add hardhat-ethers into hardhat.config

--- nitro-protocol/hardhat.config.ts
+++ nitro-protocol/hardhat.config.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import {HardhatUserConfig, task} from 'hardhat/config';
+import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
