bed80de16390be8d4a68cdd6bf226aaec0887073
geoknee <georgeknee@googlemail.com>
2022-08-18 10:28:06 +0100

    deploy ViritualPaymentApp during unit testing

--- nitro-protocol/environment.d.ts
+++ nitro-protocol/environment.d.ts
@@ -12,6 +12,7 @@ declare global {
       NITRO_ADJUDICATOR_ADDRESS: string;
       COUNTING_APP_ADDRESS: string;
       CONSENSUS_APP_ADDRESS: string;
+      VIRTUAL_PAYMENT_APP_ADDRESS: string;
       HASH_LOCK_ADDRESS: string;
       EMBEDDED_APPLICATION_ADDRESS: string;
       SINGLE_ASSET_PAYMENTS_ADDRESS: string;
