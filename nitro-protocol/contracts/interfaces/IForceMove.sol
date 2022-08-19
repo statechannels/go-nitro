7e03c85fc786fbadd655c4fb3694812ff29bbf76
nksazonov <nsazonov@openware.com>
2022-08-19 14:31:12 +0300

    proof and candidate comment improvements

--- nitro-protocol/contracts/interfaces/IForceMove.sol
+++ nitro-protocol/contracts/interfaces/IForceMove.sol
@@ -12,8 +12,8 @@ interface IForceMove is INitroTypes {
      * @notice Registers a challenge against a state channel. A challenge will either prompt another participant into clearing the challenge (via one of the other methods), or cause the channel to finalize at a specific time.
      * @dev Registers a challenge against a state channel. A challenge will either prompt another participant into clearing the challenge (via one of the other methods), or cause the channel to finalize at a specific time.
      * @param fixedPart Data describing properties of the state channel that do not change with state updates.
-     * @param proof An ordered array of structs, that can be signed by any number of participants, each struct describing the properties of the state channel that may change with each state update. The proof is a validation for the supplied candidate.
-     * @param candidate A struct, that can be signed by any number of participants, describing the properties of the state channel to change to. The candidate state is supported by proof states.
+     * @param proof Additional proof material (in the form of an array of signed states) which completes the support proof.
+     * @param candidate A candidate state (along with signatures) which is being claimed to be supported.
      * @param challengerSig The signature of a participant on the keccak256 of the abi.encode of (supportedStateHash, 'forceMove').
      */
     function challenge(
@@ -24,11 +24,11 @@ interface IForceMove is INitroTypes {
     ) external;
 
     /**
-     * @notice Overwrites the `turnNumRecord` stored against a channel by providing a proof with higher turn number.
-     * @dev Overwrites the `turnNumRecord` stored against a channel by providing a proof with higher turn number.
+     * @notice Overwrites the `turnNumRecord` stored against a channel by providing a candidate with higher turn number.
+     * @dev Overwrites the `turnNumRecord` stored against a channel by providing a candidate with higher turn number.
      * @param fixedPart Data describing properties of the state channel that do not change with state updates.
-     * @param proof An ordered array of structs, that can be signed by any number of participants, each struct describing the properties of the state channel that may change with each state update. The proof is a validation for the supplied candidate.
-     * @param candidate A struct, that can be signed by any number of participants, describing the properties of the state channel to change to. The candidate state is supported by proof states.
+     * @param proof Additional proof material (in the form of an array of signed states) which completes the support proof.
+     * @param candidate A candidate state (along with signatures) which is being claimed to be supported.
      */
     function checkpoint(
         FixedPart memory fixedPart,
@@ -40,8 +40,8 @@ interface IForceMove is INitroTypes {
      * @notice Finalizes a channel by providing a finalization proof. External wrapper for _conclude.
      * @dev Finalizes a channel by providing a finalization proof. External wrapper for _conclude.
      * @param fixedPart Data describing properties of the state channel that do not change with state updates.
-     * @param proof An ordered array of structs, that can be signed by any number of participants, each struct describing the properties of the state channel that may change with each state update. The proof is a validation for the supplied candidate.
-     * @param candidate A struct, that can be signed by any number of participants, describing the properties of the state channel to change to. The candidate state is supported by proof states.
+     * @param proof Additional proof material (in the form of an array of signed states) which completes the support proof.
+     * @param candidate A candidate state (along with signatures) which is being claimed to be supported.
      */
     function conclude(
         FixedPart memory fixedPart,
@@ -58,8 +58,8 @@ interface IForceMove is INitroTypes {
      * @param finalizesAt The unix timestamp when `channelId` will finalize.
      * @param isFinal Boolean denoting whether the challenge state is final.
      * @param fixedPart Data describing properties of the state channel that do not change with state updates.
-     * @param proof An ordered array of structs, that can be signed by any number of participants, each struct describing the properties of the state channel that may change with each state update. The proof is a validation for the supplied candidate.
-     * @param candidate A struct, that can be signed by any number of participants, describing the properties of the state channel to change to. The candidate state is supported by proof states.
+     * @param Additional proof material (in the form of an array of signed states) which completes the support proof.
+     * @param candidate A candidate state (along with signatures) which is being claimed to be supported.
      */
     event ChallengeRegistered(
         bytes32 indexed channelId,
