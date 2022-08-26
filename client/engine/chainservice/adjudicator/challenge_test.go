8fdcb806f470fe058fea7d251be80bdc703419c0
geoknee <georgeknee@googlemail.com>
2022-08-26 13:41:16 +0100

    challenge_test extended to turnNums 0,1,2

--- client/engine/chainservice/adjudicator/challenge_test.go
+++ client/engine/chainservice/adjudicator/challenge_test.go
@@ -3,6 +3,7 @@ package NitroAdjudicator
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"math/big"
 	"testing"
 
@@ -45,132 +46,134 @@ var Actors actors = actors{
 	},
 }
 
-// TODO https://github.com/statechannels/go-nitro/issues/785
-// This test should cover several different turn numbers, including pre and post fund tun numbers.
-var turnNum = uint64(2)
-
 func TestChallenge(t *testing.T) {
 
-	// Setup transacting EOA
-	key, _ := crypto.GenerateKey()
-	auth, _ := bind.NewKeyedTransactorWithChainID(key, big.NewInt(1337)) // 1337 according to godoc on backends.NewSimulatedBackend
-	address := auth.From
+	TestChallengeWithTurnNum := func(t *testing.T, turnNum uint64) {
+		// Setup transacting EOA
+		key, _ := crypto.GenerateKey()
+		auth, _ := bind.NewKeyedTransactorWithChainID(key, big.NewInt(1337)) // 1337 according to godoc on backends.NewSimulatedBackend
+		address := auth.From
 
-	// Setup a second transacting EOA
-	key2, _ := crypto.GenerateKey()
-	auth2, _ := bind.NewKeyedTransactorWithChainID(key2, big.NewInt(1337)) // 1337 according to godoc on backends.NewSimulatedBackend
-	address2 := auth2.From
+		// Setup a second transacting EOA
+		key2, _ := crypto.GenerateKey()
+		auth2, _ := bind.NewKeyedTransactorWithChainID(key2, big.NewInt(1337)) // 1337 according to godoc on backends.NewSimulatedBackend
+		address2 := auth2.From
 
-	// Setup "blockchain"
-	balance := new(big.Int)
-	balance.SetString("10000000000000000000", 10) // 10 eth in wei
-	gAlloc := map[common.Address]core.GenesisAccount{
-		address:  {Balance: balance},
-		address2: {Balance: balance},
+		// Setup "blockchain"
+		balance := new(big.Int)
+		balance.SetString("10000000000000000000", 10) // 10 eth in wei
+		gAlloc := map[common.Address]core.GenesisAccount{
+			address:  {Balance: balance},
+			address2: {Balance: balance},
+		}
+		blockGasLimit := uint64(4712388)
+		sim := backends.NewSimulatedBackend(gAlloc, blockGasLimit)
+
+		// Deploy Adjudicator
+		_, _, na, err := DeployNitroAdjudicator(auth, sim)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Mine a block
+		sim.Commit()
+
+		// Deploy ConsensusApp
+		consensusAppAddress, _, _, err := ConsensusApp.DeployConsensusApp(auth2, sim)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Mine a block
+		sim.Commit()
+
+		var s = state.State{
+			ChainId: big.NewInt(1337),
+			Participants: []types.Address{
+				Actors.Alice.Address,
+				Actors.Bob.Address,
+			},
+			ChannelNonce:      big.NewInt(37140676580),
+			AppDefinition:     consensusAppAddress,
+			ChallengeDuration: big.NewInt(60),
+			AppData:           []byte{},
+			Outcome:           outcome.Exit{},
+			TurnNum:           turnNum,
+			IsFinal:           false,
+		}
+
+		// Generate Signatures
+		aSig, _ := s.Sign(Actors.Alice.PrivateKey)
+		bSig, _ := s.Sign(Actors.Bob.PrivateKey)
+		challengerSig, err := SignChallengeMessage(s, Actors.Alice.PrivateKey)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Mine a block
+		sim.Commit()
+
+		// Construct support proof
+		candidate := INitroTypesSignedVariablePart{
+			ConvertVariablePart(s.VariablePart()),
+			[]INitroTypesSignature{ConvertSignature(aSig), ConvertSignature(bSig)},
+		}
+		proof := make([]INitroTypesSignedVariablePart, 0)
+
+		// Fire off a Challenge tx
+		tx, err := na.Challenge(
+			auth,
+			INitroTypesFixedPart(s.FixedPart()),
+			proof,
+			candidate,
+			ConvertSignature(challengerSig),
+		)
+		if err != nil {
+			t.Log(tx)
+			t.Fatal(err)
+		}
+
+		// Mine a block
+		sim.Commit()
+
+		// Compute challenge time
+		receipt, err := sim.TransactionReceipt(context.Background(), tx.Hash())
+		if err != nil {
+			t.Fatal(err)
+		}
+		header, err := sim.HeaderByNumber(context.Background(), receipt.BlockNumber)
+		if err != nil {
+			t.Fatal(err)
+		}
+		challengeTime := big.NewInt(int64(header.Time))
+
+		// Generate expectation
+		expectedFinalizesAt := big.NewInt(0).Add(challengeTime, s.ChallengeDuration)
+		cId := s.ChannelId()
+		expectedOnChainStatus, err := generateStatus(s, expectedFinalizesAt)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Inspect state of chain (call StatusOf)
+		statusOnChain, err := na.StatusOf(&bind.CallOpts{}, cId)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Make assertion
+		if !bytes.Equal(statusOnChain[:], expectedOnChainStatus) {
+			t.Fatalf("Adjudicator not updated as expected, got %v wanted %v", common.Bytes2Hex(statusOnChain[:]), common.Bytes2Hex(expectedOnChainStatus[:]))
+		}
+
+		// Not sure if this is necessary
+		sim.Close()
 	}
-	blockGasLimit := uint64(4712388)
-	sim := backends.NewSimulatedBackend(gAlloc, blockGasLimit)
 
-	// Deploy Adjudicator
-	_, _, na, err := DeployNitroAdjudicator(auth, sim)
-
-	if err != nil {
-		t.Fatal(err)
+	for _, turnNum := range []uint64{0, 1, 2} {
+		t.Run("turnNum = "+fmt.Sprint(turnNum), func(t *testing.T) { TestChallengeWithTurnNum(t, turnNum) })
 	}
-
-	// Mine a block
-	sim.Commit()
-
-	// Deploy ConsensusApp
-	consensusAppAddress, _, _, err := ConsensusApp.DeployConsensusApp(auth2, sim)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Mine a block
-	sim.Commit()
-
-	var s = state.State{
-		ChainId: big.NewInt(1337),
-		Participants: []types.Address{
-			Actors.Alice.Address,
-			Actors.Bob.Address,
-		},
-		ChannelNonce:      big.NewInt(37140676580),
-		AppDefinition:     consensusAppAddress,
-		ChallengeDuration: big.NewInt(60),
-		AppData:           []byte{},
-		Outcome:           outcome.Exit{},
-		TurnNum:           turnNum,
-		IsFinal:           false,
-	}
-
-	// Generate Signatures
-	aSig, _ := s.Sign(Actors.Alice.PrivateKey)
-	bSig, _ := s.Sign(Actors.Bob.PrivateKey)
-	challengerSig, err := SignChallengeMessage(s, Actors.Alice.PrivateKey)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Mine a block
-	sim.Commit()
-
-	// Construct support proof
-	candidate := INitroTypesSignedVariablePart{
-		ConvertVariablePart(s.VariablePart()),
-		[]INitroTypesSignature{ConvertSignature(aSig), ConvertSignature(bSig)},
-	}
-	proof := make([]INitroTypesSignedVariablePart, 0)
-
-	// Fire off a Challenge tx
-	tx, err := na.Challenge(
-		auth,
-		INitroTypesFixedPart(s.FixedPart()),
-		proof,
-		candidate,
-		ConvertSignature(challengerSig),
-	)
-	if err != nil {
-		t.Log(tx)
-		t.Fatal(err)
-	}
-
-	// Mine a block
-	sim.Commit()
-
-	// Compute challenge time
-	receipt, err := sim.TransactionReceipt(context.Background(), tx.Hash())
-	if err != nil {
-		t.Fatal(err)
-	}
-	header, err := sim.HeaderByNumber(context.Background(), receipt.BlockNumber)
-	if err != nil {
-		t.Fatal(err)
-	}
-	challengeTime := big.NewInt(int64(header.Time))
-
-	// Generate expectation
-	expectedFinalizesAt := big.NewInt(0).Add(challengeTime, s.ChallengeDuration)
-	cId := s.ChannelId()
-	expectedOnChainStatus, err := generateStatus(s, expectedFinalizesAt)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Inspect state of chain (call StatusOf)
-	statusOnChain, err := na.StatusOf(&bind.CallOpts{}, cId)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Make assertion
-	if !bytes.Equal(statusOnChain[:], expectedOnChainStatus) {
-		t.Fatalf("Adjudicator not updated as expected, got %v wanted %v", common.Bytes2Hex(statusOnChain[:]), common.Bytes2Hex(expectedOnChainStatus[:]))
-	}
-
-	// Not sure if this is necessary
-	sim.Close()
 }
