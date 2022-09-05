08d2f92aab3f382a1183db4236ee3760138dcfc8
nksazonov <nsazonov@openware.com>
2022-09-05 09:49:16 +0300

    add bitfield utils towards #761

---
+++ nitro-protocol/src/bitfield-utils.ts
@@ -0,0 +1,39 @@
+import {Uint256} from './contract/types';
+
+export function getSignersNum(signedByStr: Uint256): number {
+  let signedBy = parseFloat(signedByStr);
+  let amount = 0;
+
+  for (; signedBy > 0; amount++) {
+    signedBy &= signedBy - 1;
+  }
+
+  return amount;
+}
+
+export function getSignersIndices(signedByStr: Uint256): number[] {
+  let signedBy = parseFloat(signedByStr);
+
+  const signerIndices: number[] = [];
+  let signerNum = 0;
+  let acceptedSigners = 0;
+
+  while (signedBy > 0) {
+    if (signedBy % 2 == 1) {
+      signerIndices[acceptedSigners] = signerNum;
+      acceptedSigners++;
+    }
+    signedBy = signedBy >> 1;
+    signerNum++;
+  }
+
+  return signerIndices;
+}
+
+export function getSignedBy(signerIndices: number[]): Uint256 {
+  let signedBy = 0;
+  for (const sIdx of signerIndices) {
+    signedBy += 2 ** sIdx;
+  }
+  return signedBy.toString();
+}
