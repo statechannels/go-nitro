e06316ce61ccdc692f9ab687594d190cf6e26381
geoknee <georgeknee@googlemail.com>
2022-09-08 11:33:33 +0100

    WIP

--- nitro-protocol/test-filecoin/filecoin.test.ts
+++ nitro-protocol/test-filecoin/filecoin.test.ts
@@ -8,22 +8,25 @@ import {utils} from 'ethers';
 import cbor from 'cbor';
 // @ts-ignore
 import {FilecoinSigner, Message, FilecoinClient} from '@blitslabs/filecoin-js-signer';
+import axios from 'axios';
 
 const filecoin_signer = new FilecoinSigner();
 
 import TESTNitroAdjudicatorArtifact from '../artifacts/contracts/test/TESTNitroAdjudicator.sol/TESTNitroAdjudicator.json';
 
-const localGanache = 'ws://localhost:7777/rpc/v0';
-const localProvider = new NodejsProvider(localGanache);
-const localClient = new LotusRPC(localProvider, {schema: mainnet.fullNode});
+// const localGanache = 'ws://localhost:7777/rpc/v0';
+// const localProvider = new NodejsProvider(localGanache);
+// const localClient = new LotusRPC(localProvider, {schema: mainnet.fullNode});
 
-// follow https://github.com/Zondax/filecoin-signing-tools/blob/master/examples/wasm_node/payment_channel.js
+// // follow https://github.com/Zondax/filecoin-signing-tools/blob/master/examples/wasm_node/payment_channel.js
 
-const wallaby = 'https://wallaby.node.glif.io/rpc/v1';
-const provider = new NodejsProvider(wallaby);
-const client = new LotusRPC(provider, {schema: mainnet.fullNode});
+const wallaby = 'https://wallaby.node.glif.io/rpc/v0';
+const jimpicknet = 'https://fvm-4.default.knative.hex.camp/rpc/v0';
+const endpoint = jimpicknet;
+// const provider = new NodejsProvider(wallaby);
+// const client = new LotusRPC(provider, {schema: mainnet.fullNode});
 
-const filecoin_client = new FilecoinClient(wallaby, '');
+// const filecoin_client = new FilecoinClient(wallaby, '');
 
 const myAddress =
   't3w3ycieznjdzczqknlroo7aruchh5e373wz76su66ae4if3jrqjux346hyffu3l6htawazyiagbk6rne44cya';
@@ -49,31 +52,47 @@ describe('Connects to a local filecoin-flavoured ganache instance', () => {
     From: myAddress,
     Value: 0,
     Method: 2,
-    Params: params.toString('base64'),
-    Version: 1,
-    Nonce: 0,
+    Params: bytecodeHexString,
+    Version: 42,
+    Nonce: 42,
     GasLimit: 1_000_000,
     GasFeeCap: '0',
     GasPremium: '0',
   };
 
   it('reads the chain height', async () => {
-    const head = await localClient.chainHead();
-    console.log(head);
-    expect(head.Height).toEqual(0);
+    let response = await axios.post(wallaby, {
+      jsonrpc: '2.0',
+      method: 'Filecoin.Version',
+      params: [],
+      id: 1,
+    });
+    console.log(response.data);
 
-    // console.log('signing message...');
-    // const signedMessage = filecoin_signer.tx.transactionSignLotus(messageBody, privateKey);
+    console.log('signing message...');
+    const signature = filecoin_signer.utils.signMessage(messageBody, privateKey);
 
-    // console.log(signedMessage);
     console.log('pushing signed message to mempool');
-    const response = await filecoin_client.tx.sendMessage(messageBody, privateKey);
-    // const response = await client.mpoolPush(signedMessage);
-    //   const waitResponse = await client.stateWaitMsg(response.CID, 0)
+    const responsePromise = axios.post(endpoint, {
+      jsonrpc: '2.0',
+      method: 'Filecoin.MpoolPush',
+      params: [{Message: messageBody, Signature: {Type: 2, Data: signature}}],
+      id: 1,
+    });
+    await Promise.race([
+      responsePromise.catch(err => {
+        console.log(err.message);
+        throw err;
+      }),
+      responsePromise.then(response => console.log(response.data)),
+    ]);
+    // const response = await filecoin_client.tx.sendMessage(messageBody, privateKey);
+    // // const response = await client.mpoolPush(signedMessage);
+    // //   const waitResponse = await client.stateWaitMsg(response.CID, 0)
 
-    expect(response).toEqual('foo');
+    // expect(response).toEqual('foo');
 
-    localClient.destroy();
-    client.destroy();
+    // localClient.destroy();
+    // client.destroy();
   });
 });
