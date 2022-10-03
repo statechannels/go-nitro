1f18d19cca97ecdcf59a5140255f4eab66bcc22b
geoknee <georgeknee@googlemail.com>
2022-10-03 15:38:33 +0100

    slow progress
    failed to load message: ipld: could not find bafy2bzaceaivkjc6psqhz2xbl6ibsbmqz2ge5gtcxjffoolndcux6za65kxnq

--- client/engine/chainservice/eth_chainservice_wallaby_test.go
+++ client/engine/chainservice/eth_chainservice_wallaby_test.go
@@ -11,15 +11,11 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
+	gethTypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/filecoin-project/go-address"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	NitroAdjudicator "github.com/statechannels/go-nitro/client/engine/chainservice/adjudicator"
-	"github.com/statechannels/go-nitro/protocols"
-	"github.com/statechannels/go-nitro/types"
 )
 
 func TestEthChainServiceAgainstWallaby(t *testing.T) {
@@ -40,7 +36,7 @@ func TestEthChainServiceAgainstWallaby(t *testing.T) {
 	// Add a valid private key with testnet Eth. DO NOT check into git.
 	pkString := "6645aa9129061ccef190e1bb1e11319b3d716b3140eec27595d045dbd565733b"
 
-	one := big.NewInt(1)
+	// one := big.NewInt(1)
 
 	pk, err := crypto.HexToECDSA(pkString)
 	if err != nil {
@@ -78,74 +74,84 @@ func TestEthChainServiceAgainstWallaby(t *testing.T) {
 
 	nonce := responseBody.result
 
-	txSubmitter, err := bind.NewKeyedTransactorWithChainID(pk, big.NewInt(5))
+	txSubmitter, err := bind.NewKeyedTransactorWithChainID(pk, big.NewInt(31415))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	txSubmitter.GasLimit = uint64(300000) // in units
+	txSubmitter.GasLimit = uint64(47755863) // in units
 	txSubmitter.GasFeeCap = big.NewInt(100)
 
 	txSubmitter.Nonce = big.NewInt(nonce)
 
-	// nonce, err := client.NonceAt(context.Background(), txSubmitter.From, nil)
+	// As of the "Iron" FVM release, it seems that the return value of things like eth_getBlockByNumber do not match the spec.
+	// Linked to this (probably) https://github.com/filecoin-project/ref-fvm/issues/908
+	// Since the geth ethClient calls out to eth_getBlockNumber and tries to deserialize the result to one including `logsBloom` parameter, the following command will not yet work:
+	// naAddress, _, na, err := NitroAdjudicator.DeployNitroAdjudicator(txSubmitter, client)
+
+	signedTx, err := txSubmitter.Signer(txSubmitter.From,
+		gethTypes.NewTx(&(gethTypes.DynamicFeeTx{
+			// ChainID:   big.NewInt(31415),
+			Nonce:     uint64(nonce),
+			GasTipCap: txSubmitter.GasTipCap,
+			GasFeeCap: txSubmitter.GasFeeCap,
+			Gas:       txSubmitter.GasLimit,
+			Value:     big.NewInt(0),
+			Data:      []byte(NitroAdjudicator.NitroAdjudicatorMetaData.Bin),
+		})))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	receipt, err := client.TransactionReceipt(context.Background(), signedTx.Hash())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(receipt)
+
+	// caAddress := common.Address{}  // TODO use proper address
+	// vpaAddress := common.Address{} // TODO use proper address
+
+	// cs, err := NewEthChainService(client, na, naAddress, caAddress, vpaAddress, txSubmitter, NoopLogger{})
 	// if err != nil {
 	// 	t.Fatal(err)
 	// }
 
-	// txSubmitter.Nonce = big.NewInt(int64(nonce))
+	// // Prepare test data to trigger EthChainService
+	// testDeposit := types.Funds{
+	// 	common.HexToAddress("0x00"): one,
+	// }
 
-	// getting an error on the below: failed to retrieve account nonce: actor not found
-	// possible solution:
-	//   const f1addr = fa.newSecp256k1Address(pubKey).toString();
-	//   const priorityFee = await callRpc("eth_maxPriorityFeePerGas");
-	//   const nonce = await callRpc("Filecoin.MpoolGetNonce", [f1addr]); we need to call filecoin mpoolgetnonce with the filecoin address
+	// channelID := types.Destination(common.HexToHash(`4ebd366d014a173765ba1e50f284c179ade31f20441bec41664712aac6cc461d`))
+	// testTx := protocols.NewDepositTransaction(channelID, testDeposit)
 
-	naAddress, _, na, err := NitroAdjudicator.DeployNitroAdjudicator(txSubmitter, client)
+	// out := cs.EventFeed()
+	// // Submit transactiom
+	// err = cs.SendTransaction(testTx)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	// // Check that the recieved events matches the expected event
+	// for i := 0; i < 2; i++ {
+	// 	receivedEvent := <-out
+	// 	dEvent := receivedEvent.(DepositedEvent)
+	// 	expectedEvent := NewDepositedEvent(channelID, 2, dEvent.AssetAddress, testDeposit[dEvent.AssetAddress], testDeposit[dEvent.AssetAddress])
+	// 	// TODO to validate BlockNum and NowHeld values, chain state prior to transaction must be inspected
+	// 	ignoreBlockNum := cmpopts.IgnoreFields(commonEvent{}, "BlockNum")
+	// 	ignoreNowHeld := cmpopts.IgnoreFields(DepositedEvent{}, "NowHeld")
 
-	caAddress := common.Address{}  // TODO use proper address
-	vpaAddress := common.Address{} // TODO use proper address
+	// 	if diff := cmp.Diff(expectedEvent, dEvent, cmp.AllowUnexported(DepositedEvent{}, commonEvent{}, big.Int{}), ignoreBlockNum, ignoreNowHeld); diff != "" {
+	// 		t.Fatalf("Received event did not match expectation; (-want +got):\n%s", diff)
+	// 	}
+	// 	delete(testDeposit, dEvent.AssetAddress)
+	// }
 
-	cs, err := NewEthChainService(client, na, naAddress, caAddress, vpaAddress, txSubmitter, NoopLogger{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Prepare test data to trigger EthChainService
-	testDeposit := types.Funds{
-		common.HexToAddress("0x00"): one,
-	}
-
-	channelID := types.Destination(common.HexToHash(`4ebd366d014a173765ba1e50f284c179ade31f20441bec41664712aac6cc461d`))
-	testTx := protocols.NewDepositTransaction(channelID, testDeposit)
-
-	out := cs.EventFeed()
-	// Submit transactiom
-	err = cs.SendTransaction(testTx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Check that the recieved events matches the expected event
-	for i := 0; i < 2; i++ {
-		receivedEvent := <-out
-		dEvent := receivedEvent.(DepositedEvent)
-		expectedEvent := NewDepositedEvent(channelID, 2, dEvent.AssetAddress, testDeposit[dEvent.AssetAddress], testDeposit[dEvent.AssetAddress])
-		// TODO to validate BlockNum and NowHeld values, chain state prior to transaction must be inspected
-		ignoreBlockNum := cmpopts.IgnoreFields(commonEvent{}, "BlockNum")
-		ignoreNowHeld := cmpopts.IgnoreFields(DepositedEvent{}, "NowHeld")
-
-		if diff := cmp.Diff(expectedEvent, dEvent, cmp.AllowUnexported(DepositedEvent{}, commonEvent{}, big.Int{}), ignoreBlockNum, ignoreNowHeld); diff != "" {
-			t.Fatalf("Received event did not match expectation; (-want +got):\n%s", diff)
-		}
-		delete(testDeposit, dEvent.AssetAddress)
-	}
-
-	if len(testDeposit) != 0 {
-		t.Fatalf("Mismatch between the deposit transaction and the received events")
-	}
+	// if len(testDeposit) != 0 {
+	// 	t.Fatalf("Mismatch between the deposit transaction and the received events")
+	// }
 }
