0579db094ead7344eba1526942ab4e721d3ab3d7
geoknee <georgeknee@googlemail.com>
2022-10-05 14:45:33 +0100

    add comments

--- client/engine/chainservice/fevm_chainservice_test.go
+++ client/engine/chainservice/fevm_chainservice_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+// Run this using
+// go test ./client/engine/chainservice -run TestFevmChainService -v -p=1
 func TestFevmChainService(t *testing.T) {
 
 	fcs, err := NewFevmChainService("https://wallaby.node.glif.io/rpc/v0", "9182b5bf5b9c966e001934ebaf008f65516290cef6e3069d11e718cbd4336aae", log.Default().Writer())
