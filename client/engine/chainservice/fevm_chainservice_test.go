c3678e17b6ac234cc80330804a359ad7b7f4650e
geoknee <georgeknee@googlemail.com>
2022-10-05 14:51:49 +0100

    add fevmchainservice and test

--- client/engine/chainservice/fevm_chainservice_test.go
+++ client/engine/chainservice/fevm_chainservice_test.go
@@ -1,46 +1,34 @@
 package chainservice
 
 import (
-	"math/big"
+	"context"
+	"log"
 	"testing"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
-	"github.com/statechannels/go-nitro/protocols"
-	"github.com/statechannels/go-nitro/types"
 )
 
+// Run this using
+// go test ./client/engine/chainservice -run TestFevmChainService -v -p=1
 func TestFevmChainService(t *testing.T) {
-	one := big.NewInt(1)
 
-	cs := NewFevmChainService()
-
-	// Prepare test data to trigger EthChainService
-	testDeposit := types.Funds{
-		common.HexToAddress("0x00"): one,
+	fcs, err := NewFevmChainService("https://wallaby.node.glif.io/rpc/v0", "9182b5bf5b9c966e001934ebaf008f65516290cef6e3069d11e718cbd4336aae", log.Default().Writer())
+	if err != nil {
+		t.Fatal(fcs)
 	}
-	channelID := types.Destination(common.HexToHash(`4ebd366d014a173765ba1e50f284c179ade31f20441bec41664712aac6cc4611`))
-	testTx := protocols.NewDepositTransaction(channelID, testDeposit)
+	blockNumber, err := fcs.chain.BlockNumber(context.Background())
+	if err != nil {
+		t.Fatal(fcs)
+	}
+	t.Log("Block number is", blockNumber)
+	nonce, err := fcs.filecoinNonce()
 
-	out := cs.EventFeed()
-	cs.Monitor(channelID, testDeposit, testDeposit)
-
-	// Submit transactiom
-	err := cs.SendTransaction(testTx)
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Log("Filecoin.MpoolGetNonce call returned", nonce)
 
-	// Check that the recieved events matches the expected event
-	receivedEvent := <-out
-	dEvent := receivedEvent.(DepositedEvent)
-	expectedEvent := NewDepositedEvent(channelID, 2, dEvent.AssetAddress, testDeposit[dEvent.AssetAddress], testDeposit[dEvent.AssetAddress])
-	// TODO to validate BlockNum and NowHeld values, chain state prior to transaction must be inspected
-	ignoreBlockNum := cmpopts.IgnoreFields(commonEvent{}, "BlockNum")
-	ignoreNowHeld := cmpopts.IgnoreFields(DepositedEvent{}, "NowHeld")
-
-	if diff := cmp.Diff(expectedEvent, dEvent, cmp.AllowUnexported(DepositedEvent{}, commonEvent{}, big.Int{}), ignoreBlockNum, ignoreNowHeld); diff != "" {
-		t.Fatalf("Received event did not match expectation; (-want +got):\n%s", diff)
+	err = fcs.deployAdjudicator()
+	if err != nil {
+		t.Fatal(err)
 	}
+	t.Log("chain service stored adjudicator address:", fcs.naAddress)
 }
