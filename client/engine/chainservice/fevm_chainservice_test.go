e96462534a8172586498093d5d7b135c9073641c
geoknee <georgeknee@googlemail.com>
2022-10-05 15:29:10 +0100

    silence linter warnings

--- client/engine/chainservice/fevm_chainservice_test.go
+++ client/engine/chainservice/fevm_chainservice_test.go
@@ -9,7 +9,7 @@ import (
 // Run this using
 // go test ./client/engine/chainservice -run TestFevmChainService -v -p=1
 func TestFevmChainService(t *testing.T) {
-	t.Skip() // should not run in CI because it depends on wallaby testnet
+	// t.Skip() // should not run in CI because it depends on wallaby testnet
 	fcs, err := NewFevmChainService("https://wallaby.node.glif.io/rpc/v0", "9182b5bf5b9c966e001934ebaf008f65516290cef6e3069d11e718cbd4336aae", log.Default().Writer())
 	if err != nil {
 		t.Fatal(fcs)
@@ -26,7 +26,7 @@ func TestFevmChainService(t *testing.T) {
 	}
 	t.Log("Filecoin.MpoolGetNonce call returned", nonce)
 
-	err = fcs.deployAdjudicator()
+	err = fcs.DeployAdjudicator()
 	if err != nil {
 		t.Fatal(err)
 	}
