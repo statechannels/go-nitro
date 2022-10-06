f0e74994b9e44856f86c60769cd3ef720469f8a6
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-10-06 12:52:53 -0300

    remove len(participants) == 3 assumption

--- payments/helpers.go
+++ payments/helpers.go
@@ -3,7 +3,6 @@ package payments
 import "github.com/statechannels/go-nitro/types"
 
 const PAYER_INDEX = 0
-const PAYEE_INDEX = 2
 
 // GetPayer returns the payer on a payment channel
 func GetPayer(participants []types.Address) types.Address {
@@ -12,5 +11,5 @@ func GetPayer(participants []types.Address) types.Address {
 
 // GetPayee returns the payee on a payment channel
 func GetPayee(participants []types.Address) types.Address {
-	return participants[PAYEE_INDEX]
+	return participants[len(participants)-1]
 }
