280c06d98ebe222515091f540315cb554244598c
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-10-06 14:47:10 -0300

    update doccomment

--- client/client.go
+++ client/client.go
@@ -93,7 +93,8 @@ func (c *Client) ReceivedVouchers() <-chan payments.Voucher {
 	return c.receivedVouchers
 }
 
-// CreateVirtualChannel creates a virtual channel with the counterParty using ledger channels with the intermediary.
+// CreateVirtualChannel creates a virtual channel with the counterParty using ledger channels
+// with the supplied intermediaries.
 func (c *Client) CreateVirtualPaymentChannel(Intermediaries []types.Address, CounterParty types.Address, ChallengeDuration uint32, Outcome outcome.Exit) virtualfund.ObjectiveResponse {
 
 	objectiveRequest := virtualfund.ObjectiveRequest{
