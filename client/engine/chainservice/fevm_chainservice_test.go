7f7298fa5a0b36ac3114c4da3eb65711f7a609dd
geoknee <georgeknee@googlemail.com>
2022-10-12 12:23:09 +0100

    pass err to t.Fatal

--- client/engine/chainservice/fevm_chainservice_test.go
+++ client/engine/chainservice/fevm_chainservice_test.go
@@ -12,17 +12,17 @@ func TestFevmChainService(t *testing.T) {
 
 	fcs, err := NewFevmChainService("https://wallaby.node.glif.io/rpc/v0", "9182b5bf5b9c966e001934ebaf008f65516290cef6e3069d11e718cbd4336aae", log.Default().Writer())
 	if err != nil {
-		t.Fatal(fcs)
+		t.Fatal(err)
 	}
 	blockNumber, err := fcs.chain.BlockNumber(context.Background())
 	if err != nil {
-		t.Fatal(fcs)
+		t.Fatal(err)
 	}
 	t.Log("Block number is", blockNumber)
 	nonce, err := fcs.filecoinNonce()
 
 	if err != nil {
-		t.Fatal(fcs)
+		t.Fatal(err)
 	}
 	t.Log("Filecoin.MpoolGetNonce call returned", nonce)
 
