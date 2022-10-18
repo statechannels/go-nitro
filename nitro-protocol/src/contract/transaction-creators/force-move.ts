d5a149a9f8e366f66ef32bc2cdedff265d191c2a
geoknee <georgeknee@googlemail.com>
2022-10-18 11:36:42 +0100

    Revert "remove import/export of force move artifact"
    This reverts commit 2cbb905c1dba03815a8f53c8d5b9750c3f7403fd.

--- nitro-protocol/src/contract/transaction-creators/force-move.ts
+++ nitro-protocol/src/contract/transaction-creators/force-move.ts
@@ -1,7 +1,11 @@
 import {Signature, ethers} from 'ethers';
 
+import ForceMoveArtifact from '../../../artifacts/contracts/ForceMove.sol/ForceMove.json';
 import {bindSignatures, signChallengeMessage} from '../../signatures';
 import {getFixedPart, getVariablePart, separateProofAndCandidate, State} from '../state';
+
+// https://github.com/ethers-io/ethers.js/issues/602#issuecomment-574671078
+export const ForceMoveContractInterface = new ethers.utils.Interface(ForceMoveArtifact.abi);
 
 interface CheckpointData {
   challengeState?: State;
