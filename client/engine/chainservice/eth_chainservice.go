ba0c1eb136254e310f471320e9da6730279fa8a9
Alex Gap <alex.gap@gmail.com>
2022-11-22 11:46:01 -0800

    Update chain service to work with http nodes

--- client/engine/chainservice/eth_chainservice.go
+++ client/engine/chainservice/eth_chainservice.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/big"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
@@ -25,6 +26,7 @@ var depositedTopic = crypto.Keccak256Hash([]byte("Deposited(bytes32,address,uint
 type ethChain interface {
 	bind.ContractBackend
 	ethereum.TransactionReader
+	BlockNumber(ctx context.Context) (uint64, error)
 }
 
 type EthChainService struct {
@@ -38,11 +40,9 @@ type EthChainService struct {
 	logger                   *log.Logger
 }
 
-// RESUB_INTERVAL is how often we resubscribe to log events.
-// We do this to avoid https://github.com/ethereum/go-ethereum/issues/23845
-// We use 2.5 minutes as the default filter timeout is 5 minutes.
-// See https://github.com/ethereum/go-ethereum/blob/e14164d516600e9ac66f9060892e078f5c076229/eth/filters/filter_system.go#L43
-const RESUB_INTERVAL = 2*time.Minute + 30*time.Second
+// Since we only fetch events if there's a new block number
+// it's safe to poll relatively frequently.
+const EVENT_POLL_INTERVAL = 500 * time.Millisecond
 
 // NewEthChainService constructs a chain service that submits transactions to a NitroAdjudicator
 // and listens to events from an eventSource
@@ -53,18 +53,19 @@ func NewEthChainService(chain ethChain, na *NitroAdjudicator.NitroAdjudicator,
 	// Use a buffered channel so we don't have to worry about blocking on writing to the channel.
 	ecs := EthChainService{chain, na, naAddress, caAddress, vpaAddress, txSigner, make(chan Event, 10), logger}
 
-	err := ecs.subcribeToEvents()
-	return &ecs, err
+	go ecs.listenForLogEvents(context.Background())
+	return &ecs, nil
 }
 
 // defaultTxOpts returns transaction options suitable for most transaction submissions
 func (ecs *EthChainService) defaultTxOpts() *bind.TransactOpts {
+	// FEVM only supports type 2 transactions
 	return &bind.TransactOpts{
-		From:     ecs.txSigner.From,
-		Nonce:    ecs.txSigner.Nonce,
-		Signer:   ecs.txSigner.Signer,
-		GasPrice: ecs.txSigner.GasPrice,
-		GasLimit: ecs.txSigner.GasLimit,
+		From:      ecs.txSigner.From,
+		Nonce:     ecs.txSigner.Nonce,
+		Signer:    ecs.txSigner.Signer,
+		GasFeeCap: ecs.txSigner.GasFeeCap,
+		GasTipCap: ecs.txSigner.GasTipCap,
 	}
 }
 
@@ -116,84 +117,105 @@ func (ecs *EthChainService) SendTransaction(tx protocols.ChainTransaction) error
 		return fmt.Errorf("unexpected transaction type %T", tx)
 	}
 }
-func (ecs *EthChainService) subcribeToEvents() error {
 
-	go ecs.listenForLogEvents()
-	return nil
+// dispatchChainEvents takes in a collection of event logs from the chain
+// and dispatches events to the out channel
+func (ecs *EthChainService) dispatchChainEvents(logs []ethTypes.Log) {
+	for _, l := range logs {
+		switch l.Topics[0] {
+		case depositedTopic:
+			nad, err := ecs.na.ParseDeposited(l)
+			if err != nil {
+				ecs.logger.Printf("error in ParseDeposited: %v", err)
+			}
+
+			event := NewDepositedEvent(nad.Destination, l.BlockNumber, nad.Asset, nad.AmountDeposited, nad.DestinationHoldings)
+			ecs.out <- event
+		case allocationUpdatedTopic:
+			au, err := ecs.na.ParseAllocationUpdated(l)
+			if err != nil {
+				ecs.logger.Printf("error in ParseAllocationUpdated: %v", err)
+			}
+
+			tx, pending, err := ecs.chain.TransactionByHash(context.Background(), l.TxHash)
+			if pending {
+				ecs.logger.Printf("Expected transacion to be part of the chain, but the transaction is pending")
+			}
+			if err != nil {
+				ecs.logger.Printf("error in TransactoinByHash: %v", err)
+			}
+
+			assetAddress, amount, err := getChainHolding(ecs.na, tx, au)
+			if err != nil {
+				ecs.logger.Printf("error in getChainHoldings: %v", err)
+			}
+			event := NewAllocationUpdatedEvent(au.ChannelId, l.BlockNumber, assetAddress, amount)
+			ecs.out <- event
+		case concludedTopic:
+			ce, err := ecs.na.ParseConcluded(l)
+			if err != nil {
+				ecs.logger.Printf("error in ParseConcluded: %v", err)
+			}
+
+			event := ConcludedEvent{commonEvent: commonEvent{channelID: ce.ChannelId, BlockNum: l.BlockNumber}}
+			ecs.out <- event
+
+		default:
+			ecs.logger.Printf("Unknown chain event")
+		}
+	}
+
 }
 
-func (ecs *EthChainService) listenForLogEvents() {
-	// Subsribe to Adjudicator events
-	query := ethereum.FilterQuery{
-		Addresses: []common.Address{ecs.naAddress},
-	}
-	logs := make(chan ethTypes.Log)
-	sub, err := ecs.chain.SubscribeFilterLogs(context.Background(), query, logs)
+// getCurrentBlockNumber returns the current block number.
+func (ecs *EthChainService) getCurrentBlockNum() *big.Int {
+	// TODO: We specifically call BlockNumber as HeaderByNumber can fail
+	// see https://github.com/filecoin-project/ref-fvm/issues/1135
+	blockNum, err := ecs.chain.BlockNumber(context.Background())
 	if err != nil {
 		panic(err)
 	}
+
+	return big.NewInt(int64(blockNum))
+}
+
+// listenForLogEvents periodically polls the chain client to check if there new events.
+// If so it dispatches events to
+func (ecs *EthChainService) listenForLogEvents(ctx context.Context) {
+
+	// The initial query we want to get all events since the contract was deployed to the current block.
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{ecs.naAddress},
+		FromBlock: nil,
+		ToBlock:   ecs.getCurrentBlockNum(),
+	}
+
+	fetchedLogs, err := ecs.chain.FilterLogs(context.Background(), query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	ecs.dispatchChainEvents(fetchedLogs)
 	for {
 		select {
-		case err := <-sub.Err():
-			if err != nil {
-				panic(err)
+		case <-time.After(EVENT_POLL_INTERVAL):
+			currentBlock := ecs.getCurrentBlockNum()
+			if moreRecentBlockAvailable := currentBlock.Cmp(query.ToBlock) > 0; moreRecentBlockAvailable {
+				// We update to query to be between our previous block number and the latest block number
+				query.FromBlock = big.NewInt(0).Set(query.ToBlock)
+				query.ToBlock = big.NewInt(0).Set(currentBlock)
+
+				fetchedLogs, err := ecs.chain.FilterLogs(context.Background(), query)
+
+				if err != nil {
+					panic(err)
+				}
+
+				ecs.dispatchChainEvents(fetchedLogs)
 			}
-
-			// If the error is nil then the subscription was closed and we need to re-subscribe.
-			// This is a workaround for https://github.com/ethereum/go-ethereum/issues/23845
-			var sErr error
-			sub, sErr = ecs.chain.SubscribeFilterLogs(context.Background(), query, logs)
-			if sErr != nil {
-				panic(err)
-			}
-			ecs.logger.Println("resubscribed to filtered logs")
-
-		case <-time.After(RESUB_INTERVAL):
-			// Due to https://github.com/ethereum/go-ethereum/issues/23845 we can't rely on a long running subscription.
-			// We unsub here and recreate the subscription in the next iteration of the select.
-			sub.Unsubscribe()
-		case chainEvent := <-logs:
-			switch chainEvent.Topics[0] {
-			case depositedTopic:
-				nad, err := ecs.na.ParseDeposited(chainEvent)
-				if err != nil {
-					ecs.logger.Printf("error in ParseDeposited: %v", err)
-				}
-
-				event := NewDepositedEvent(nad.Destination, chainEvent.BlockNumber, nad.Asset, nad.AmountDeposited, nad.DestinationHoldings)
-				ecs.out <- event
-			case allocationUpdatedTopic:
-				au, err := ecs.na.ParseAllocationUpdated(chainEvent)
-				if err != nil {
-					ecs.logger.Printf("error in ParseAllocationUpdated: %v", err)
-				}
-
-				tx, pending, err := ecs.chain.TransactionByHash(context.Background(), chainEvent.TxHash)
-				if pending {
-					ecs.logger.Printf("Expected transacion to be part of the chain, but the transaction is pending")
-				}
-				if err != nil {
-					ecs.logger.Printf("error in TransactoinByHash: %v", err)
-				}
-
-				assetAddress, amount, err := getChainHolding(ecs.na, tx, au)
-				if err != nil {
-					ecs.logger.Printf("error in getChainHoldings: %v", err)
-				}
-				event := NewAllocationUpdatedEvent(au.ChannelId, chainEvent.BlockNumber, assetAddress, amount)
-				ecs.out <- event
-			case concludedTopic:
-				ce, err := ecs.na.ParseConcluded(chainEvent)
-				if err != nil {
-					ecs.logger.Printf("error in ParseConcluded: %v", err)
-				}
-
-				event := ConcludedEvent{commonEvent: commonEvent{channelID: ce.ChannelId, BlockNum: chainEvent.BlockNumber}}
-				ecs.out <- event
-
-			default:
-				ecs.logger.Printf("Unknown chain event")
-			}
+		case <-ctx.Done():
+			return
 		}
 	}
 
