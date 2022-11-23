c7088ab4a44477137212c46df406f9723ce94c46
geoknee <georgeknee@googlemail.com>
2022-11-23 13:18:11 +0000

    remove usage of lodash.pick
    This appears to have been used to try and trim down the tarball size for npm. However, it doesn't actually work. Seeing as the dependency is listed as a dev dependency (but is being used as a runtime dependency), best to remove it.
    
    Fixes #995

--- nitro-protocol/src/index.ts
+++ nitro-protocol/src/index.ts
@@ -1,48 +1,11 @@
-import pick from 'lodash.pick';
-
 import FULLNitroAdjudicatorArtifact from '../artifacts/contracts/NitroAdjudicator.sol/NitroAdjudicator.json';
 import FULLConsensusAppArtifact from '../artifacts/contracts/ConsensusApp.sol/ConsensusApp.json';
 import FULLVirtualPaymentAppArtifact from '../artifacts/contracts/VirtualPaymentApp.sol/VirtualPaymentApp.json';
 
-interface ArtifactT {
-  _format: string;
-  contractName: string;
-  sourceName: string;
-  abi: object;
-  bytecode: string;
-  deployedBytecode: string;
-  linkReferences: object;
-  deployedLinkReferences: object;
-}
-
-// https://hardhat.org/guides/compile-contracts.html#artifacts
-const fields = [
-  'contractName',
-  'abi',
-  'bytecode',
-  'deployedBytecode',
-  'linkReferences',
-  'deployedLinkReferences',
-];
-
-interface MinimalArtifact {
-  contractName: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  abi: any;
-  bytecode: string;
-  deployedBytecode: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  linkReferences: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  deployedLinkReferences: any;
-}
-
-const minimize = (artifact: ArtifactT) => pick(artifact, fields) as MinimalArtifact;
-
 export const ContractArtifacts = {
-  NitroAdjudicatorArtifact: minimize(FULLNitroAdjudicatorArtifact),
-  ConsensusAppArtifact: minimize(FULLConsensusAppArtifact),
-  VirtualPaymentAppArtifact: minimize(FULLVirtualPaymentAppArtifact),
+  NitroAdjudicatorArtifact: FULLNitroAdjudicatorArtifact,
+  ConsensusAppArtifact: FULLConsensusAppArtifact,
+  VirtualPaymentAppArtifact: FULLVirtualPaymentAppArtifact,
 };
 
 export {
