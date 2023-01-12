13d407569f953fcb8d913ee85b34a35a7f1ced03
geoknee <georgeknee@googlemail.com>
2023-01-12 13:54:16 +0000

    pass through turnNum in removeProposal

--- protocols/messages_test.go
+++ protocols/messages_test.go
@@ -16,7 +16,7 @@ import (
 
 func removeProposal(ledgerId types.Destination, turnNum uint64) consensus_channel.SignedProposal {
 	remove := consensus_channel.NewRemoveProposal(ledgerId, types.Destination{'a'}, big.NewInt(1))
-	return consensus_channel.SignedProposal{Proposal: remove, Signature: state.Signature{}}
+	return consensus_channel.SignedProposal{Proposal: remove, Signature: state.Signature{}, TurnNum: turnNum}
 }
 
 func addProposal(ledgerId types.Destination, turnNum uint64) consensus_channel.SignedProposal {
