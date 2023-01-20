3ad8a4ac58c223d2b18a26ea1b38f11ad0c97ad8
geoknee <georgeknee@googlemail.com>
2023-01-20 14:07:56 +0000

    test helper

---
+++ client/engine/chainservice/split_block_range_test.go
@@ -0,0 +1,52 @@
+package chainservice
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestSplitBlockRange(t *testing.T) {
+
+	type bR struct {
+		from uint
+		to   uint
+	}
+
+	toBlockRange := func(br bR) blockRange {
+		return blockRange{from: big.NewInt(int64(br.from)), to: big.NewInt(int64(br.to))}
+	}
+
+	toBlockRanges := func(brs []bR) []blockRange {
+		blockRanges := make([]blockRange, len(brs))
+		for i, br := range brs {
+			blockRanges[i] = toBlockRange(br)
+		}
+		return blockRanges
+	}
+
+	type testCase struct {
+		testBlockRange  bR
+		testMaxInterval uint
+		expectation     []bR
+	}
+
+	testCases := []testCase{
+		{bR{from: 3, to: 7}, 2, []bR{{3, 5}, {6, 7}}},
+		{bR{from: 0, to: 11282}, 5640, []bR{{0, 5640}, {5641, 11281}, {11282, 11282}}},
+	}
+
+	for i, tc := range testCases {
+		if diff := cmp.Diff(
+			splitBlockRange(
+				toBlockRange(tc.testBlockRange),
+				big.NewInt(int64(tc.testMaxInterval)),
+			),
+			toBlockRanges(tc.expectation),
+			cmp.AllowUnexported(blockRange{}, big.Int{})); diff != "" {
+			t.Fatalf("splitBlockRange output incorrect on test case %v: %v", i, diff)
+		}
+	}
+
+}
