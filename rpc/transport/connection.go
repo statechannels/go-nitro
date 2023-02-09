ed6920a6931d4b50b7f41b6111fea68cf01b8713
geoknee <georgeknee@googlemail.com>
2023-02-09 15:24:25 +0000

    split transport connection interface into requester/subscriber

--- rpc/transport/connection.go
+++ rpc/transport/connection.go
@@ -2,17 +2,20 @@ package transport
 
 import "github.com/statechannels/go-nitro/rpc/serde"
 
-type Connection interface {
+type Requester interface {
+	Close()
+
 	// Request sends data for a topic and returns the response data or an error
 	Request(serde.RequestMethod, []byte) ([]byte, error)
-	// Respond listens for requests for a topic and calls the handler function when a request is received
-	Respond(serde.RequestMethod, func([]byte) []byte) error
-
-	// Notify sends data for a topic without expecting a response
-	Notify(serde.NotificationMethod, []byte) error
 	// Subscribe listens for notifications for a topic and does not respond to notifications
 	Subscribe(serde.NotificationMethod, func([]byte)) error
+}
 
-	// Close shuts down the connection
+type Subscriber interface {
 	Close()
+
+	// Respond listens for requests for a topic and calls the handler function when a request is received
+	Respond(serde.RequestMethod, func([]byte) []byte) error
+	// Notify sends data for a topic without expecting a response
+	Notify(serde.NotificationMethod, []byte) error
 }
