b14af210f279fb6071461f97f25eebcd6080274d
Mike Kerzhner <mikerzhner@gmail.com>
2023-02-16 10:22:31 -0800

    Refactor Connection interface
    A connection should know as little as possible about the
    data sent over the connection

--- rpc/transport/connection.go
+++ rpc/transport/connection.go
@@ -1,18 +1,32 @@
 package transport
 
-import "github.com/statechannels/go-nitro/rpc/serde"
+type ConnectionType string
 
-type Connection interface {
-	// Request sends data for a topic and returns the response data or an error
-	Request(serde.RequestMethod, []byte) ([]byte, error)
-	// Respond listens for requests for a topic and calls the handler function when a request is received
-	Respond(serde.RequestMethod, func([]byte) []byte) error
+const (
+	Nats ConnectionType = "nats"
+	Ws   ConnectionType = "ws"
+)
 
-	// Notify sends data for a topic without expecting a response
-	Notify(serde.NotificationMethod, []byte) error
-	// Subscribe listens for notifications for a topic and does not respond to notifications
-	Subscribe(serde.NotificationMethod, func([]byte)) error
-
-	// Close shuts down the connection
+// Requester is a connection that can send requests and subscribe to notifications
+type Requester interface {
+	// Close closes the connection
 	Close()
+
+	// Request sends data for a topic and returns the response data or an error
+	Request(uint64, []byte) ([]byte, error)
+	// Subscribe listens for notifications for a topic and does not respond to notifications
+	Subscribe() (<-chan []byte, error)
+}
+
+// Responder is a connection that can respond to requests and send notifications
+type Responder interface {
+	// Close closes the connection
+	Close()
+	// Url returns the url that the responder is listening on
+	Url() string
+
+	// Respond listens for requests for a topic and calls the handler function when a request is received
+	Respond(func([]byte) []byte) error
+	// Notify sends data for a topic without expecting a response
+	Notify([]byte) error
 }
