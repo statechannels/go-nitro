dd5a11f67f6735a6166b07ab749e455bbf1d3a65
Alex Gap <me@alexgap.ca>
2023-02-21 14:34:54 -0800

    ignore challenge registered event

--- client/engine/chainservice/eth_chainservice.go
+++ client/engine/chainservice/eth_chainservice.go
@@ -25,6 +25,7 @@ import (
 var allocationUpdatedTopic = crypto.Keccak256Hash([]byte("AllocationUpdated(bytes32,uint256,uint256)"))
 var concludedTopic = crypto.Keccak256Hash([]byte("Concluded(bytes32,uint48)"))
 var depositedTopic = crypto.Keccak256Hash([]byte("Deposited(bytes32,address,uint256,uint256)"))
+var challengeRegisteredTopic = crypto.Keccak256Hash([]byte("ChallengeRegistered(bytes32 indexed channelId, uint48 turnNumRecord, uint48 finalizesAt, bool isFinal, (address[],uint64,address,uint48) fixedPart, (((address,(uint8,bytes),(bytes32,uint256,uint8,bytes)[])[],bytes,uint48,bool),(uint8,bytes32,bytes32)[])[] proof, (((address,(uint8,bytes),(bytes32,uint256,uint8,bytes)[])[],bytes,uint48,bool),(uint8,bytes32,bytes32)[]) candidate)"))
 
 type ethChain interface {
 	bind.ContractBackend
@@ -227,6 +228,9 @@ func (ecs *EthChainService) dispatchChainEvents(logs []ethTypes.Log) {
 
 			event := ConcludedEvent{commonEvent: commonEvent{channelID: ce.ChannelId, BlockNum: l.BlockNumber}}
 			ecs.out <- event
+
+		case challengeRegisteredTopic:
+			ecs.logger.Info().Msg("Ignoring Challenge registered event")
 
 		default:
 			ecs.fatalF("Unknown chain event %+v", l)
