e301f494d49249ca8e57f09a73229489fda84993
Alex Gap <me@alexgap.ca>
2023-03-10 09:17:21 -0800

    allow config file to be passed in

--- client_test/crash_test.go
+++ client_test/crash_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/statechannels/go-nitro/client/engine/messageservice"
 	"github.com/statechannels/go-nitro/client/engine/store"
 	"github.com/statechannels/go-nitro/types"
+	"github.com/tidwall/buntdb"
 )
 
 func TestCrashTolerance(t *testing.T) {
@@ -45,7 +46,7 @@ func TestCrashTolerance(t *testing.T) {
 	defer os.RemoveAll(dataFolder)
 
 	// Client setup
-	storeA := store.NewPersistStore(alice.PrivateKey, dataFolder)
+	storeA := store.NewPersistStore(alice.PrivateKey, dataFolder, buntdb.Config{SyncPolicy: buntdb.Always})
 	messageserviceA := messageservice.NewTestMessageService(alice.Address(), broker, 0)
 	clientA := client.New(messageserviceA, chainA, storeA, logDestination, &engine.PermissivePolicy{}, nil)
 
@@ -59,7 +60,7 @@ func TestCrashTolerance(t *testing.T) {
 		clientA.Close()
 		anotherMessageserviceA := messageservice.NewTestMessageService(alice.Address(), broker, 0)
 		anotherChainA, err := chainservice.NewSimulatedBackendChainService(sim, bindings, ethAccounts[0], logDestination)
-		anotherStoreA := store.NewPersistStore(alice.PrivateKey, dataFolder)
+		anotherStoreA := store.NewPersistStore(alice.PrivateKey, dataFolder, buntdb.Config{SyncPolicy: buntdb.Always})
 		if err != nil {
 			t.Fatal(err)
 		}
