c33f9286c511867252e6d45d6ee5c65e4a577423
Mike Kerzhner <mikerzhner@gmail.com>
2023-03-16 12:56:39 -0600

    Add request validation to jsonrpc server

--- rpc/error.go
+++ rpc/error.go
@@ -3,6 +3,7 @@ package rpc
 import "github.com/statechannels/go-nitro/rpc/serde"
 
 var parseError = serde.JsonRpcError{Code: -32700, Message: "Parse error"}
+var invalidRequestError = serde.JsonRpcError{Code: -32600, Message: "Invalid Request"}
 var methodNotFoundError = serde.JsonRpcError{Code: -32601, Message: "Method not found"}
 var unexpectedRequestUnmarshalError = serde.JsonRpcError{Code: -32010, Message: "Unexpected unmarshal error"}
 var unexpectedRequestUnmarshalError2 = serde.JsonRpcError{Code: -32009, Message: "Unexpected unmarshal error"}
