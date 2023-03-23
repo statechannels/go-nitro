2303fbdb891b9be88bde421eaa67513f8eedbca3
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-03-23 11:41:31 -0300

    regenerate go bindings

--- client/engine/chainservice/erc20/Token.go
+++ client/engine/chainservice/erc20/Token.go
@@ -26,7 +26,6 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // TokenMetaData contains all meta data concerning the Token contract.
@@ -157,11 +156,11 @@ func NewTokenFilterer(address common.Address, filterer bind.ContractFilterer) (*
 
 // bindToken binds a generic wrapper to an already deployed contract.
 func bindToken(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := TokenMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(TokenABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
