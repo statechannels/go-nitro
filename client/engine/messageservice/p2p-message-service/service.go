1abb837d7ef609c7b6e800827b21789318e36cb0
Alex Gap <me@alexgap.ca>
2023-04-11 12:16:01 -0700

    simplify peer exchange

--- client/engine/messageservice/p2p-message-service/service.go
+++ client/engine/messageservice/p2p-message-service/service.go
@@ -41,9 +41,9 @@ const (
 
 // P2PMessageService is a rudimentary message service that uses TCP to send and receive messages.
 type P2PMessageService struct {
-	toEngine    chan protocols.Message // for forwarding processed messages to the engine
-	peers       *safesync.Map[PeerInfo]
-	gossipedTo  *safesync.Map[bool]
+	toEngine chan protocols.Message // for forwarding processed messages to the engine
+	peers    *safesync.Map[PeerInfo]
+
 	me          types.Address
 	key         p2pcrypto.PrivKey
 	p2pHost     host.Host
@@ -64,7 +64,6 @@ func NewMessageService(ip string, port int, me types.Address, pk []byte) *P2PMes
 		toEngine:    make(chan protocols.Message, BUFFER_SIZE),
 		newPeerInfo: make(chan PeerInfo, BUFFER_SIZE),
 		peers:       &safesync.Map[PeerInfo]{},
-		gossipedTo:  &safesync.Map[bool]{},
 		me:          me,
 	}
 
@@ -85,9 +84,6 @@ func NewMessageService(ip string, port int, me types.Address, pk []byte) *P2PMes
 		panic(err)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	ms.cancel = cancel
-
 	mdns := mdns.NewMdnsService(host, "", ms)
 	if err := mdns.Start(); err != nil {
 		panic(err)
@@ -96,17 +92,19 @@ func NewMessageService(ip string, port int, me types.Address, pk []byte) *P2PMes
 	ms.p2pHost = host
 
 	ms.p2pHost.SetStreamHandler(PROTOCOL_ID, ms.msgStreamHandler)
-	ms.p2pHost.SetStreamHandler(PEER_EXCHANGE_PROTOCOL_ID, ms.gossipStreamHandler)
-
-	go ms.gossip(ctx)
+	ms.p2pHost.SetStreamHandler(PEER_EXCHANGE_PROTOCOL_ID, ms.peerExchangeHandler)
 
 	return ms
 }
 
 // HandlePeerFound is called by the mDNS service when a peer is found.
 func (ms *P2PMessageService) HandlePeerFound(pi peer.AddrInfo) {
-	fmt.Printf("Found peer %v\n", pi)
 	ms.p2pHost.Peerstore().AddAddr(pi.ID, pi.Addrs[0], peerstore.PermanentAddrTTL)
+	stream, err := ms.p2pHost.NewStream(context.Background(), pi.ID, PEER_EXCHANGE_PROTOCOL_ID)
+	ms.checkError(err)
+	ms.sendPeerInfo(stream)
+	ms.receivePeerInfo(stream)
+	stream.Close()
 }
 
 func (ms *P2PMessageService) msgStreamHandler(stream network.Stream) {
@@ -126,7 +124,29 @@ func (ms *P2PMessageService) msgStreamHandler(stream network.Stream) {
 	stream.Close()
 }
 
-func (ms *P2PMessageService) gossipStreamHandler(stream network.Stream) {
+func (ms *P2PMessageService) peerExchangeHandler(stream network.Stream) {
+	ms.receivePeerInfo(stream)
+	ms.sendPeerInfo(stream)
+	stream.Close()
+}
+
+// sendPeerInfo sends our peer info over the given stream
+func (ms *P2PMessageService) sendPeerInfo(stream network.Stream) {
+	raw, err := json.Marshal(PeerInfo{
+		Id:      ms.Id(),
+		Address: ms.me,
+	})
+
+	ms.checkError(err)
+	writer := bufio.NewWriter(stream)
+	// We don't care about the number of bytes written
+	_, err = writer.WriteString(string(raw) + string(DELIMITER))
+	ms.checkError(err)
+	writer.Flush()
+}
+
+// receivePeerInfo receives peer info from the given stream
+func (ms *P2PMessageService) receivePeerInfo(stream network.Stream) {
 	reader := bufio.NewReader(stream)
 	// Create a buffer stream for non blocking read and write.
 	raw, err := reader.ReadString(DELIMITER)
@@ -142,49 +162,12 @@ func (ms *P2PMessageService) gossipStreamHandler(stream network.Stream) {
 	err = json.Unmarshal([]byte(raw), &peerInfo)
 	ms.checkError(err)
 
-	fmt.Printf("Received peer info: %v\n", peerInfo)
+	if _, hasPeer := ms.peers.Load(peerInfo.Address.String()); !hasPeer {
+		fmt.Printf("Adding info received from peer: %v\n", peerInfo)
 
-	ms.peers.Store(peerInfo.Address.String(), *peerInfo)
-	ms.newPeerInfo <- *peerInfo
-
-	stream.Close()
-}
-
-func (ms *P2PMessageService) gossip(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-time.After(PEER_EXCHANGE_SLEEP_DURATION):
-
-			for _, p := range ms.p2pHost.Peerstore().Peers() {
-				alreadyMessaged, _ := ms.gossipedTo.Load(p.String())
-				if p != ms.Id() && !alreadyMessaged {
-					ms.gossipedTo.Store(p.String(), true)
-					ms.sendInfoToPeer(p)
-				}
-			}
-		}
+		ms.peers.Store(peerInfo.Address.String(), *peerInfo)
+		ms.newPeerInfo <- *peerInfo
 	}
-}
-
-// sendInfoToPeer sends our peer info to the given peer.
-func (ms *P2PMessageService) sendInfoToPeer(id peer.ID) {
-	s, err := ms.p2pHost.NewStream(context.Background(), id, PEER_EXCHANGE_PROTOCOL_ID)
-	ms.checkError(err)
-
-	raw, err := json.Marshal(PeerInfo{
-		Id:      ms.Id(),
-		Address: ms.me,
-	})
-
-	ms.checkError(err)
-	writer := bufio.NewWriter(s)
-	// We don't care about the number of bytes written
-	_, err = writer.WriteString(string(raw) + string(DELIMITER))
-	ms.checkError(err)
-	writer.Flush()
-	s.Close()
 }
 
 // Send sends messages to other participants.
