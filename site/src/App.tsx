5ca833c21ec07f75824061c4f12de650f0db952e
Mike Kerzhner <mikerzhner@gmail.com>
2023-05-04 09:49:13 -0600

    site uses nitro client

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import statechannelsLogo from "./assets/statechannels.svg";
 import "./App.css";
 import { NetworkBalance } from "./components/NetworkBalance";
+
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,9 +12,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
