6535b4839ab6e4fb1640947d0f6502171b79dab1
Alex Gap <me@alexgap.ca>
2023-05-09 10:57:45 -0700

    add go version of the script

---
+++ scripts/start-rpc-servers.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+type participant string
+
+const (
+	alice participant = "alice"
+	bob   participant = "bob"
+	irene participant = "irene"
+)
+
+type color string
+
+const (
+	black   color = "[30m"
+	red     color = "[31m"
+	green   color = "[32m"
+	yellow  color = "[33m"
+	blue    color = "[34m"
+	magenta color = "[35m"
+	cyan    color = "[36m"
+	white   color = "[37m"
+	gray    color = "[90m"
+)
+
+func main() {
+	chainCmd := exec.Command("anvil", "--chain-id", "1337")
+	chainCmd.Stdout = os.Stdout
+	chainCmd.Stderr = os.Stderr
+	err := chainCmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	setupRPCServer(alice, blue)
+	setupRPCServer(irene, green)
+	setupRPCServer(bob, yellow)
+
+	waitForKillSignal()
+}
+
+// waitForKillSignal blocks until we receive a kill or interrupt signal
+func waitForKillSignal() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigs
+	fmt.Printf("Received signal %s, exiting..", sig)
+}
+
+// setupRPCServer starts up an RPC server for the given participant
+func setupRPCServer(p participant, c color) *exec.Cmd {
+	var cmd *exec.Cmd
+	switch p {
+	case alice:
+		cmd = exec.Command("go", strings.Split("run . -autodeploy -usedurablestore", " ")...)
+	case irene:
+		cmd = exec.Command("go", strings.Split("run . -autodeploy -usedurablestore -msgport 3006 -rpcport 4006 -pk febb3b74b0b52d0976f6571d555f4ac8b91c308dfa25c7b58d1e6a7c3f50c781", " ")...)
+	case bob:
+		cmd = exec.Command("go", strings.Split("run . -autodeploy -usedurablestore -msgport 3007 -rpcport 4007 -pk 0279651921cd800ac560c21ceea27aab0107b67daf436cdd25ce84cad30159b4", " ")...)
+	default:
+		panic("Invalid participant")
+
+	}
+	cmd.Stdout = newColorWriter(c, os.Stdout)
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+	return cmd
+}
+
+// colorWriter is a writer that writes to the underlying writer with the given color
+type colorWriter struct {
+	writer io.Writer
+	color  color
+}
+
+func (cw colorWriter) Write(p []byte) (n int, err error) {
+	_, err = cw.writer.Write([]byte("\033" + string(cw.color) + string(p) + "\033[0m"))
+	if err != nil {
+		return 0, err
+	}
+	return len(p), nil
+}
+
+func newColorWriter(c color, w io.Writer) colorWriter {
+	return colorWriter{
+		writer: w,
+		color:  c,
+	}
+}
