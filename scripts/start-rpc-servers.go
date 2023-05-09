afc2716580eddff753908275b3673e637f9d1a8c
Alex Gap <me@alexgap.ca>
2023-05-09 11:52:36 -0700

    stop commands when exiting

--- scripts/start-rpc-servers.go
+++ scripts/start-rpc-servers.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
-	"strings"
 	"syscall"
 )
 
@@ -33,19 +32,57 @@ const (
 )
 
 func main() {
+	running := []*exec.Cmd{}
+
 	chainCmd := exec.Command("anvil", "--chain-id", "1337")
 	chainCmd.Stdout = os.Stdout
 	chainCmd.Stderr = os.Stderr
 	err := chainCmd.Start()
 	if err != nil {
+		stopCommands(running...)
 		panic(err)
 	}
+	running = append(running, chainCmd)
 
-	setupRPCServer(alice, blue)
-	setupRPCServer(irene, green)
-	setupRPCServer(bob, yellow)
+	aliceClient, err := setupRPCServer(alice, blue)
+	if err != nil {
+		stopCommands(running...)
+		panic(err)
+	}
+	running = append(running, aliceClient)
+
+	ireneClient, err := setupRPCServer(irene, green)
+	if err != nil {
+		stopCommands(running...)
+		panic(err)
+	}
+	running = append(running, ireneClient)
+
+	bobClient, err := setupRPCServer(bob, yellow)
+	if err != nil {
+		stopCommands(running...)
+		panic(err)
+	}
+	running = append(running, bobClient)
 
 	waitForKillSignal()
+
+	stopCommands(running...)
+}
+
+// stopCommands stops the given executing commands
+func stopCommands(cmds ...*exec.Cmd) {
+	for _, cmd := range cmds {
+		fmt.Printf("Stopping process %v\n", cmd.Args)
+		err := cmd.Process.Signal(syscall.SIGINT)
+		if err != nil {
+			panic(err)
+		}
+		err = cmd.Process.Kill()
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 // waitForKillSignal blocks until we receive a kill or interrupt signal
@@ -53,30 +90,41 @@ func waitForKillSignal() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	sig := <-sigs
-	fmt.Printf("Received signal %s, exiting..", sig)
+	fmt.Printf("Received signal %s, exiting..\n", sig)
 }
 
 // setupRPCServer starts up an RPC server for the given participant
-func setupRPCServer(p participant, c color) *exec.Cmd {
-	var cmd *exec.Cmd
+func setupRPCServer(p participant, c color) (*exec.Cmd, error) {
+	args := []string{"run", ".", "-usedurablestore"}
+
 	switch p {
 	case alice:
-		cmd = exec.Command("go", strings.Split("run . -autodeploy -usedurablestore", " ")...)
+		args = append(args, "-msgport", "3005")
+		args = append(args, "-rpcport", "4005")
+		args = append(args, "-pk", "2d999770f7b5d49b694080f987b82bbc9fc9ac2b4dcc10b0f8aba7d700f69c6d")
+
 	case irene:
-		cmd = exec.Command("go", strings.Split("run . -autodeploy -usedurablestore -msgport 3006 -rpcport 4006 -pk febb3b74b0b52d0976f6571d555f4ac8b91c308dfa25c7b58d1e6a7c3f50c781", " ")...)
+		args = append(args, "-msgport", "3006")
+		args = append(args, "-rpcport", "4006")
+		args = append(args, "-pk", "febb3b74b0b52d0976f6571d555f4ac8b91c308dfa25c7b58d1e6a7c3f50c781")
+
 	case bob:
-		cmd = exec.Command("go", strings.Split("run . -autodeploy -usedurablestore -msgport 3007 -rpcport 4007 -pk 0279651921cd800ac560c21ceea27aab0107b67daf436cdd25ce84cad30159b4", " ")...)
+		args = append(args, "-msgport", "3007")
+		args = append(args, "-rpcport", "4007")
+		args = append(args, "-pk", "0279651921cd800ac560c21ceea27aab0107b67daf436cdd25ce84cad30159b4")
+
 	default:
 		panic("Invalid participant")
 
 	}
+	cmd := exec.Command("go", args...)
 	cmd.Stdout = newColorWriter(c, os.Stdout)
 	cmd.Stderr = os.Stderr
 	err := cmd.Start()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return cmd
+	return cmd, nil
 }
 
 // colorWriter is a writer that writes to the underlying writer with the given color
