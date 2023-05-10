de23a0035c5508d11422ee65f004cb90bb9a3533
Mike Kerzhner <mikerzhner@gmail.com>
2023-05-09 19:11:10 -0600

    Remove eslint overrides

--- site/.eslintrc.cjs
+++ site/.eslintrc.cjs
@@ -5,7 +5,6 @@ module.exports = {
     es2020: true,
   },
   extends: [
-    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:storybook/recommended",
