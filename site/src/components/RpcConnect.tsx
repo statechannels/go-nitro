b0d13eab8d496135ad4aea91313328a5fc7e067b
Mike Kerzhner <mikerzhner@gmail.com>
2023-05-12 06:25:55 -0600

    Use material UI for RPCConnect component

--- site/src/components/RpcConnect.tsx
+++ site/src/components/RpcConnect.tsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import * as React from "react";
 
 export default function RpcConnect() {
   const [text, setText] = useState("localhost:4005");
@@ -8,16 +12,17 @@ export default function RpcConnect() {
   }
 
   return (
-    <div>
-      RPC Connect
-      <input value={text} onChange={handleChange} />
-      <button
+    <Box>
+      Nitro RPC Connect:
+      <TextField value={text} onChange={handleChange} />
+      <Button
+        variant="contained"
         onClick={() => {
           console.log("Button click");
         }}
       >
         Connect
-      </button>
-    </div>
+      </Button>
+    </Box>
   );
 }
--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import App from "./App.tsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
