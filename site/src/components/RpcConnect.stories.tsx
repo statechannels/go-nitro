105666ae3459587e588a59f338f643f7dae1749e
George Knee <georgeknee@googlemail.com>
2023-05-15 17:35:48 +0100

    fix story

--- site/src/components/RpcConnect.stories.tsx
+++ site/src/components/RpcConnect.stories.tsx
@@ -11,9 +11,6 @@ export default meta;
 
 type Story = StoryObj<typeof RpcConnect>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const setUrl = function (_url: string) {};
-
 export const Primary: Story = {
-  render: () => <RpcConnect url="localhost:8545" setUrl={setUrl} />,
+  render: () => <RpcConnect url="localhost:8545" />,
 };
