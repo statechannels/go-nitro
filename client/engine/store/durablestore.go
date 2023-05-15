7f990e26a98311c5d8503479b0b713ab0623b45f
Alex Gap <me@alexgap.ca>
2023-05-15 16:44:51 -0700

    catch storing an empty con channel

--- client/engine/store/durablestore.go
+++ client/engine/store/durablestore.go
@@ -212,6 +212,9 @@ func (ds *DurableStore) DestroyChannel(id types.Destination) {
 
 // SetConsensusChannel sets the channel in the store.
 func (ps *DurableStore) SetConsensusChannel(ch *consensus_channel.ConsensusChannel) error {
+	if ch.Id.IsZero() {
+		return fmt.Errorf("cannot store a channel with a zero id")
+	}
 	chJSON, err := ch.MarshalJSON()
 	if err != nil {
 		return err
