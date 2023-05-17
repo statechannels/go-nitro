17cc518098f8e3fd58c02e826b447e29024d4ed6
Mike Kerzhner <mikerzhner@gmail.com>
2023-05-17 08:59:57 -0600

    Fetch ledger channels from nitro node

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { NitroRpcClient } from "@statechannels/nitro-rpc-client";
+import { LedgerChannelInfo } from "@statechannels/nitro-rpc-client/src/types";
 
 import "./App.css";
 import TopBar from "./components/TopBar";
@@ -7,18 +8,6 @@ import { QUERY_KEY } from "./constants";
 import PaymentChannelList from "./components/PaymentChannelList";
 import PaymentChannelDetails from "./components/PaymentChannelDetails";
 import LedgerChannelDetails from "./components/LedgerChannelDetails";
-
-const ledgerChannels = [
-  {
-    ID: "0x9823fa3d37ec304f90d1bef2c03c1fc70f86b6417f022d5e9ab88902a874f0cc",
-  },
-  {
-    ID: "0x06a508ca629080f81954bb4dcce6b71f1d8de0dded88d333c720d3b9d4067af0",
-  },
-  {
-    ID: "0x06a508ca629080f81954bb4dcce6b71f1d8de0dded88d333c720d3b9d4067af1",
-  },
-];
 
 const paymentChannels = [
   {
@@ -39,6 +28,7 @@ function App() {
   const [nitroClient, setNitroClient] = useState<NitroRpcClient | null>(null);
   const [version, setVersion] = useState("");
   const [address, setAddress] = useState("");
+  const [ledgerChannels, setLedgerChannels] = useState<LedgerChannelInfo[]>([]);
   const [focusedLedgerChannel, setFocusedLedgerChannel] = useState<string>(
     "0x9823fa3d37ec304f90d1bef2c03c1fc70f86b6417f022d5e9ab88902a874f0cc"
   );
@@ -55,6 +45,7 @@ function App() {
     if (nitroClient) {
       nitroClient.GetVersion().then((v) => setVersion(v));
       nitroClient.GetAddress().then((a) => setAddress(a));
+      nitroClient.GetAllLedgerChannels().then((l) => setLedgerChannels(l));
     }
   }, [nitroClient]);
 
