d52b3391efa13e82c64c59a8112996d230b6d2e7
Mike Kerzhner <mikerzhner@gmail.com>
2023-05-19 09:43:54 -0600

    Add MSW Storybook Addon

--- site/.storybook/preview.ts
+++ site/.storybook/preview.ts
@@ -1,4 +1,11 @@
 import type { Preview } from "@storybook/react";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 const preview: Preview = {
   parameters: {
