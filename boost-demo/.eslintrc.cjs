90ff9cfd3b791d25996320e323e0e8671b293e53
Alex Gap <me@alexgap.ca>
2023-05-30 17:08:16 -0700

    generate new boost-demo site

---
+++ boost-demo/.eslintrc.cjs
@@ -0,0 +1,17 @@
+/* global module */
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  ignorePatterns: ["!.storybook"],
+  extends: ["plugin:react-hooks/recommended", "plugin:storybook/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+  },
+};
