5b2e27857301460a6765348b36a0cc61533fec84
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-06-02 16:19:16 -0300

    update docstring on stateIsSupported

--- nitro-protocol/contracts/interfaces/IForceMoveApp.sol
+++ nitro-protocol/contracts/interfaces/IForceMoveApp.sol
@@ -9,7 +9,7 @@ import './INitroTypes.sol';
  */
 interface IForceMoveApp is INitroTypes {
     /**
-     * @notice Encodes application-specific rules for a particular ForceMove-compliant state channel. Must revert when invalid support proof and a candidate are supplied.
+     * @notice Encodes application-specific rules for a particular ForceMove-compliant state channel. Must revert or return false when invalid support proof and a candidate are supplied.
      * @dev Depending on the application, it might be desirable to narrow the state mutability of an implementation to 'pure' to make security analysis easier.
      * @param fixedPart Fixed part of the state channel.
      * @param proof Array of recovered variable parts which constitutes a support proof for the candidate. May be omitted when `candidate` constitutes a support proof itself.
