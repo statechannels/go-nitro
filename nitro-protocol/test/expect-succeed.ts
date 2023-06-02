d125e5ac0c7f6f783ca98df687de33764f391f88
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-06-02 12:36:14 -0300

    replace removed expectSucceed fcn, add jsDoc

--- nitro-protocol/test/expect-succeed.ts
+++ nitro-protocol/test/expect-succeed.ts
@@ -1,3 +1,15 @@
+/**
+ * Wrapper for transactions that are expected to succeed with no return values.
+ */
+export async function expectSucceed(fn: () => void) {
+  const txResult = (await fn()) as any;
+
+  expect(txResult.length).toBe(0);
+}
+
+/**
+ * Wrapper for calls to `stateIsSupported` that are expected to succeed.
+ */
 export async function expectSupportedState(fn: () => void) {
   const txResult = (await fn()) as any;
 
