d9ce587758e913428ce6e938b33428feb6d4322e
Mike Kerzhner <mikerzhner@gmail.com>
2023-06-06 13:55:35 -0600

    Update jest config to ignore node_modules and dist directories

--- nitro-rpc-client/jest.config.ts
+++ nitro-rpc-client/jest.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "@jest/types";
 // Sync object
 const config: Config.InitialOptions = {
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
+  preset: "ts-jest",
+  testEnvironment: "node",
   verbose: true,
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
 };
 export default config;
