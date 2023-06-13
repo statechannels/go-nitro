3656e1eaf8ba05283ca25b09f68e4683aa140ce9
Alex Gap <me@alexgap.ca>
2023-06-12 18:50:12 -0700

    do not set gas limit

--- node/engine/chainservice/utils/utils.go
+++ node/engine/chainservice/utils/utils.go
@@ -43,12 +43,15 @@ func ConnectToChain(ctx context.Context, chainUrl, chainAuthToken string, chainP
 	if err != nil {
 		return nil, nil, err
 	}
-	txSubmitter.GasLimit = uint64(30_000_000) // in units
 
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		return nil, nil, err
-	}
-	txSubmitter.GasPrice = gasPrice
+	// TODO: Filecoin only uses v2 transactions so we want to avoid setting a gas limit
+
+	// txSubmitter.GasLimit = uint64(30_000_000) // in units
+
+	// gasPrice, err := client.SuggestGasPrice(context.Background())
+	// if err != nil {
+	// 	return nil, nil, err
+	// }
+	// txSubmitter.GasPrice = gasPrice
 	return client, txSubmitter, nil
 }
