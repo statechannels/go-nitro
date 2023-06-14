672acf7d0549889dbc4d0d1343f0889673d9d7c6
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-06-14 11:08:26 -0300

    expose ExecuteSideEffects...
    This requires some thought. Are engine sideEffects generally presumed to
    be benign? Does this change open attack vectors?
    
    The same immediate benefit could be had by opening a vouchers-only pipe,
    but that feels more like api-creep than this general method.

--- client/engine/engine.go
+++ client/engine/engine.go
@@ -289,7 +289,7 @@ func (e *Engine) handleMessage(message protocols.Message) (EngineEvent, error) {
 
 				allCompleted.CompletedObjectives = append(allCompleted.CompletedObjectives, objective)
 
-				err = e.executeSideEffects(sideEffects)
+				err = e.ExecuteSideEffects(sideEffects)
 				// An error would mean we failed to send a message. But the objective is still "completed".
 				// So, we should return allCompleted even if there was an error.
 				return allCompleted, err
@@ -542,7 +542,7 @@ func (e *Engine) handlePaymentRequest(request PaymentRequest) (EngineEvent, erro
 	ee.PaymentChannelUpdates = append(ee.PaymentChannelUpdates, info)
 
 	se := protocols.SideEffects{MessagesToSend: protocols.CreateVoucherMessage(voucher, payee)}
-	return ee, e.executeSideEffects(se)
+	return ee, e.ExecuteSideEffects(se)
 }
 
 // sendMessages sends out the messages and records the metrics.
@@ -550,7 +550,7 @@ func (e *Engine) sendMessages(msgs []protocols.Message) {
 	defer e.metrics.RecordFunctionDuration()()
 
 	for _, message := range msgs {
-		message.From = *e.store.GetAddress()
+		message.From = *e.store.GetAddress() // read from disk on each msg: bottleneck?
 		e.logMessage(message, Outgoing)
 		e.recordMessageMetrics(message)
 		e.msg.Send(message)
@@ -558,8 +558,8 @@ func (e *Engine) sendMessages(msgs []protocols.Message) {
 	e.wg.Done()
 }
 
-// executeSideEffects executes the SideEffects declared by cranking an Objective or handling a payment request.
-func (e *Engine) executeSideEffects(sideEffects protocols.SideEffects) error {
+// ExecuteSideEffects executes the SideEffects declared by cranking an Objective or handling a payment request.
+func (e *Engine) ExecuteSideEffects(sideEffects protocols.SideEffects) error {
 	defer e.metrics.RecordFunctionDuration()()
 
 	e.wg.Add(1)
@@ -623,7 +623,7 @@ func (e *Engine) attemptProgress(objective protocols.Objective) (outgoing Engine
 			return
 		}
 	}
-	err = e.executeSideEffects(sideEffects)
+	err = e.ExecuteSideEffects(sideEffects)
 	return
 }
 
