fdc1ba291f29d2527905a390a1f232322bf48b88
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-07-10 14:11:17 -0300

    update hash calculation for `Deposited` events

--- node/engine/chainservice/eth_chainservice.go
+++ node/engine/chainservice/eth_chainservice.go
@@ -25,7 +25,7 @@ import (
 var (
 	allocationUpdatedTopic   = crypto.Keccak256Hash([]byte("AllocationUpdated(bytes32,uint256,uint256)"))
 	concludedTopic           = crypto.Keccak256Hash([]byte("Concluded(bytes32,uint48)"))
-	depositedTopic           = crypto.Keccak256Hash([]byte("Deposited(bytes32,address,uint256,uint256)"))
+	depositedTopic           = crypto.Keccak256Hash([]byte("Deposited(bytes32,address,uint256)"))
 	challengeRegisteredTopic = crypto.Keccak256Hash([]byte("ChallengeRegistered(bytes32 indexed channelId, uint48 turnNumRecord, uint48 finalizesAt, bool isFinal, (address[],uint64,address,uint48) fixedPart, (((address,(uint8,bytes),(bytes32,uint256,uint8,bytes)[])[],bytes,uint48,bool),(uint8,bytes32,bytes32)[])[] proof, (((address,(uint8,bytes),(bytes32,uint256,uint8,bytes)[])[],bytes,uint48,bool),(uint8,bytes32,bytes32)[]) candidate)"))
 	challengeClearedTopic    = crypto.Keccak256Hash([]byte("ChallengeCleared(bytes32 indexed channelId, uint48 newTurnNumRecord)"))
 )
