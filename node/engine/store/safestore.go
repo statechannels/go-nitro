c4a3de9916df87d46db0b9a180a81c3d0b500f4c
George Knee <georgeknee@googlemail.com>
2023-07-10 14:09:28 +0100

    use mutexes where possible
    many methods on the memstore are using sync.Map.Range. For those, we probably need to lock all mutexes or lock a special "everything" mutex?

--- node/engine/store/safestore.go
+++ node/engine/store/safestore.go
@@ -29,6 +29,9 @@ func (ss *SafeStore) GetAddress() *types.Address   { return (*common.Address)(ss
 
 // Objective store
 func (ss *SafeStore) GetObjectiveById(id protocols.ObjectiveId) (protocols.Objective, error) {
+	mu, _ := ss.mutexes.LoadOrStore(string(id), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
 	return ss.store.GetObjectiveById(id)
 }
 
@@ -36,21 +39,41 @@ func (ss *SafeStore) GetObjectiveByChannelId(id types.Destination) (obj protocol
 	return ss.store.GetObjectiveByChannelId(id)
 }
 
-func (ss *SafeStore) SetObjective(o protocols.Objective) error { return ss.store.SetObjective(o) }
+func (ss *SafeStore) SetObjective(o protocols.Objective) error {
+	mu, _ := ss.mutexes.LoadOrStore(string(o.Id()), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
+	return ss.store.SetObjective(o)
+}
 
 // Channel store
 func (ss *SafeStore) GetChannelsByIds(ids []types.Destination) ([]*channel.Channel, error) {
+	for _, id := range ids {
+		mu, _ := ss.mutexes.LoadOrStore(id.String(), &(sync.Mutex{}))
+		mu.Lock()
+		defer mu.Unlock()
+	}
 	return ss.store.GetChannelsByIds(ids)
 }
 
 func (ss *SafeStore) GetChannelById(id types.Destination) (c *channel.Channel, ok bool) {
+	mu, _ := ss.mutexes.LoadOrStore(id.String(), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
 	return ss.store.GetChannelById(id)
 }
 
 func (ss *SafeStore) GetChannelsByParticipant(participant types.Address) ([]*channel.Channel, error) {
+	// TODO lock all mutexes??
 	return ss.store.GetChannelsByParticipant(participant)
 }
-func (ss *SafeStore) SetChannel(c *channel.Channel) error       { return ss.store.SetChannel(c) }
+
+func (ss *SafeStore) SetChannel(c *channel.Channel) error {
+	mu, _ := ss.mutexes.LoadOrStore(c.Id.String(), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
+	return ss.store.SetChannel(c)
+}
 func (ss *SafeStore) DestroyChannel(id types.Destination) error { return ss.store.DestroyChannel(id) }
 
 func (ss *SafeStore) GetChannelsByAppDefinition(appDef types.Address) ([]*channel.Channel, error) {
@@ -58,28 +81,42 @@ func (ss *SafeStore) GetChannelsByAppDefinition(appDef types.Address) ([]*channe
 }
 
 func (ss *SafeStore) ReleaseChannelFromOwnership(id types.Destination) error {
+	mu, _ := ss.mutexes.LoadOrStore(id.String(), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
 	return ss.store.ReleaseChannelFromOwnership(id)
 }
 
 // Consensus Channel Store
 
 func (ss *SafeStore) GetAllConsensusChannels() ([]*consensus_channel.ConsensusChannel, error) {
+	// TODO lock all mutexes??
 	return ss.store.GetAllConsensusChannels()
 }
 
 func (ss *SafeStore) GetConsensusChannel(counterparty types.Address) (channel *consensus_channel.ConsensusChannel, ok bool) {
+	// TODO lock all mutexes??
 	return ss.store.GetConsensusChannel(counterparty)
 }
 
 func (ss *SafeStore) GetConsensusChannelById(id types.Destination) (channel *consensus_channel.ConsensusChannel, err error) {
+	mu, _ := ss.mutexes.LoadOrStore(id.String(), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
 	return ss.store.GetConsensusChannelById(id)
 }
 
 func (ss *SafeStore) SetConsensusChannel(c *consensus_channel.ConsensusChannel) error {
+	mu, _ := ss.mutexes.LoadOrStore(c.Id.String(), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
 	return ss.store.SetConsensusChannel(c)
 }
 
 func (ss *SafeStore) DestroyConsensusChannel(id types.Destination) error {
+	mu, _ := ss.mutexes.LoadOrStore(id.String(), &(sync.Mutex{}))
+	mu.Lock()
+	defer mu.Unlock()
 	return ss.store.DestroyConsensusChannel(id)
 }
 
