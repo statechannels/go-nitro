4bde30a25ccc59e12d8c88a0635e36231af533cf
Samuel Stokes <sam.adam.stokes@gmail.com>
2023-08-15 09:59:47 -0400

    Regenerate smart contract abi bindings with abigen v1.10.8

--- node/engine/chainservice/virtualpaymentapp/VirtualPaymentApp.go
+++ node/engine/chainservice/virtualpaymentapp/VirtualPaymentApp.go
@@ -26,7 +26,6 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // ExitFormatAllocation is an auto generated low-level Go binding around an user-defined struct.
@@ -200,11 +199,11 @@ func NewVirtualPaymentAppFilterer(address common.Address, filterer bind.Contract
 
 // bindVirtualPaymentApp binds a generic wrapper to an already deployed contract.
 func bindVirtualPaymentApp(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := VirtualPaymentAppMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(VirtualPaymentAppABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
