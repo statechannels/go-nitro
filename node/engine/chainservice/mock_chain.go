f02759a0e18f87b1d56d28ed0c25113dc3d94542
Mike Kerzhner <mikerzhner@gmail.com>
2023-08-18 13:01:14 -0600

    holdings is converted to a map since reads/writes are protected by a mutex

--- node/engine/chainservice/mock_chain.go
+++ node/engine/chainservice/mock_chain.go
@@ -15,7 +15,7 @@ import (
 type MockChain struct {
 	blockNum uint64
 	// holdings tracks funds for each channel.
-	holdings safesync.Map[types.Funds]
+	holdings map[types.Destination]types.Funds
 	// out maps addresses to an Event channel. Given that MockChainServices only subscribe
 	// (and never unsubscribe) to events, this can be converted to a list.
 	out safesync.Map[chan Event]
@@ -27,7 +27,7 @@ type MockChain struct {
 func NewMockChain() *MockChain {
 	chain := MockChain{}
 	chain.blockNum = 1
-	chain.holdings = safesync.Map[types.Funds]{}
+	chain.holdings = map[types.Destination]types.Funds{}
 	chain.out = safesync.Map[chan Event]{}
 	return &chain
 }
@@ -38,12 +38,11 @@ func (mc *MockChain) SubmitTransaction(tx protocols.ChainTransaction) error {
 	eventsToBroadcast := []Event{}
 	mc.txMutex.Lock()
 	mc.blockNum++
-	channelIdString := tx.ChannelId().String()
-	h, _ := mc.holdings.Load(channelIdString) // ignore `ok` because the returned zero-value is what we want
+	h := mc.holdings[tx.ChannelId()] // ignore `ok` because the returned zero-value is what we want
 	switch tx := tx.(type) {
 	case protocols.DepositTransaction:
 		if tx.Deposit.IsNonZero() {
-			mc.holdings.Store(channelIdString, h.Add(tx.Deposit))
+			mc.holdings[tx.ChannelId()] = h.Add(tx.Deposit)
 		}
 
 		for address := range tx.Deposit {
@@ -55,7 +54,7 @@ func (mc *MockChain) SubmitTransaction(tx protocols.ChainTransaction) error {
 			event := NewAllocationUpdatedEvent(tx.ChannelId(), mc.blockNum, assetAddress, common.Big0)
 			eventsToBroadcast = append(eventsToBroadcast, event)
 		}
-		mc.holdings.Store(channelIdString, types.Funds{})
+		mc.holdings[tx.ChannelId()] = types.Funds{}
 	default:
 		return fmt.Errorf("unexpected transaction type %T", tx)
 	}
