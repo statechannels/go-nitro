62249c5d25817ed56a6955ea4f9adaf2361b7881
lalexgap <me@alexgap.ca>
2023-08-28 11:50:08 -0700

    Improve logging

--- node/engine/policy/policy.go
+++ node/engine/policy/policy.go
@@ -86,6 +86,7 @@ func (fp *FairOutcomePolicy) ShouldApprove(o protocols.Objective) bool {
 			forMe := e.TotalAllocatedFor(types.AddressToDestination(fp.me))
 			for _, a := range e.Allocations {
 				if a.Amount.Cmp(forMe) != 0 {
+					slog.Warn("FairOutcomePolicy: rejecting directfund objective with unequal allocations", "objective", o.Id(), "allocations", e.Allocations, logging.WithObjectiveIdAttribute(o.Id()))
 					return false
 				}
 			}
@@ -98,8 +99,10 @@ func (fp *FairOutcomePolicy) ShouldApprove(o protocols.Objective) bool {
 			total := e.TotalAllocated()
 			for i, a := range e.Allocations {
 				if i == 0 && a.Amount.Cmp(total) != 0 {
+					slog.Warn("FairOutcomePolicy: rejecting virtualfund objective, expected payer to start with full amount", "allocations", e.Allocations, logging.WithObjectiveIdAttribute(o.Id()))
 					return false
 				} else if i > 0 && a.Amount.Cmp(big.NewInt(0)) != 0 {
+					slog.Warn("FairOutcomePolicy: rejecting virtualfund objective, expected payee to start with 0", "allocations", e.Allocations, logging.WithObjectiveIdAttribute(o.Id()))
 					return false
 				}
 			}
