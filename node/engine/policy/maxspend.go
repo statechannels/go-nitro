ea3aa746b194421651197bb60d46523a2bc8a2ef
lalexgap <me@alexgap.ca>
2023-08-28 12:57:29 -0700

    implement max spend policy

---
+++ node/engine/policy/maxspend.go
@@ -0,0 +1,62 @@
+package policy
+
+import (
+	"github.com/statechannels/go-nitro/payments"
+	"github.com/statechannels/go-nitro/protocols"
+	"github.com/statechannels/go-nitro/protocols/directfund"
+	"github.com/statechannels/go-nitro/protocols/virtualfund"
+	"github.com/statechannels/go-nitro/types"
+)
+
+func NewLedgerChannelMaxSpendPolicy(maxAmount types.Funds) PolicyMaker {
+	return &maxSpendPolicy{maxAmount: maxAmount, channelType: ledger}
+}
+
+func NewPaymentChannelMaxSpendPolicy(maxAmount types.Funds) PolicyMaker {
+	return &maxSpendPolicy{maxAmount: maxAmount, channelType: payment}
+}
+
+type channelType string
+
+const (
+	ledger  channelType = "ledger"
+	payment channelType = "payment"
+)
+
+type maxSpendPolicy struct {
+	me          types.Address
+	maxAmount   types.Funds
+	channelType channelType
+}
+
+func (mp *maxSpendPolicy) ShouldApprove(o protocols.Objective) bool {
+	switch obj := o.(type) {
+	case *virtualfund.Objective:
+		if mp.channelType == payment && obj.MyRole == payments.PAYER_INDEX {
+			myAmount := obj.V.PreFundState().Outcome.TotalAllocatedFor(types.AddressToDestination(mp.me))
+
+			if !underMaxAmount(myAmount, mp.maxAmount) {
+				return false
+			}
+		}
+	case *directfund.Objective:
+		if mp.channelType == ledger {
+			myAmount := obj.C.PreFundState().Outcome.TotalAllocatedFor(types.AddressToDestination(mp.me))
+			if !underMaxAmount(myAmount, mp.maxAmount) {
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
+
+func underMaxAmount(myAmount types.Funds, maxAmount types.Funds) bool {
+	for asset, amt := range myAmount {
+		if amt.Cmp(maxAmount[asset]) > 0 {
+			return false
+		}
+	}
+	return true
+}
