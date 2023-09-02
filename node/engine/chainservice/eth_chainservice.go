c7f2881cba9d382b0578cca564449858ae40bb71
Mike Kerzhner <mikerzhner@gmail.com>
2023-09-01 20:16:32 -0600

    Add back commit https://github.com/statechannels/go-nitro/commit/06eab9aa468262889fc4a00179858225327469fe

--- node/engine/chainservice/eth_chainservice.go
+++ node/engine/chainservice/eth_chainservice.go
@@ -358,7 +358,7 @@ out:
 				errorChan <- fmt.Errorf("subscribeFilterLogs failed on resubscribe: %w", err)
 				break out
 			}
-			ecs.logger.Debug("resubscribed to filtered event logs")
+			ecs.logger.Log(context.Background(), logging.LevelTrace, "resubscribed to filtered event logs")
 
 		case <-time.After(RESUB_INTERVAL):
 			// Due to https://github.com/ethereum/go-ethereum/issues/23845 we can't rely on a long running subscription.
@@ -395,11 +395,11 @@ out:
 				errorChan <- fmt.Errorf("subscribeNewHead failed on resubscribe: %w", err)
 				break out
 			}
-			ecs.logger.Debug("resubscribed to new blocks")
+			ecs.logger.Log(context.Background(), logging.LevelTrace, "resubscribed to new blocks")
 
 		case newBlock := <-newBlockChan:
 			newBlockNum := newBlock.Number.Uint64()
-			ecs.logger.Debug("detected new block", "block-num", newBlockNum)
+			ecs.logger.Log(context.Background(), logging.LevelTrace, "detected new block", "block-num", newBlockNum)
 			ecs.updateEventTracker(errorChan, &newBlockNum, nil)
 		}
 	}
