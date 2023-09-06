53e78cdaec9768e550911359c7e9d54f3b4248e5
lalexgap <me@alexgap.ca>
2023-09-06 13:11:50 -0700

    clean up config file warning

--- packages/nitro-protocol/config/jest/jest.config.js
+++ packages/nitro-protocol/config/jest/jest.config.js
@@ -10,11 +10,16 @@ const root = resolve(__dirname, '../../');
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsconfig: './test/tsconfig.json',
-    },
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './test/tsconfig.json',
+      },
+    ],
   },
+
   rootDir: root,
   collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}'],
   reporters: ['default'],
