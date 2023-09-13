0a7a31161aac2968ebd90d8981eb0b3e651037c1
George Knee <georgeknee@googlemail.com>
2023-09-13 14:06:11 +0100

    get around storybook / bigint problems

--- packages/nitro-gui/src/components/PaymentChannelDetails.stories.tsx
+++ packages/nitro-gui/src/components/PaymentChannelDetails.stories.tsx
@@ -1,37 +1,43 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ChannelStatus } from "@statechannels/nitro-rpc-client/src/types";
+import { FC } from "react";
 
 import PaymentChannelDetails from "./PaymentChannelDetails";
 import { PaymentChannelType } from "./PaymentChannelContainer";
 
-const meta: Meta<typeof PaymentChannelDetails> = {
+const wrappedComponent: FC<{ t: PaymentChannelType }> = (p) => {
+  return (
+    <PaymentChannelDetails
+      type={p.t}
+      channelID="fa745d81208c3f9f394a04db57a27f11c46be1d6dce0f81dd2852347d83fe4e4"
+      payer="b25e8dc6f4795e9441b3e0b2519f2c9c827eb734"
+      payee="b55e8dc6f4795e9441b3e0b2519f2c9c827eb734"
+      remainingFunds={BigInt(850)}
+      paidSoFar={BigInt(150)}
+      status={"Ready" as ChannelStatus}
+    />
+  );
+};
+
+const meta: Meta<typeof wrappedComponent> = {
   title: "PaymentChannelDetails",
-  component: PaymentChannelDetails,
+  component: wrappedComponent,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof PaymentChannelDetails>;
+type Story = StoryObj<typeof wrappedComponent>;
 
 export const PaymentChannelDetailsComponent: Story = {
-  args: {
-    channelID:
-      "fa745d81208c3f9f394a04db57a27f11c46be1d6dce0f81dd2852347d83fe4e4",
-    payer: "b25e8dc6f4795e9441b3e0b2519f2c9c827eb734",
-    payee: "b55e8dc6f4795e9441b3e0b2519f2c9c827eb734",
-    remainingFunds: BigInt(850),
-    paidSoFar: BigInt(150),
-    status: "Ready" as ChannelStatus,
-    type: PaymentChannelType.inbound,
-  },
   argTypes: {
-    type: {
+    t: {
       options: [
         PaymentChannelType.inbound,
         PaymentChannelType.outbound,
         PaymentChannelType.mediated,
       ],
       control: { type: "radio" },
+      defaultValue: PaymentChannelType.inbound,
     },
   },
 };
