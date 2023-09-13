e33df636d1f4a905cd67ba2cba369faaed0fbbc4
lalexgap <me@alexgap.ca>
2023-09-12 18:02:56 -0700

    use env vars for configuration

--- packages/payment-proxy-client/src/vite-env.d.ts
+++ packages/payment-proxy-client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_NITRO_RPC_URL: string;
+  readonly VITE_FILE_URL: string;
+  readonly VITE_PROVIDER: string;
+  readonly VITE_HUB: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
