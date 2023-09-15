b39cc1c61d0c2a18997fb3d2e4b776551042f289
George Knee <georgeknee@googlemail.com>
2023-09-15 12:36:44 +0100

    first attempt at wiring up app2

--- packages/payment-proxy-client/src/App2.tsx
+++ packages/payment-proxy-client/src/App2.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -15,6 +16,21 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import PersonIcon from "@mui/icons-material/Person";
 import StorageIcon from "@mui/icons-material/Storage";
+import { NitroRpcClient } from "@statechannels/nitro-rpc-client";
+import { PaymentChannelInfo } from "@statechannels/nitro-rpc-client/src/types";
+
+import {
+  CHANNEL_ID_KEY,
+  QUERY_KEY,
+  costPerByte,
+  dataSize,
+  defaultNitroRPCUrl,
+  fileUrl,
+  hub,
+  initialChannelBalance,
+  provider,
+} from "./constants";
+import { fetchFile } from "./file";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Copyright(props: any) {
@@ -39,6 +55,253 @@ function Copyright(props: any) {
 const defaultTheme = createTheme();
 
 export default function App2() {
+  const url =
+    new URLSearchParams(window.location.search).get(QUERY_KEY) ??
+    defaultNitroRPCUrl;
+
+  const [nitroClient, setNitroClient] = useState<NitroRpcClient | null>(null);
+  const [paymentChannelId, setPaymentChannelId] = useState<string>("");
+  const [paymentChannelInfo, setPaymentChannelInfo] = useState<
+    PaymentChannelInfo | undefined
+  >();
+
+  const [errorText, setErrorText] = useState<string>("");
+
+  useEffect(() => {
+    NitroRpcClient.CreateHttpNitroClient(url)
+      .then((c) => setNitroClient(c))
+      .catch((e) => {
+        console.error(e);
+        setErrorText(e.message);
+      });
+  }, [url]);
+
+  useEffect(() => {
+    const fetchedId = localStorage.getItem(CHANNEL_ID_KEY);
+    if (fetchedId && fetchedId != "") {
+      setPaymentChannelId(fetchedId);
+
+      nitroClient?.GetPaymentChannel(fetchedId).then((paymentChannel) => {
+        console.log(paymentChannel);
+        setPaymentChannelInfo(paymentChannel);
+      });
+    }
+  }, [nitroClient]);
+
+  const updateChannelInfo = async (channelId: string) => {
+    if (channelId == "") {
+      throw new Error("Empty channel id provided");
+    }
+    const paymentChannel = await nitroClient?.GetPaymentChannel(channelId);
+    setPaymentChannelInfo(paymentChannel);
+  };
+
+  const triggerFileDownload = (file: File) => {
+    // This will prompt the browser to download the file
+    const blob = new Blob([file], { type: file.type });
+
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = file.name;
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+
+  const createPaymentChannel = async () => {
+    if (!nitroClient) {
+      setErrorText("Nitro client not initialized");
+      return;
+    }
+    const result = await nitroClient.CreatePaymentChannel(
+      provider,
+      [hub],
+      initialChannelBalance
+    );
+
+    localStorage.setItem(CHANNEL_ID_KEY, result.ChannelId);
+    setPaymentChannelId(result.ChannelId);
+    updateChannelInfo(result.ChannelId);
+
+    // TODO: Slightly hacky but we wait a beat before querying so we see the updated balance
+    setTimeout(() => {
+      updateChannelInfo(result.ChannelId);
+    }, 1000);
+  };
+
+  const fetchAndDownloadFile = async () => {
+    setErrorText("");
+
+    if (!nitroClient) {
+      setErrorText("Nitro client not initialized");
+      return;
+    }
+    if (!paymentChannelInfo) {
+      setErrorText("No payment channel to use");
+      return;
+    }
+
+    try {
+      const file = await fetchFile(
+        fileUrl,
+        costPerByte * dataSize,
+        paymentChannelInfo.ID,
+        nitroClient
+      );
+
+      triggerFileDownload(file);
+
+      // TODO: Slightly hacky but we wait a beat before querying so we see the updated balance
+      setTimeout(() => {
+        updateChannelInfo(paymentChannelInfo.ID);
+      }, 50);
+    } catch (e: unknown) {
+      console.error(e);
+      setErrorText((e as Error).message);
+    }
+  };
+
+  function VerticalLinearStepper() {
+    const [activeStep, setActiveStep] = React.useState(0);
+
+    const [createChannelDisabled, setCreateChannelDisabled] = useState(false);
+    const [payDisabled, setPayDisabled] = useState(false);
+
+    const handleNext = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleCreateChannelButton = () => {
+      setCreateChannelDisabled(true);
+      createPaymentChannel().then(handleNext);
+    };
+
+    const handlePayButton = () => {
+      setPayDisabled(true);
+      fetchAndDownloadFile().then(() => setPayDisabled(false));
+    };
+
+    return (
+      <Box sx={{ maxWidth: 400 }}>
+        <Stepper activeStep={activeStep} orientation="vertical">
+          <Step key={"Join the Nitro Payment Network"}>
+            <StepLabel>{"Join the Nitro Payment Network"}</StepLabel>
+            <StepContent>
+              <Typography>{`In this demonstration, you will be sharing in a prefunded network account on Calibration Tesnet with all other users.`}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    OK
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+
+          <Step key={"Connect to a Retrieval Provider"}>
+            <StepLabel>{"Connect to a Retrieval Provider"}</StepLabel>
+            <StepContent>
+              <Typography>
+                {
+                  "Create a virtual payment with enough capacity to pay for 10 retrievals."
+                }
+              </Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    disabled={createChannelDisabled}
+                    onClick={handleCreateChannelButton}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Create Channel
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+
+          <Step key={"Execute a Paid Retrieval"}>
+            <StepLabel>{"Execute a Paid Retrieval"}</StepLabel>
+            <StepContent>
+              <Stack spacing={5} direction="column">
+                <Stack>
+                  <Typography>
+                    {
+                      "Create a payment voucher, and attach it to a request for the provider."
+                    }
+                  </Typography>
+                  <Box sx={{ mb: 2 }}>
+                    <div>
+                      <Box
+                        component="form"
+                        noValidate
+                        onSubmit={() => {
+                          /* TODO */
+                        }}
+                        sx={{ mt: 1 }}
+                      >
+                        <Stack direction="row" spacing={2}></Stack>
+                        <FormControlLabel
+                          control={
+                            <Switch value="skippayment" color="primary" />
+                          }
+                          label="Skip payment"
+                        />
+                        <FormControlLabel
+                          control={
+                            <Switch value="usemicropayments" color="primary" />
+                          }
+                          label="Use micropayments"
+                        />
+                        <Button
+                          variant="contained"
+                          disabled={payDisabled}
+                          onClick={handlePayButton}
+                          sx={{ mt: 1, mr: 1 }}
+                        >
+                          Pay & Download
+                        </Button>
+                      </Box>
+                    </div>
+                  </Box>
+                </Stack>
+                <Stack direction="column">
+                  <Stack
+                    spacing={2}
+                    direction="row"
+                    sx={{ mb: 1 }}
+                    alignItems="center"
+                  >
+                    <Typography variant="body2" color="text.secondary">
+                      150 FIL
+                    </Typography>
+                    <PersonIcon />
+                    <Slider
+                      aria-label="Volume"
+                      value={75}
+                      valueLabelDisplay="on"
+                    />
+                    <StorageIcon />{" "}
+                    <Typography variant="body2" color="text.secondary">
+                      250 FIL
+                    </Typography>
+                  </Stack>
+                  {paymentChannelId}
+                  {errorText}
+                </Stack>
+              </Stack>
+            </StepContent>
+          </Step>
+        </Stepper>
+      </Box>
+    );
+  }
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: "100vh" }}>
@@ -84,124 +347,5 @@ export default function App2() {
         </Grid>
       </Grid>
     </ThemeProvider>
-  );
-}
-
-function VerticalLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  return (
-    <Box sx={{ maxWidth: 400 }}>
-      <Stepper activeStep={activeStep} orientation="vertical">
-        <Step key={"Join the Nitro Payment Network"}>
-          <StepLabel>{"Join the Nitro Payment Network"}</StepLabel>
-          <StepContent>
-            <Typography>{`In this demonstration, you will be sharing in a prefunded network account on Calibration Tesnet with all other users.`}</Typography>
-            <Box sx={{ mb: 2 }}>
-              <div>
-                <Button
-                  variant="contained"
-                  onClick={handleNext}
-                  sx={{ mt: 1, mr: 1 }}
-                >
-                  OK
-                </Button>
-              </div>
-            </Box>
-          </StepContent>
-        </Step>
-
-        <Step key={"Connect to a Retrieval Provider"}>
-          <StepLabel>{"Connect to a Retrieval Provider"}</StepLabel>
-          <StepContent>
-            <Typography>
-              {
-                "Create a virtual payment with enough capacity to pay for 10 retrievals."
-              }
-            </Typography>
-            <Box sx={{ mb: 2 }}>
-              <div>
-                <Button
-                  variant="contained"
-                  onClick={handleNext}
-                  sx={{ mt: 1, mr: 1 }}
-                >
-                  Create Channel
-                </Button>
-              </div>
-            </Box>
-          </StepContent>
-        </Step>
-
-        <Step key={"Execute a Paid Retrieval"}>
-          <StepLabel>{"Execute a Paid Retrieval"}</StepLabel>
-          <StepContent>
-            <Stack spacing={5} direction="column">
-              <Stack>
-                <Typography>
-                  {
-                    "Create a payment voucher, and attach it to a request for the provider."
-                  }
-                </Typography>
-                <Box sx={{ mb: 2 }}>
-                  <div>
-                    <Box
-                      component="form"
-                      noValidate
-                      onSubmit={() => {
-                        /* TODO */
-                      }}
-                      sx={{ mt: 1 }}
-                    >
-                      <Stack direction="row" spacing={2}></Stack>
-                      <FormControlLabel
-                        control={<Switch value="skippayment" color="primary" />}
-                        label="Skip payment"
-                      />
-                      <FormControlLabel
-                        control={
-                          <Switch value="usemicropayments" color="primary" />
-                        }
-                        label="Use micropayments"
-                      />
-                      <Button
-                        variant="contained"
-                        onClick={() => {
-                          /* TODO */
-                        }}
-                        sx={{ mt: 1, mr: 1 }}
-                      >
-                        Pay & Download
-                      </Button>
-                    </Box>
-                  </div>
-                </Box>
-              </Stack>
-
-              <Stack
-                spacing={2}
-                direction="row"
-                sx={{ mb: 1 }}
-                alignItems="center"
-              >
-                <Typography variant="body2" color="text.secondary">
-                  150 FIL
-                </Typography>
-                <PersonIcon />
-                <Slider aria-label="Volume" value={75} valueLabelDisplay="on" />
-                <StorageIcon />{" "}
-                <Typography variant="body2" color="text.secondary">
-                  250 FIL
-                </Typography>
-              </Stack>
-            </Stack>
-          </StepContent>
-        </Step>
-      </Stepper>
-    </Box>
   );
 }
--- packages/payment-proxy-client/src/constants.ts
+++ packages/payment-proxy-client/src/constants.ts
@@ -3,11 +3,15 @@ export const QUERY_KEY = "rpcUrl";
 export const CHANNEL_ID_KEY = "channelId";
 export const costPerByte = 1;
 
+export const proxyUrl = "https://www.w3.org";
+export const fileRelativePath = "/assets/logos/w3c/w3c-no-bars.svg";
+export const fileUrl = proxyUrl + fileRelativePath;
+export const dataSize = 6833;
+
 // env vars
 export const provider = import.meta.env.VITE_PROVIDER;
 export const hub = import.meta.env.VITE_HUB;
 export const defaultNitroRPCUrl = import.meta.env.VITE_NITRO_RPC_URL;
-export const defaultFileUrl = import.meta.env.VITE_FILE_URL;
 export const initialChannelBalance = parseInt(
   import.meta.env.VITE_INITIAL_CHANNEL_BALANCE,
   10
