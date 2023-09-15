d8f0b8f5cb1434b9adf3613513fcf50a021d586c
lalexgap <me@alexgap.ca>
2023-09-15 14:18:06 -0700

    multiply by 100

--- packages/payment-proxy-client/src/App.tsx
+++ packages/payment-proxy-client/src/App.tsx
@@ -59,9 +59,11 @@ function Copyright(props: any) {
 }
 
 function computePercentagePaid(info: PaymentChannelInfo): number {
-  return Number(
-    info.Balance.PaidSoFar /
-      (info.Balance.RemainingFunds + info.Balance.PaidSoFar)
+  return (
+    Number(
+      info.Balance.PaidSoFar /
+        (info.Balance.RemainingFunds + info.Balance.PaidSoFar)
+    ) * 100
   );
 }
 export default function App() {
