fa35b3abcc414e41682a44cf1bc154f1d3908949
lalexgap <me@alexgap.ca>
2023-09-15 16:11:28 -0700

    fix url

--- packages/payment-proxy-client/src/constants.ts
+++ packages/payment-proxy-client/src/constants.ts
@@ -3,7 +3,8 @@ export const QUERY_KEY = "rpcUrl";
 export const costPerByte = 1;
 
 export const proxyUrl = import.meta.env.VITE_PROXY_URL;
-export const fileRelativePath = "People/mimasa/test/imgformat/img/w3c_home.png";
+export const fileRelativePath =
+  "/People/mimasa/test/imgformat/img/w3c_home.png";
 export const fileUrl = proxyUrl + fileRelativePath;
 export const dataSize = 6833;
 
