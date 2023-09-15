9b4851eb485719aec950df103b05a16b652d94ea
George Knee <georgeknee@googlemail.com>
2023-09-15 11:59:49 +0100

    Revert "move functions to utils folder"
    This reverts commit 5d036325b25c4f45240c50f0f442897ddad86efc.

--- packages/nitro-gui/src/utils/index.ts
+++ packages/nitro-gui/src/utils/index.ts
@@ -1,8 +1,5 @@
 import bigDecimal from "js-big-decimal-esm";
 
-import { NitroRpcClient } from "@statechannels/nitro-rpc-client";
-import { LedgerChannelInfo } from "@statechannels/nitro-rpc-client/src/types";
-
 export const prettyPrintWei = (wei: bigint): string => {
   const PRECISION = 1;
   const names = ["wei", "kwei", "Mwei", "Gwei", "szabo", "finney", "ether"];
@@ -23,35 +20,3 @@ export const prettyPrintWei = (wei: bigint): string => {
   });
   return formattedString;
 };
-
-export async function fetchAndSetLedgerChannels(
-  nitroClient: NitroRpcClient,
-  setLedgerChannels: (l: LedgerChannelInfo[]) => void
-) {
-  setLedgerChannels(await nitroClient.GetAllLedgerChannels());
-}
-
-export async function getRpcPort(): Promise<string> {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", new URL("/rpc-port", window.location.href));
-  xhr.send();
-  return new Promise((res, rej) => {
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        res(xhr.responseText);
-      } else if (xhr.status === 404) {
-        rej("could not get rpc port");
-      }
-    };
-  });
-}
-
-export async function getRpcHost(): Promise<string> {
-  if (import.meta.env.VITE_RPC_HOST) {
-    return import.meta.env.VITE_RPC_HOST;
-  } else {
-    return getRpcPort().then(
-      (rpcPort) => window.location.hostname + ":" + rpcPort
-    );
-  }
-}
