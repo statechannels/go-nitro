71daabd58e0d554cf442976d360ea160403586ea
lalexgap <me@alexgap.ca>
2023-09-18 12:31:08 -0700

    refactor create-channel

--- cmd/create-channel/main.go
+++ cmd/create-channel/main.go
@@ -1,55 +1,72 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"log/slog"
+	"os"
 	"time"
 
-	"github.com/BurntSushi/toml"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/statechannels/go-nitro/cmd/utils"
 	"github.com/statechannels/go-nitro/internal/logging"
 	"github.com/statechannels/go-nitro/rpc"
 	"github.com/statechannels/go-nitro/rpc/transport/http"
+	"github.com/urfave/cli/v2"
 )
 
-type participantOpts struct {
-	UseDurableStore bool
-	MsgPort         int
-	RpcPort         int
-	Pk              string
-	ChainPk         string
-	ChainUrl        string
-	ChainAuthToken  string
-}
-
 const (
-	HUB_STATE_CHANNEL_ADDRESS = "0x69Ae2DF44965e6f87b25bc648E097B00762583Dc"
-	CONFIG_FILE               = "./docker/local/config.toml"
-	LOG_FILE                  = "create-single-channel.log"
+	COUNTERPARTY_ADDRESS = "counterpartyaddress"
+	NITRO_ENDPOINT       = "nitroendpoint"
+	LOG_FILE             = "create-single-channel.log"
+	AMOUNT               = "amount"
 )
 
 func main() {
-	var participantOpts participantOpts
-	if _, err := toml.DecodeFile(CONFIG_FILE, &participantOpts); err != nil {
-		panic(err)
-	}
-
 	logging.SetupDefaultFileLogger(LOG_FILE, slog.LevelDebug)
-
-	url := fmt.Sprintf(":%d/api/v1", participantOpts.RpcPort)
-	clientConnection, err := http.NewHttpTransportAsClient(url, 10*time.Millisecond)
-	if err != nil {
-		panic(err)
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:    NITRO_ENDPOINT,
+			Usage:   "Specifies the endpoint of the Nitro RPC server to connect to. This should be in the form 'host:port/api/v1'",
+			Value:   "localhost:4005/api/v1",
+			Aliases: []string{"n"},
+		},
+		&cli.StringFlag{
+			Name:    COUNTERPARTY_ADDRESS,
+			Usage:   "Specifies the address of the counterparty to create the ledger channel with.",
+			Value:   "0x111A00868581f73AB42FEEF67D235Ca09ca1E8db",
+			Aliases: []string{"c"},
+		},
+		&cli.UintFlag{
+			Name:    AMOUNT,
+			Value:   5_000_000,
+			Usage:   "Specifies the amount of wei to deposit into the ledger channel.",
+			Aliases: []string{"a"},
+		},
 	}
-	client, err := rpc.NewRpcClient(clientConnection)
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
 
-	err = utils.CreateLedgerChannel(client, common.HexToAddress(HUB_STATE_CHANNEL_ADDRESS))
-	if err != nil {
-		panic(err)
+	app := &cli.App{
+		Name:  "create-ledger-channel",
+		Usage: "Creates a ledger channel with the specified counterparty and amount",
+		Flags: flags,
+		Action: func(cCtx *cli.Context) error {
+			clientConnection, err := http.NewHttpTransportAsClient(cCtx.String(NITRO_ENDPOINT), 10*time.Millisecond)
+			if err != nil {
+				return err
+			}
+			client, err := rpc.NewRpcClient(clientConnection)
+			if err != nil {
+				return err
+			}
+			defer client.Close()
+
+			err = utils.CreateLedgerChannel(client, common.HexToAddress(cCtx.String(COUNTERPARTY_ADDRESS)), cCtx.Uint(AMOUNT))
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
