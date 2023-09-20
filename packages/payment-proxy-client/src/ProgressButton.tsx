ee8f1bb4592c65177e657d1e678c17a621ef0ef8
George Knee <georgeknee@googlemail.com>
2023-09-20 22:41:38 +0100

    fix err

--- packages/payment-proxy-client/src/ProgressButton.tsx
+++ packages/payment-proxy-client/src/ProgressButton.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import { Button, ButtonProps } from "@mui/material";
 import { styled } from "@mui/system";
 
-interface CustomButtonProps extends ButtonProps {
-  fillPercentage: number;
-}
-
 const CustomButton = styled(Button)({
   background: `linear-gradient(90deg, transparent 0%, transparent var(--fill-percentage), var(--primary-color) var(--fill-percentage), var(--primary-color) 100%)`,
-}) as React.FC<CustomButtonProps>;
+}) as React.FC<ButtonProps>;
 
 export default CustomButton;
