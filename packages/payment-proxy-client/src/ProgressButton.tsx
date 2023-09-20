abbbdc7a52103fb1173e7141732445d9813cd5a3
George Knee <georgeknee@googlemail.com>
2023-09-20 21:13:38 +0100

    GPT4 attempt 2

--- packages/payment-proxy-client/src/ProgressButton.tsx
+++ packages/payment-proxy-client/src/ProgressButton.tsx
@@ -1,31 +1,29 @@
 import React from "react";
 import Button, { ButtonProps } from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import { makeStyles } from "@mui/styles";
 
-interface ProgressButtonProps extends ButtonProps {
+interface ProgressButtonProps extends Omit<ButtonProps, "onClick"> {
   value: number;
-  children: React.ReactNode;
+  onClick: () => void;
 }
 
-const StyledButton = styled(Button)<{ progress: number }>(
-  ({ theme, progress }) => ({
-    position: "relative",
-    "&:before": {
-      content: '""',
-      display: progress < 100 ? "block" : "none",
-      position: "absolute",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: theme.palette.primary.main,
-      opacity: 0.4,
-      zIndex: -1,
-      width: `${progress}%`,
-      transition: "width 0.3s ease",
-    },
-  })
-);
+const useStyles = makeStyles({
+  progressStyle: (props: ProgressButtonProps) => ({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0, 0, 255, 0.5)", // Adjust color as needed
+    width: `${props.value}%`,
+    // transition: "width 0.3s ease",
+  }),
+  customDisabled: {
+    cursor: "not-allowed",
+    pointerEvents: "none",
+    opacity: 0.5,
+  },
+});
 
 const ProgressButton: React.FC<ProgressButtonProps> = ({
   value,
@@ -33,18 +31,27 @@ const ProgressButton: React.FC<ProgressButtonProps> = ({
   children,
   ...props
 }) => {
-  const isDisabled = value != 0;
+  const isDisabled = value > 0;
+  const classes = useStyles({
+    value,
+    onClick,
+    children,
+    ...props,
+  });
 
   return (
-    <StyledButton
-      variant="contained"
-      disabled={isDisabled}
-      onClick={onClick}
-      progress={value}
-      {...props}
-    >
-      {children}
-    </StyledButton>
+    <Box position="relative">
+      <Button
+        variant="contained"
+        fullWidth
+        onClick={onClick}
+        className={isDisabled ? classes.customDisabled : ""}
+        {...props}
+      >
+        {children}
+      </Button>
+      <Box className={classes.progressStyle}></Box>
+    </Box>
   );
 };
 
